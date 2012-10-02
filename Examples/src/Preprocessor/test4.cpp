a37592133a87744382918f5d6ea278350ca9e6b4
formaggia <luca.formaggia@gmail.com>
2012-10-02 21:13:42 +0200

    Added examples on cpp

---
+++ Examples/src/Preprocessor/test4.cpp
@@ -0,0 +1,26 @@
+#ifndef PIPPO
+#define PIPPO
+#endif
+
+#ifdef PIPPO
+If PIPPO is defined this part of text will be contained in
+the output file
+#endif
+
+#ifdef PLUTO
+If PLUTO is defined this part of text will be contained in
+the output file
+#endif
+
+#ifdef __cplusplus
+ Only if C++ compiler
+#endif
+	  /* Works only with newer compilers */
+
+#if __cplusplus == 201103L
+ We are compiling with a C++11 compliant compiler!
+#endif
+
+#if __cpusplus != 201103L
+#error We need a c++11 compliant compiler
+#endif
