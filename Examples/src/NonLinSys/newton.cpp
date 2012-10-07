2eb92ee3be4384813fa12949a9a95c303acf5c35
formaggia <luca.formaggia@gmail.com>
2012-10-07 23:39:49 +0200

    First version (still unchecked) of example of non-linear system

---
+++ Examples/src/NonLinSys/newton.cpp
@@ -0,0 +1,36 @@
+#include "newton.hpp"
+namespace NonLinearSystems{
+  NewtonStatus Newton(NonLinSys const & fSys,
+		      Jacobian const & jac,
+		      argumentType & x,
+		      const NewtonOptions opt)
+  {
+    if(fSys.numEq()!=x.size()) return  NewtonStatus{0,0,false};
+    // Check if we are already at zero
+    auto res=fSys.residual(x);
+    double resNorm=res.norm();
+    if(resNorm<=opt.minRes) return NewtonStatus{resNorm,0,true};
+    // Start Newton iterations
+    unsigned int iter(0);
+    double stepNorm;
+    do{
+      // Get jacobian
+      auto J=jac(x);
+      //
+      // compute step=-J^-1(x) F(x)
+      // A better code would check if the Jacobian is singular
+      // and eventualy regularize it.
+      //
+      auto step=J.fullPivLu().solve(res);
+      stepNorm=step.norm();
+      x  += step;
+      res = fSys.residual(x);
+      resNorm = res.norm();
+      ++iter;
+    }  
+    while(iter<opt.maxIter && resNorm>opt.minRes && stepNorm>opt.tolerance);
+    // Check what happened
+    bool converged(stepNorm<=opt.tolerance);
+    return NewtonStatus{resNorm,iter,converged};
+  }
+}
