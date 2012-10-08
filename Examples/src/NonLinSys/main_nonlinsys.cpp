61cdf2c0a0687e0684887ef53dc062c999211205
formaggia <luca.formaggia@gmail.com>
2012-10-08 08:48:03 +0200

    New example on non linear systems, with a working main.

--- Examples/src/NonLinSys/main_nonlinsys.cpp
+++ Examples/src/NonLinSys/main_nonlinsys.cpp
@@ -1,5 +1,4 @@
-#include "nonLinSys.hpp"
-#include "jacob.hpp"
+#include "newton.hpp"
 #include <iostream>
 //! A functor
 struct f1{
@@ -35,5 +34,17 @@ int main(){
   cout<<" Residual at ("<<x<<")=("<<res<<")"<<endl;
   cout<<" Jacobian at ("<<x<<")=["<<endl;
   cout<< DiscreteJacobian(&F)(x)<<"]"<<endl;
-
+  /// Another non linear system
+  NonLinSys G;
+  G.addToSystem([](argumentType const & x){return x[0]+x[1]+x[2];});
+  G.addToSystem([](argumentType const & x){return x[0]*x[0]+3*x[1]-x[2];});
+  G.addToSystem([](argumentType const & x){return x[0]+2*x[1]+7*x[2];});
+  DiscreteJacobian J(&G);
+  NewtonOptions optNewton{1e-6,1.e-8,100};
+  argumentType y(3);
+  y[0]=1;y[1]=1;y[2]=1;
+  NewtonStatus result=Newton(G,J,y,optNewton);
+  cout<<"Residual="<<result.residual<<" Iterations= "<<result.iterations;
+  cout<<" Converged="<<result.converged<<endl;
+  cout<<"Solution= ("<<y<<")"<<endl;
 }
