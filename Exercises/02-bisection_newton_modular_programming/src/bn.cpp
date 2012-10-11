28f7aa11a8d6a00a60c7027985c3cfc0fcc73eee
Antonio Cervone <ant.cervone@gmail.com>
2012-10-11 17:27:37 +0200

    update exercise session 2

--- Exercises/02-bisection_newton_modular_programming/src/bn.cpp
+++ Exercises/02-bisection_newton_modular_programming/src/bn.cpp
@@ -13,17 +13,14 @@ int main() {
   int nit_bis;
   int nit_newt;
 
-  cout << bisection(0., 1., f, 1e-8, 100,
-            INCREMENT, nit_bis) << flush;
+  cout << bisection(0., 1., f, 1e-8, 100, INCREMENT, nit_bis) << flush;
   cout << '\t' << nit_bis << endl;
-  cout << newton(.1, f, df, 1e-8, 100,
-         INCREMENT, nit_newt) << flush;
+
+  cout << newton(.1, f, df, 1e-8, 100, INCREMENT, nit_newt) << flush;
   cout << '\t' << nit_newt << endl;
-  cout << robust(0., 1., f, df, 1e-8, 1e4, 100,
-         INCREMENT, nit_bis, nit_newt)
-       << flush;
-  cout << '\t' << nit_bis
-       << " "  << nit_newt << endl;
+
+  cout << robust(0., 1., f, df, 1e-8, 1e4, 100, INCREMENT, nit_bis, nit_newt) << flush;
+  cout << '\t' << nit_bis << " "  << nit_newt << endl;
 
   return 0;
 }
