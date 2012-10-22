788a5640715173dbcffec431a2f7320a78cd7789
formaggia <luca.formaggia@gmail.com>
2012-10-22 23:47:52 +0200

    Example on curiosly recurring template pattern

---
+++ Examples/src/CRTP/crtp.hpp
@@ -0,0 +1,32 @@
+#ifndef  __HHH_CRTP_HH__
+#define  __HHH_CRTP_HH__
+#include <iostream>
+//! @file crtp.hpp
+//! @brief An example of Curiusly recurring template patterns
+
+//! Base class
+template<typename T>
+class Base{
+ public:
+  //! Delegates to derived class
+  void fun(double const & x){
+    leaf().fun(x);}
+  //! Example with a static function
+  static void fstatic(){T::fstatic();}
+private:
+  //! Returns myself as derived
+  T & leaf(){return static_cast<T &>(*this);}
+};
+
+//!Derived class
+class Derived: public Base<Derived>
+{
+public:
+  //!Implementation of function fun
+  void fun(double const & x){
+    std::cout<<"In the Derived. x="<<x<<std::endl;
+  }
+  //! Implementation of the static function.
+  static void fstatic(){std::cout<<"Static function in derived"<<std::endl;}
+};
+#endif
