5ac00fc19aabde61e55554fe3dabb7d2049424c2
formaggia <luca.formaggia@gmail.com>
2012-10-27 23:57:59 +0200

    Example on explicit template istantiation

---
+++ Examples/src/Templates/ExplicitInstantiation/main.cpp
@@ -0,0 +1,16 @@
+#include "mytemp.hpp"
+//! An example of explicit template instantiation
+/*!
+   If you compile the program and analyze the symbols
+   contained in the object file with nm --demangle main.o
+   you will note that the code for the templates that
+   are declared extern in mytemp.hpp has NOT been generated
+   (the symbols are undefined).
+*/
+int main(){
+  Myclass<double> a(5.0);
+  Myclass<int> b(5);
+  Myclass<char> c('A');
+  double d=func(5.0);// call on a double
+  double e=func(5);// call on a int
+}
---
+++ Examples/src/Templates/ExplicitInstantiation/mytemp.hpp
@@ -0,0 +1,31 @@
+#ifndef __HH_MYTEML_HH__
+#define __HH_MYTEML_HH__
+//! @file mytemp.hpp
+/*!
+  @brief An example of template extern instantitiation.
+  @note  It works only with C++11
+ */
+//! A class template
+template
+<typename T>
+class Myclass{
+public:
+  Myclass(T const &i):my_data(i){}
+  double fun();
+private:
+  T my_data;
+};
+
+template
+<typename T>
+double Myclass<T>::fun(){return 1./my_data;}
+
+//! A function template
+template
+<typename T> double func(T const &a){return 0.5*a*a;}
+
+//! Extern explicit template instantiations
+extern template class Myclass<double>; 
+extern template class Myclass<int>; 
+extern template double func<double>(double const &); 
+#endif
