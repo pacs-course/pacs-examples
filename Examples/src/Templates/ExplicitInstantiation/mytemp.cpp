02e04a5ce6c8f16e80881153e492f6528535f0b3
formaggia <luca.formaggia@gmail.com>
2012-10-27 23:57:59 +0200

    Example on explicit template istantiation

---
+++ Examples/src/Templates/ExplicitInstantiation/mytemp.cpp
@@ -0,0 +1,10 @@
+#include "mytemp.hpp"
+//! @file mytemp.cpp
+/*!
+  @brief explicit template instantiations.
+  @detail With explicit instantiation we tell the compiler to generate the code for the template
+  with the specified template arguments. For a class template all methods will be compiled.
+ */
+template class Myclass<double>; // explicit instantiantion
+template class Myclass<int>; // explicit instantiation
+template double func(double const &); // explicit instantiation
