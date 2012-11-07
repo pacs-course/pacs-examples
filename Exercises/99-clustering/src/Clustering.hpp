7fcf55e1c80f0632356ada019b5e578ad704a49a
Antonio Cervone <ant.cervone@gmail.com>
2012-11-07 17:58:05 +0100

    add random generation of point set

--- Exercises/99-clustering/src/Clustering.hpp
+++ Exercises/99-clustering/src/Clustering.hpp
@@ -30,12 +30,7 @@ public:
     centroidList_T const & getCentroids() { return M_centroids; }
 
     //setup
-    void setup( real tol, int maxIt, bool verbose = false )
-    {
-        M_tol = tol;
-        M_maxIt = maxIt;
-        M_verbose = verbose;
-    }
+    void setup( real tol, int maxIt, bool verbose = false );
 
     void setInitialCentroids( centroidList_T const & centroids )
     {
@@ -43,89 +38,115 @@ public:
     }
 
     // methods
-    real apply( objectList_T const & objects )
-    {
-        // iteration loop truncated at maxIt
-        for( int it = 0; it < M_maxIt; it++ )
-        {
-            if( M_verbose )
-                std::cout << "=======================" << std::endl
-                          << "iteration " << it << std::endl;
-            // loop on the object set
-            for( size_t kObj = 0; kObj < objects.size(); kObj++ )
-            {
-                // compute the distance of the k-th object from all the centroids
-                std::array<real,N> distances;
-                for( size_t d = 0; d < N; d++ )
-                {
-                    distances[d] = M_distancePolicy.computeDistance( objects[kObj],
-                                                                     M_centroids[d] );
-                }
-
-                // find the minimum distance
-                int minPos = std::min_element( distances.begin(), distances.end() )
-                        - distances.begin();
-
-                // insert the object in the list of the closest centroid
-                M_objectList[minPos].push_back( objects[kObj] );
-            }
-
-            if( M_verbose )
-            {
-                for( size_t k = 0; k < N; k++ )
-                {
-                    std::cout << k << " > " << M_objectList[k].size() << std::endl;
-                    for( size_t j = 0; j < M_objectList[k].size(); j++ )
-                        std::cout << M_objectList[k][j] << std::endl;
-                }
-            }
-
-            bool const isIncremented = updateCentroids();
-            if ( !isIncremented )
-            {
-                if ( M_verbose )
-                    std::cout << "iteration interrupted at " << it << std::endl;
-                break;
-            }
-        }
-        return M_distancePolicy.computeDistance( M_centroids[0], M_centroids[1] );
-    }
+    real apply( objectList_T const & objects );
 
 private:
 
-    bool updateCentroids()
+    bool updateCentroids();
+
+    // members
+    real M_tol;
+    int M_maxIt;
+    bool M_verbose;
+
+    centroidList_T M_centroids;
+    objectListOnCentroid_T M_objectList;
+
+    distance_P M_distancePolicy;
+
+}; // class Clustering
+
+// TEMPLATE METHODS IMPLEMENTATION
+
+//setup
+template <size_t N, typename ObjectT, typename DistanceP>
+void Clustering<N, ObjectT, DistanceP>::setup( real tol, int maxIt, bool verbose )
+{
+    M_tol = tol;
+    M_maxIt = maxIt;
+    M_verbose = verbose;
+}
+
+template <size_t N, typename ObjectT, typename DistanceP>
+real Clustering<N, ObjectT, DistanceP>::apply( objectList_T const & objects )
+{
+    // iteration loop truncated at maxIt
+    for( int it = 0; it < M_maxIt; it++ )
     {
-        std::array<real,N> increment;
+        if( M_verbose )
+            std::cout << "=======================" << std::endl
+                      << "iteration " << it << std::endl;
+
+        // clean up object.list
         for( size_t d = 0; d < N; d++ )
+            M_objectList[d].clear();
+
+        // loop on the object set
+        for( size_t kObj = 0; kObj < objects.size(); kObj++ )
         {
-            object_T mean = object_T::Constant( 0. );
-            for( size_t kObj = 0; kObj < M_objectList[d].size(); kObj++ )
-                mean += M_objectList[d][kObj];
-            mean /= M_objectList[d].size();
-            std::cout << "mean = " << mean << std::endl;
-            increment[d] = std::fabs(
-                        M_distancePolicy.computeDistance( M_centroids[d], mean ) );
-            // update centroids value
-            M_centroids[d] = mean;
+            // compute the distance of the k-th object from all the centroids
+            std::array<real,N> distances;
+            for( size_t d = 0; d < N; d++ )
+            {
+                distances[d] = M_distancePolicy.computeDistance( objects[kObj],
+                                                                 M_centroids[d] );
+            }
+
+            // find the minimum distance
+            int minPos = std::min_element( distances.begin(), distances.end() )
+                    - distances.begin();
+
+            // insert the object in the list of the closest centroid
+            M_objectList[minPos].push_back( objects[kObj] );
         }
-        real incrementMean;
-        std::for_each( increment.begin(),
-                       increment.end(),
-                       [&]( real i ){ incrementMean += i; } );
+
+        if( M_verbose )
+        {
+            for( size_t d = 0; d < N; d++ )
+            {
+                std::cout << "centroid " << d << " has " << M_objectList[d].size() << " objects" << std::endl;
+                for( size_t j = 0; j < M_objectList[d].size(); j++ )
+                    std::cout << M_objectList[d][j] << std::endl;
+            }
+        }
+
+        bool const isIncremented = updateCentroids();
+        if ( !isIncremented )
+        {
+            if ( M_verbose )
+                std::cout << "iteration interrupted at " << it << std::endl;
+            break;
+        }
+    }
+    return M_distancePolicy.computeDistance( M_centroids[0], M_centroids[1] );
+}
+
+template <size_t N, typename ObjectT, typename DistanceP>
+bool Clustering<N, ObjectT, DistanceP>:: updateCentroids()
+{
+    std::array<real,N> increment;
+    for( size_t d = 0; d < N; d++ )
+    {
+        object_T mean = object_T::Constant( 0. );
+        for( size_t kObj = 0; kObj < M_objectList[d].size(); kObj++ )
+            mean += M_objectList[d][kObj];
+        mean /= M_objectList[d].size();
+
+        if( M_verbose ) std::cout << "mean = " << mean << std::endl;
+
+        increment[d] = std::fabs(
+                    M_distancePolicy.computeDistance( M_centroids[d], mean ) );
+
+        // update centroids value
+        M_centroids[d] = mean;
+    }
+
+    real incrementMean;
+    std::for_each( increment.begin(),
+                   increment.end(),
+                   [&]( real i ){ incrementMean += i; } );
     incrementMean /= N;
     return incrementMean > M_tol;
 }
-
-// members
-real M_tol;
-int M_maxIt;
-bool M_verbose;
-
-centroidList_T M_centroids;
-objectListOnCentroid_T M_objectList;
-
-distance_P M_distancePolicy;
-
-}; // class Clustering
 
 #endif // CLUSTERING_HPP
--- Exercises/99-clustering/src/main_clustering.cpp
+++ Exercises/99-clustering/src/main_clustering.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include <algorithm>
+#include <random>
 
 #include "Clustering.hpp"
 
 int main()
 {
+    typedef Clustering<2, Point2D, EuclideanDistance> euclideanClustering;
 
-  typedef Clustering<2, Point2D, EuclideanDistance> euclideanClustering;
+    euclideanClustering c;
 
-  euclideanClustering c;
+    c.setup( 1.e-6, 3, true );
 
-  c.setup( 1.e-6, 3, true );
+    euclideanClustering::centroidList_T centroids;
 
-  euclideanClustering::centroidList_T centroids;
+    centroids[0] << -1., 0.5;
+    centroids[1] << 2., 0.5;
 
-  centroids[0] << -1., 0.5;
-  centroids[1] << 2., 0.5;
+    c.setInitialCentroids( centroids );
 
-  c.setInitialCentroids( centroids );
+    std::random_device rd;
+    std::mt19937 gen( rd() );
+    std::normal_distribution<> n_dist( 0.5, 0.5 );
 
-  euclideanClustering::objectList_T points( 4 );
+    euclideanClustering::objectList_T points( 4 );
 
-  points[0] << 0., 0.;
-  points[1] << 1., 0.;
-  points[2] << 1., 1.;
-  points[3] << 0., 1.;
+    for( int iPoint = 0; iPoint < points.size(); iPoint++ )
+    {
+        points[iPoint] << n_dist( gen ), n_dist( gen );
+    }
 
-  real dist = c.apply( points );
+    real dist = c.apply( points );
 
-  std::cout << "centroid distance = " << dist << std::endl;
+    std::cout << "centroid distance = " << dist << std::endl;
 
-  std::array<int, 3> v({2, 1, 3});
-  //v[0] = 2; v[1] = 1; v[2] = 3;
-  std::cout << "min value at "
-            << std::min_element( v.begin(), v.end()) - v.begin()
-            << std::endl;
-  //Eigen::Vector2d x;
-  //x << 2., 3.;
-  //std::cout
+    std::array<int, 3> v({2, 1, 3});
+    //v[0] = 2; v[1] = 1; v[2] = 3;
+    std::cout << "min value at "
+              << std::min_element( v.begin(), v.end()) - v.begin()
+              << std::endl;
+    //Eigen::Vector2d x;
+    //x << 2., 3.;
+    //std::cout
 
-  return 0;
+return 0;
 }
