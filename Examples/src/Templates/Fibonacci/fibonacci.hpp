390d1704a3f6e36edaee044046e0ebefd7866361
formaggia <luca.formaggia@gmail.com>
2012-11-08 13:18:16 +0100

    Example for Fibonacci (old and new style)

---
+++ Examples/src/Templates/Fibonacci/fibonacci.hpp
@@ -0,0 +1,43 @@
+#ifndef  H_FIBONACCI_H
+#define H_FIBONACCI_H
+// ! Fibonacci class. Compatible with C++98
+template<int N> struct fib{
+  enum { value = fib<N-1>::value + fib<N-2>::value};
+};
+
+//! Specialization for 1
+template<> struct fib<1>{
+  enum { value = 1 };
+};
+
+//!Specializatio for 0
+template<> struct fib<0>{
+  enum { value = 0 };
+};
+
+
+/*!
+  Fibonacci template function (only C++11).
+  
+  It implements function template specialization and uses
+  the new constexpr keyword to indicate that we are in fact returning
+  a constant expression.
+*/
+
+template<unsigned int N>
+constexpr unsigned int Fibonacci(){
+  return Fibonacci<N-1>()+Fibonacci<N-2>();
+}
+
+template<>
+constexpr unsigned int Fibonacci<1>(){
+  return 1;
+}
+
+template<>
+constexpr unsigned int Fibonacci<0>(){
+  return 0;
+}
+
+
+#endif
---
+++ Examples/src/Templates/Fibonacci/main_fibonacci.cpp
@@ -0,0 +1,19 @@
+// template metaprogram for Fibonacci numbers
+#include "fibonacci.hpp"
+#include <iostream>
+
+
+int main() {
+  const int f3 = fib<3>::value;
+  const int f9 = fib<9>::value;
+  const int f10 =fib<10>::value;
+
+  std::cout << " f3 = " << f3 << '\n';
+  std::cout << " f9 = " << f9 << '\n';
+  std::cout << " f10= " << f10 << '\n';
+  std::cout<<" Now with the function template"<<std::endl;
+  std::cout << " f3 = " << Fibonacci<3>() << '\n';
+  std::cout << " f9 = " << Fibonacci<9>() << '\n';
+  std::cout << " f10= " << Fibonacci<10>() << '\n';
+  return 0;
+}
