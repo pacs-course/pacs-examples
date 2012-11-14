d17d00f910780252dee8e4f1464862099410eb01
formaggia <luca.formaggia@gmail.com>
2012-11-14 21:39:13 +0100

    Evample on the effect of ordering relations

---
+++ Examples/src/STL/cont/oddeven.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+//! Comparison operator
+/*!
+  Even numbers are "smaller" than odd numbers
+ */
+struct OddEven{
+  bool operator()(int const & i, int const & j)
+  {
+    return i %2 < j %2;
+  }
+};
+
+int main()
+{
+  multiset<int,OddEven> ms={1,2,3,4,5,6,7,8,9,10};
+  //! A set with the same element and same ordering
+  set<int,OddEven> s(ms.begin(),ms.end());
+  // print all elements in the multiset
+  for (int elem : ms) {
+    cout << elem << ' ';
+  }
+    cout << endl;
+
+  // print all elements in the set
+  for (int elem : s) {
+    cout << elem << ' ';
+  }
+    cout << endl;
+
+    auto p=ms.equal_range(1);
+    // print all elements equivalent to 1
+    copy (p.first, p.second,
+          ostream_iterator<int>(cout," "));
+    cout << endl;
+}
