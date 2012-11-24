691a1246800227ec0c744ca033f4248714fcd49e
Antonio Cervone <ant.cervone@gmail.com>
2012-11-24 15:03:45 +0100

    update to es7 code

--- Exercises/07-randomwalk/src/ex1/create_pics.sh
+++ Exercises/07-randomwalk/src/ex1/create_pics.sh
@@ -1,20 +1,33 @@
 #! /bin/bash
 
+# loop on files
 for n in {1..20}; do
-  n_padded=$(printf "%02d" $n)
-  echo "creatinf fig ${n_padded}" 
+
+  # keep n stored before reassigning it
+  n_stripped=${n}
+
+  # add zeroes in front of n
+  n=$(printf "%02d" $n)
+
+  echo "creating fig ${n}" 
+
+  # write down a temporary file with gnuplot commands
   cat > tmp.gnuplot << EOF
 set terminal png
-set output 'randomlap${n_padded}.png'
+set output 'randomlap${n}.png'
 
 set xrange [-19:19]
 set yrange [0:0.51]
 
 gauss(x,D,t) = 2*exp(-x*x / (4*D*t))/sqrt( 4*pi*D*t )
 
-plot 'randomlap${n}.out' w boxes lw 2, gauss(x,0.5,${n}) w l lw 3
+plot 'randomlap${n}.out' w boxes lw 2, gauss(x,0.5,${n_stripped}) w l lw 3
 EOF
 
-gnuplot tmp.gnuplot
+  # invoke gnuplot to print out the figure
+  gnuplot tmp.gnuplot
+
+  # clean up the temporary file
+  rm tmp.gnuplot
 
 done
--- Exercises/07-randomwalk/src/ex1/main_randomlap.cpp
+++ Exercises/07-randomwalk/src/ex1/main_randomlap.cpp
@@ -1,69 +1,18 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <algorithm>
-#include <random>
-#include <map>
 
-
-typedef unsigned int uint;
+#include "Distributions.hpp"
+#include "RandomWalk.hpp"
 
 int main()
 {
-    std::default_random_engine generator;
-    std::discrete_distribution<int> distribution { 1, 1 };
 
-    std::map<int,uint> a;
+    RandomWalk<discrete_distribution<int>> rw( {1,1}, 10000, SCREEN | GNUPLOT );
+    //RandomWalk<FakeDistribution> rw( {1,1}, 10000, SCREEN | GNUPLOT );
 
-    uint const s = 10000;
-    uint const c = 100;
-
-    a[0] = s;
-
-    //int fake = 1;
-
-    for( uint j = 0; j < 20; j++ )
+    for( uint time = 0; time < 20; time++ )
     {
-        std::map<int,uint> b;	
-        for( auto it = a.begin(); it != a.end(); ++it )
-        {
-	    int p = it->first;
-	    uint n = it->second;
-            for( uint k = 0; k < n; k++ )
-            {
-                int number = distribution(generator);
-		//int number = fake;
-		//fake = 1 - fake;
-                b[ p + 2 * number - 1 ]++;
-		//std::cout << "T " << p << " -> " << p + 2*number-1 << std::endl;
-            }
-        }
-
-        a = b;
-
-        std::stringstream fileName;
-	fileName << "randomlap" << j+1 << ".out";
-	std::ofstream fOut( fileName.str().c_str() );
-
-        //std::copy( a.begin(), a.end(), std::ostreambuf_iterator<uint>( std::cout ) );
-        std::cout << "==============================" << std::endl;
-        for( auto it = b.begin(); it != b.end(); ++it )
-        {
-            if( std::abs( it->first ) < 20 /*&& it->second > 0*/ )
-            {
-                std::cout << std::setw(3) << it->first << " - " 
-			  << std::setprecision(4)
-			  << std::fixed << static_cast<double>(it->second) / s << "\t";
-		fOut << it->first << " " << static_cast<double>(it->second) / s << std::endl;
-                for( uint k = 0; k < c * (double) it->second / s; k++ )
-                    std::cout << "*";
-                std::cout << std::endl;
-            }
-        }
-	fOut.close();
+        rw.step( time );
     }
-
 
     return 0;
 }
