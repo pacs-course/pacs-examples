48f1389aeb000ff11e7a1b9563b551c41665dc99
formaggia <luca.formaggia@gmail.com>
2012-11-24 22:56:33 +0100

    An example of shared library.

---
+++ Examples/src/SharedLibrary/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdlib>
+#include "smalllib.hpp"
+using namespace std;
+int main()
+{
+  foo one;
+}
---
+++ Examples/src/SharedLibrary/smalllib.hpp
@@ -0,0 +1,21 @@
+#ifndef _H_SMALLLIB_HPP
+#define _H_SMALLLIB_HPP
+#include <iosfwd>
+class foo 
+{
+public:
+  foo();
+private:
+  int a;
+  int b;
+};
+
+struct foo2
+{
+  float c;
+  float d;
+};
+
+enum pluto {first,second,third};
+
+#endif
