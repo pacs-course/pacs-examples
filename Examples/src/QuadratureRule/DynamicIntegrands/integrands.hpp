ba6870739c4d5bd98a01cf7b2f43e97127a4fef5
formaggia <luca.formaggia@gmail.com>
2012-11-27 23:44:57 +0100

    Dynamic loading of integrand functions.
    A versione of composit quadrture rule classes with dynamic
    loading of integrand.

---
+++ Examples/src/QuadratureRule/DynamicIntegrands/integrands.hpp
@@ -0,0 +1,11 @@
+#ifndef H_INTEGRANDS_HPP
+#define H_INTEGRANDS_HPP
+#include <cmath>
+/*! \file integrands.hpp
+  \brief contains the integrand funcctions
+ */
+extern "C" {
+  double fsincos(double const &);
+  double square(double const &);
+}
+#endif
