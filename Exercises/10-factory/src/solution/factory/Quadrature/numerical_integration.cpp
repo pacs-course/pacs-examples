8738598cf3d552a1c5357fa0edd6f292630dc36d
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2013-01-17 12:28:39 +0100

    Remove old solution for the factory

--- Exercises/10-factory/src/solution/factory/Quadrature/numerical_integration.cpp
+++
@@ -1,7 +0,0 @@
-#include "numerical_integration.hpp"
-namespace NumericalIntegration{
-
-  Quadrature::Quadrature(QuadratureRuleHandler const & rule, const Mesh1D & mesh):_rule(rule),_mesh(mesh){}
-
-
-}
--- Exercises/10-factory/src/solution/factory/Quadrature/numerical_rule.cpp
+++
@@ -1,9 +0,0 @@
-#include "numerical_rule.hpp"
-
-namespace NumericalIntegration{
-  QuadratureRule::QuadratureRule():_w(),_n(), my_order(0){};
-  int  QuadratureRule::num_nodes() const {return _n.size();}
-  double  QuadratureRule::node(const int i) const {return _n[i];}
-  double  QuadratureRule::weight(const int i)const {return _w[i];}
-  unsigned int  QuadratureRule::order()const {return my_order;}
-}
