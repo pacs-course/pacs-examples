32276e0ad03bb9607f9b8d35c796034c8e78d9da
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2013-01-18 08:12:29 +0100

    Update ud functions stuff

--- Exercises/10-factory/src/solution/src/QuadratureRule/AllDynamic/udf.hpp
+++ Exercises/10-factory/src/solution/src/QuadratureRule/AllDynamic/udf.hpp
@@ -1,9 +1,9 @@
 #ifndef __UDF_HPP__
 #define __UDF_HPP__
-extern "C"{
- double fsincos(double const &);
- double pippo(double const &);
- double pluto(double const &);
- double myfun(double const &);
-}
+
+double fsincos(double const &);
+double pippo(double const &);
+double pluto(double const &);
+double myfun(double const &);
+
 #endif
