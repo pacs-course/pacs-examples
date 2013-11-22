07b69cc2d9eabb98e7fdc4ce819495997829a387
Carlo de Falco <carlo.defalco@polimi.it>
2013-11-22 11:21:57 +0100

    autotools example

---
+++ Exercises/99-autotools/autoconf-automake-libtool/m4/ltoptions.m4
@@ -0,0 +1,1 @@
+/Applications/Octave.app/Contents/Resources/share/aclocal/ltoptions.m4
---
+++ Exercises/99-autotools/autotools-start/src/config.h
@@ -0,0 +1,35 @@
+/* src/config.h.  Generated from config.h.in by configure.  */
+/* src/config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the `sqrt' function. */
+#define HAVE_SQRT 1
+
+/* Name of package */
+#define PACKAGE "quadrture"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "carlo.defalco@polimi.it"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "quadrture"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "quadrture 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "quadrture"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* Version number of package */
+#define VERSION "1.0"
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
