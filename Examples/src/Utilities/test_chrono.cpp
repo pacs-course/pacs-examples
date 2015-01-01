3d7e9b99ac80475ef0892655f005ed77d172f3c7
formaggia <luca.formaggia@gmail.com>
2015-01-02 00:20:25 +0100

    updated chrono utility to c++11.

---
+++ Examples/src/Utilities/test_chrono.cpp
@@ -0,0 +1,17 @@
+#include "chrono.hpp"
+#include <iostream>
+#include <vector>
+int main()
+{
+  Timings::Chrono clock;
+  std::vector<double> bigVector;
+  clock.start();
+  for(std::size_t i=0;i<100000;++i) bigVector.push_back(3.0);
+  double sum(0);
+  for (auto i:bigVector) sum+=i;
+  std::cout<<"Elapsed time"<<clock.wallTimeNow()<<std::endl;
+  double prod(1);
+  for (auto i:bigVector) prod*=i;
+  clock.stop();
+  std::cout<<clock;
+}
