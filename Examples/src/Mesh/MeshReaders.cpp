30ec51de86fd8f90990ab63f9c238dc5f9b92275
formaggia <luca.formaggia@gmail.com>
2015-01-17 18:01:11 +0100

    Bettered the example of a mesh reader/handler for fem

--- Examples/src/Mesh/MeshReaders.cpp
+++ Examples/src/Mesh/MeshReaders.cpp
@@ -7,6 +7,7 @@
 #include "MeshReaders.hpp"
 #include <iostream>
 #include <fstream>
+#include <stdexcept>
 namespace{
 //! An helper function
 void skipInput(std::istream & f, std::string s){
@@ -18,44 +19,44 @@ void skipInput(std::istream & f, std::string s){
 namespace Fem{
 
 
-int MeshReadSimple::read(MeshTria & m, std::string const & fileName){
-	MeshHandler mesh(m);
-	std::vector<Point> & pl(mesh.pointList);
-	std::vector<Triangle> & el(mesh.elementList);
+  int MeshReadSimple::read(MeshTria & m, std::string const & fileName){
+    MeshHandler mesh(m);
+    std::vector<Point> & pl(mesh.pointList);
+    std::vector<Triangle> & el(mesh.elementList);
     std::ifstream f;
     f.open(fileName.c_str());
     std::string currLine;
     if ( !f.is_open() ) {
-    	std::cerr<<
-    			"Mesh file does not exist or is corrupted"<<
-    			std::endl;
-    	return 1;
+      std::cerr<<
+	"Mesh file does not exist or is corrupted"<<
+	std::endl;
+      return 1;
     }
-      // Read the number of points and elements
+    // Read the number of points and elements
     if(this->M_verbose)
-    	std::cout << "[Mesh::Mesh] Reading mesh data" << std::endl;
-      // Scan file lines
-      // Skip until data or end of file
+      std::cout << "[Mesh::Mesh] Reading mesh data" << std::endl;
+    // Scan file lines
+    // Skip until data or end of file
       skipInput(f,std::string("DATA"));
       // If the string is not found, abort
       if(f.eof()||f.fail()){
-    	  std::cerr << "FILE ERROR! Cannot find #DATA" << std::endl;
-      		return 2;
+	std::cerr << "FILE ERROR! Cannot find #DATA" << std::endl;
+	return 2;
       }
       // Read number of points and elements
       int nP, nE;
       f >> nP >> nE;
       if(this->M_verbose)std::cout<<"Number of points="<<nP<<
-    		  " Number of elements="<<nE<<std::endl;
+			   " Number of elements="<<nE<<std::endl;
       pl.reserve(nP);
       el.reserve(nE);
       // Now look for the points
       skipInput(f,std::string("POINTS"));
-     // If the string is not found, abort
+      // If the string is not found, abort
       if(f.eof()||f.fail()||f.bad())
       	{ std::cerr << "FILE ERROR! cannot find # POINTS" << std::endl;
-      		return 3; }
-
+	  return 3; }
+      
       // Fill point data structures
       for(int i = 0; i < nP; ++i) {
       	Point P;
@@ -69,189 +70,216 @@ int MeshReadSimple::read(MeshTria & m, std::string const & fileName){
       	//std::cout<<P[0]<<" " <<P[1]<<" "<< P.bcId()<<" "<<pl.size()<<std::endl;
       	P.id()=pl.size();
       	pl.push_back(P);
-      if(f.eof()||f.fail())
-       	{ std::cerr << "FILE ERROR! cannot read all point" << std::endl;
-       		return 3; }
+	if(f.eof()||f.fail())
+	  { std::cerr << "FILE ERROR! cannot read all point" << std::endl;
+	    return 3; }
       }
       if(this->M_verbose){
-    	  std::cout<<"Points read"<<std::endl;
-          std::cout << "Reading elements" << std::endl;
+	std::cout<<"Points read"<<std::endl;
+	std::cout << "Reading elements" << std::endl;
       }
       skipInput(f,std::string("ELEMENTS"));
-
+      
       // If the string is not found, abort
       if(f.eof()||f.fail())
       	{ std::cerr << "FILE ERROR! Cannot find # ELEMENTS" << std::endl;
-      		return 4; }
+	  return 4; }
       // Fill element data structures
       for(int i = 0; i < nE; i++) {
       	int type;
         f >> type;
         if(type != 0) {
-        	std::cerr << "I can read only triangular elements"<< std::endl;
-        	return 5;
+	  std::cerr << "I can read only triangular elements"<< std::endl;
+	  return 5;
         }
         int i1,i2,i3;
         if (!f.eof())f >> i1 >> i2 >> i3;
         if(f.eof()||f.fail()){
-        	std::cerr << "FILE ERROR! Cannot read elements" << std::endl;
-        	return 5; }
+	  std::cerr << "FILE ERROR! Cannot read elements" << std::endl;
+	  return 5; }
         Triangle t(pl[i1],pl[i2],pl[i3]);
         t.id()=el.size();
         t.bcId()=0;
-        el.push_back(t);
+        el.emplace_back(std::move(t));
       }
-    f.close();
-	return 0;
-}
-
-std::auto_ptr<MeshReader> MeshReadSimple::clone()const
-{
-	return  std::auto_ptr<MeshReader>(new MeshReadSimple(*this));
-}
-
-
-int DummyMesh::read(MeshTria & m, std::string const & filename){
-	MeshHandler mesh(m);
-	mesh.pointList.reserve(4);
-	mesh.pointList.push_back(Point(0,0));
-	mesh.pointList.push_back(Point(1,0));
-	mesh.pointList.push_back(Point(0,1));
-	mesh.pointList.push_back(Point(2,2));
-	for (unsigned int i=0;i<mesh.pointList.size();++i){
-		  Point & p=mesh.pointList.at(i);
-		  p.id()=i;
-		  p.bcId()=1;
-	  }
-	Triangle t (mesh.pointList[0],mesh.pointList[1],
-			  mesh.pointList[2]);
-	mesh.elementList.push_back(t);
-	mesh.elementList.push_back(
-		  Triangle(mesh.pointList[0],mesh.pointList[2],
-				  mesh.pointList[3]));
-	mesh.elementList[0].id()=0;
-	mesh.elementList[1].id()=1;
-	return 0;
-}
-
-std::auto_ptr<MeshReader> DummyMesh::clone()const
-{
-	return  std::auto_ptr<MeshReader>(new DummyMesh(*this));
-}
-
-
-int MeshReadTriangle::read(MeshTria & m, std::string const & filename){
-	using std::string;
-	using std::ifstream;
-	using std::clog;
-	using std::endl;
-
-	MeshHandler mesh(m);
-	std::vector<Point> & pl(mesh.pointList);
-	std::vector<Triangle> & el(mesh.elementList);
-/*	.node files:
-	    First line:  <# of points> <dimension (must be 2)> <# of attributes>
-	                                           <# of boundary markers (0 or 1)>
-	    Remaining lines:  <point #> <x> <y> [attributes] [boundary marker]
-    The attributes, which are typically floating-point values of physical
-    quantities (such as mass or conductivity) associated with the nodes of
-    a finite element mesh, are copied unchanged to the output mesh.  If -s,
-    -q, or -a is selected, each new Steiner point added to the mesh will
-    have attributes assigned to it by linear interpolation.
-
-    If the fourth entry of the first line is `1', the last column of the
-    remainder of the file is assumed to contain boundary markers.
+      f.close();
+      return 0;
+  }
+  
+  
+  int DummyMesh::read(MeshTria & m, std::string const & filename){
+    MeshHandler mesh(m);
+    mesh.pointList.reserve(4);
+    mesh.pointList.push_back(Point(0,0));
+    mesh.pointList.push_back(Point(1,0));
+    mesh.pointList.push_back(Point(0,1));
+    mesh.pointList.push_back(Point(2,2));
+    for (unsigned int i=0;i<mesh.pointList.size();++i)
+      {
+	Point & p=mesh.pointList.at(i);
+	p.id()=i;
+	p.bcId()=1;
+      }
+    mesh.elementList.emplace_back(mesh.pointList[0],mesh.pointList[1],
+				  mesh.pointList[2],0);
+    
+    mesh.elementList.emplace_back(mesh.pointList[0],mesh.pointList[2],
+				  mesh.pointList[3],1);
+    
+    return 0;
+  }
+  
+  
+  int MeshReadTriangle::read(MeshTria & m, std::string const & filename){
+    using std::string;
+    using std::ifstream;
+    using std::clog;
+    using std::endl;
+    
+    MeshHandler mesh(m);
+    std::vector<Point> & pl(mesh.pointList);
+    std::vector<Triangle> & el(mesh.elementList);
+    /*	.node files:
+	First line:  <# of points> <dimension (must be 2)> <# of attributes>
+	<# of boundary markers (0 or 1)>
+	Remaining lines:  <point #> <x> <y> [attributes] [boundary marker]
+	The attributes, which are typically floating-point values of physical
+	quantities (such as mass or conductivity) associated with the nodes of
+	a finite element mesh, are copied unchanged to the output mesh.  If -s,
+	-q, or -a is selected, each new Steiner point added to the mesh will
+	have attributes assigned to it by linear interpolation.
+	
+	If the fourth entry of the first line is `1', the last column of the
+	remainder of the file is assumed to contain boundary markers.
     */
-	string nodeFileName(filename);
-	nodeFileName+=string(".node");
-	ifstream nodeFile(nodeFileName.c_str());
-	if(nodeFile.fail()){
-		std::cerr<<"Cannot open triangle node file "<<nodeFileName<<endl;
-		return 1;
-	}
-	int numPoints,dimension,numAttributes,numBMarkers;
-	nodeFile >>numPoints>>dimension>>numAttributes>>numBMarkers;
-	pl.resize(numPoints);
-	int bMarker(0);
-	int nId;
-	double x,y,attr;
-	for (int i=0;i<numPoints;++i){
-		nodeFile>>nId>>x>>y;
-		// We discard attributes
-		for (int j=0;j<numAttributes;++j)nodeFile>>attr;
-		if(numBMarkers==1)nodeFile>>bMarker;
-		pl[nId][0]=x;
-		pl[nId][1]=y;
-		pl[nId].id()=nId;
-		pl[nId].bcId()=bMarker;
-	}
+    string nodeFileName(filename);
+    nodeFileName+=string(".node");
+    ifstream nodeFile(nodeFileName.c_str());
+    if(nodeFile.fail())
+      {
 	nodeFile.close();
-	string eleFileName(filename);
-	// Read elements
-	eleFileName+=string(".ele");
-	ifstream eleFile(eleFileName.c_str());
-	if(eleFile.fail()){
-		std::cerr<<"Cannot open triangle ele file "<<eleFileName<<endl;
-		return 2;
+	throw std::runtime_error("Cannot open triangle node file " + nodeFileName);
+      }
+    int numPoints,dimension,numAttributes,numBMarkers;
+    nodeFile >>numPoints>>dimension>>numAttributes>>numBMarkers;
+    pl.resize(numPoints);
+    if(M_verbose) std::clog<<"Num points"<<numPoints<<std::endl;
+    int bMarker(0);
+    int nId;
+    double x,y,attr;
+    for (std::size_t i=0;i<numPoints;++i)
+      {
+	nodeFile>>nId>>x>>y;
+	if(nodeFile.bad())
+	  {
+	    nodeFile.close();
+	    throw std::runtime_error("Error while reading nodes");
+	  }
+	// We discard attributes
+	for (int j=0;j<numAttributes;++j)nodeFile>>attr;
+	if(numBMarkers==1)nodeFile>>bMarker;
+	if(nodeFile.bad())
+	  {
+	    nodeFile.close();
+	    throw std::runtime_error("Error while reading nodes attributes");
+	  }
+	--nId; // Numbering from 0!
+	pl[nId][0]=x;
+	pl[nId][1]=y;
+	pl[nId].id()=nId;
+	pl[nId].bcId()=bMarker;
+      }
+    nodeFile.close();
+    string eleFileName(filename);
+    // Read elements
+    eleFileName+=string(".ele");
+    ifstream eleFile(eleFileName.c_str());
+    if(eleFile.fail())
+      {
+	std::cerr<<"Cannot open triangle ele file "<<eleFileName<<endl;
+	return 2;
+      }
+    /*   First line:  <# of triangles> <points per triangle> <# of attributes>
+	 Remaining lines:  <triangle #> <point> <point> <point> ... [attributes]
+	 
+	 Points are indices into the corresponding .node file.  The first three
+	 points are the corners, and are listed in counterclockwise order around
+	 each triangle.  (The remaining points, if any, depend on the type of
+	 finite element used.)  The attributes are just like those of .node
+	 files.*/
+    int numEle,numPTria;
+    eleFile>>numEle>>numPTria>>numAttributes;
+    if(M_verbose) std::clog<<"Num Elements  "<<numEle<<std::endl;
+    el.resize(numEle);
+    int i1,i2,i3,inext;
+    for (int i=0;i<numEle;++i){
+      eleFile>>nId>>i1>>i2>>i3;
+      if(eleFile.bad())
+	{
+	  eleFile.close();
+	  throw std::runtime_error("Error while reading elements");
 	}
-	/*   First line:  <# of triangles> <points per triangle> <# of attributes>
-	    Remaining lines:  <triangle #> <point> <point> <point> ... [attributes]
 
-	    Points are indices into the corresponding .node file.  The first three
-	    points are the corners, and are listed in counterclockwise order around
-	    each triangle.  (The remaining points, if any, depend on the type of
-	    finite element used.)  The attributes are just like those of .node
-	    files.*/
-	int numEle,numPTria;
-	eleFile>>numEle>>numPTria>>numAttributes;
-	el.resize(numEle);
-	int i1,i2,i3,inext;
-	for (int i=0;i<numEle;++i){
-		eleFile>>nId>>i1>>i2>>i3;
-		// I neglect extra points for quadratic tria
-		for (int j=3;j<numPTria;++j)eleFile>>inext;
-		for (int j=0;j<numAttributes;++j)eleFile>>attr;
-		el[nId].changePoint(0,pl[i1]);
-		el[nId].changePoint(1,pl[i2]);
-		el[nId].changePoint(2,pl[i3]);
-		el[nId].id()=nId;
-		el[nId].bcId()=0;
+      // I neglect extra points for quadratic tria
+      for (int j=3;j<numPTria;++j)eleFile>>inext;
+      for (int j=0;j<numAttributes;++j)eleFile>>attr;
+      if(eleFile.bad())
+	{
+	  eleFile.close();
+	  throw std::runtime_error("Error while reading element attributes");
 	}
-	eleFile.close();
-
-	string edgeFileName(filename);
-	// Read elements
-	edgeFileName+=string(".edge");
-	ifstream edgeFile(edgeFileName.c_str());
-	/*   First line:  <# of edges> <# of boundary markers (0 or 1)>
-	    Following lines:  <edge #> <endpoint> <endpoint> [boundary marker]
-
-	    Endpoints are indices into the corresponding .node file.  Triangle can
-	    produce .edge files (use the -e switch), but cannot read them.  The
-	    optional column of boundary markers is suppressed by the -B switch.*/
-	if (edgeFile.fail()){
-		std::clog<<"triangle Edge file not present"<<endl;
-		return 0;
-	}
-	int numEdges;
-	edgeFile>>numEdges>>numBMarkers;
-	std::vector<Edge> & edl(mesh.edgeList);
-	el.reserve(numEdges);
-	bMarker=0;
-	for (int i=0;i<numEdges;++i){
-		edgeFile>>nId>>i1>>i2;
-		if(numBMarkers==1)edgeFile>>bMarker;
-		edl[nId].changePoint(0,pl[i1]);
-		edl[nId].changePoint(1,pl[i2]);
-		edl[nId].bcId()=bMarker;
-		edl[nId].id()=nId;
-	}
+      --nId; // numbering from 0
+      el[nId].changePoint(0,pl[--i1]);
+      el[nId].changePoint(1,pl[--i2]);
+      el[nId].changePoint(2,pl[--i3]);
+      el[nId].id()=nId;
+      el[nId].bcId()=0;
+    }
+    eleFile.close();
+    
+    string edgeFileName(filename);
+    // Read elements
+    edgeFileName+=string(".edge");
+    ifstream edgeFile(edgeFileName.c_str());
+    /*   First line:  <# of edges> <# of boundary markers (0 or 1)>
+	 Following lines:  <edge #> <endpoint> <endpoint> [boundary marker]
+	 
+	 Endpoints are indices into the corresponding .node file.  Triangle can
+	 produce .edge files (use the -e switch), but cannot read them.  The
+	 optional column of boundary markers is suppressed by the -B switch.*/
+    if (edgeFile.fail())
+      {
+	std::clog<<"triangle Edge file not present"<<endl;
 	return 0;
-}
-
-std::auto_ptr<MeshReader> MeshReadTriangle::clone()const
-{
-	return  std::auto_ptr<MeshReader>(new MeshReadTriangle(*this));
-}
-
+      }
+    int numEdges;
+    edgeFile>>numEdges>>numBMarkers;
+    std::vector<Edge> & edl(mesh.edgeList);
+    edl.resize(numEdges);
+    bMarker=0;
+    for (std::size_t i=0;i<numEdges;++i)
+      {
+	edgeFile>>nId>>i1>>i2;
+	if(edgeFile.bad())
+	  {
+	    edgeFile.close();
+	    throw std::runtime_error("Error while reading edges");
+	  }
+	
+	if(numBMarkers==1)edgeFile>>bMarker;
+	if(edgeFile.bad())
+	  {
+	    edgeFile.close();
+	    throw std::runtime_error("Error while reading edge attributes");
+	  }
+	--nId;// we start from zero!
+	edl[nId].changePoint(0,pl[--i1]);
+	edl[nId].changePoint(1,pl[--i2]);
+	edl[nId].bcId()=bMarker;
+	edl[nId].id()=nId;
+      }
+    edgeFile.close();
+    return 0;
+  }
+  
 }
--- Examples/src/Mesh/femMesh.hpp
+++ Examples/src/Mesh/femMesh.hpp
@@ -13,120 +13,169 @@
 #include "smallMatrix.hpp"
 #include "Eigen/Dense"
 namespace Fem{
+  
+  using Id=unsigned int;
+  using BcId=unsigned int;
+  using Geometry::ndim;
+  using LinearAlgebra::SMatrix;
+  using Eigen::Matrix;
+  using GeoPoint=Geometry::Point;
+  
+  //! Indicates a non valid number for an identifier
+  const unsigned int NVAL=std::numeric_limits<unsigned int>::max();
+  
+  //! Extra features for a geometric entity used in a fem code
+  /*!
+   * This class implements the handling of boundary/domain markers
+   * and entity identifiers. If the entity is stored in a linear container
+   * the identifier should be equal to the position of the object
+   * in the container.
+   */
+  class FemEntity{
+  public:
+    FemEntity(Id id=NVAL):M_id(id),M_bcId(id){}
+    FemEntity(const FemEntity &)=default;
+    
+    bool unassignedId()const {return M_id==NVAL;}
+    bool unassignedBc()const {return M_bcId==NVAL;}
+    Id & id(){return M_id;}
+    Id const & id()const {return M_id;}
+    BcId & bcId(){return M_bcId;}
+    BcId const & bcId()const {return M_bcId;}
+  protected:
+    Id M_id;
+    BcId M_bcId;
+  };
 
-typedef unsigned int Id;
-typedef unsigned int BcId;
-using Geometry::ndim;
-using LinearAlgebra::SMatrix;
-using Eigen::Matrix;
-typedef Geometry::Point GeoPoint;
-typedef Geometry::Triangle GeoTriangle;
+  class Point: public GeoPoint, public FemEntity
+  {
+  public:
+  explicit  Point(double x=0, double y=0):GeoPoint(x,y),FemEntity(){};
+    Point(GeoPoint const & p):GeoPoint(p),FemEntity(){};
+    Point(const Point&)=default;
+    Point(Point&&)=default;
+    Point & operator =(const Point & p)=default;
+    Point & operator =(Point && p)=default;
+  };
 
-//! Indicates a non valid number for an identifier
-const unsigned int NVAL=std::numeric_limits<unsigned int>::max();
-
-//! Extra features for a geometric entity used in a fem code
-/*!
- * This class implements the handling of boundary/domain markers
- * and entity identifiers. If the entity is stored in a linear container
- * the identifier should be equal to the position of the object
- * in the container.
- */
-class FemEntity{
-public:
-	FemEntity():M_id(NVAL),M_bcId(NVAL){}
-	bool unassignedId()const {return M_id==NVAL;}
-	bool unassignedBc()const {return M_bcId==NVAL;}
-	Id & id(){return M_id;}
-	Id const & id()const {return M_id;}
-	BcId & bcId(){return M_bcId;}
-	BcId const & bcId()const {return M_bcId;}
-	protected:
-	Id M_id;
-	BcId M_bcId;
-};
-
-class Point: public GeoPoint, public FemEntity{
-public:
-	explicit  Point(double x=0, double y=0);
-	Point(GeoPoint const &);
-};
-
-class Edge: public FemEntity{
+  //! A FEM edge
+  /*!
+    /todo composition with Geometry::edge
+   */
+  class Edge: public FemEntity
+  {
   public:
     static const int numVertices=2;
     static const int numSides=1;
     static const int myDim=1;
-    Edge(); //Constructs an empty edge
-    Edge(Point&,Point&); //Points are given (by reference)
-    Point * changePoint(int i, Point &);
-    // We get the points by operator [] (defined in-class for inlining)
+    //!Constructs an empty edge
+    Edge():M_points{{nullptr,nullptr}}{}; 
+    //!Points are given (by reference)
+    Edge(Point & a,Point & b):M_points{{&a,&b}} {}; 
+    void changePoint(int i, Point &);
+    //! We get the points by operator [] (defined in-class for inlining)
     Point const & operator[](int i) const {return *(M_points[i]);}
     Point & operator[](int i){return *(M_points[i]);}
-    double measure() const; // Edge Length
-    // Returns x=x(t)
+    //! Edge Length
+    double measure() const; 
+    //! Returns x=x(t)
     GeoPoint map(double const &) const ;
-    // dx/dt at a point t\in(0,1) returns edge length
+    //! dx/dt at a point t\in(0,1) returns edge length
     Eigen::Matrix<double,ndim,1> jacobian(double const &) const;
-    //checks if the edge is empty: M_points contain null pointers
+    //!checks if the edge is empty: M_points contain null pointers
     bool empty()const {
-    	return M_points[0]==0||M_points[1]==0;} // inlined function
- private:
-	// I don't store directly a eigen matrix because of the limitations
-	// of the current problems of alignement (see eigen 3.0 documentation)
-	// It is not very efficient and should be changed asap
-    SMatrix<Point *, numVertices, 1> M_points;
+      return M_points[0]==nullptr||M_points[1]==nullptr;} // inlined function
+  private:
+    std::array<Point*,numVertices> M_points;
   };
-//! Class for a triangular, geometrically linear finite element.
-/*!
- * I could have inherited publicly from Geometry::Triangle, yet since
- * I have to override most of the methods I preferred writing a simple standalone class.
- */
-class Triangle : public FemEntity {
-public:
+
+  //! Class for a triangular, geometrically linear finite element.
+  /*!
+   */
+  class Triangle : public Geometry::Triangle<Fem::Point>, public FemEntity 
+  {
+  public:
+    using GeoTriangle=Geometry::Triangle<Fem::Point>;
     static const int numVertices=3;
     static const int numSides=3;
     static const int myDim=2;
-	Triangle(); //!Constructs an empty triangle
-	Triangle(Point&,Point&,Point&);
-	Point const & operator[](int i) const {return *M_points[i];}
-	Point & operator[](int i){return *M_points[i];}
-	//! The point on an edge
-	Point const & edgePoint(int edgenum,int endnum) const;
-	Point * changePoint(int i, Point &p);
-	// Return x=T(u,v)
-	GeoPoint map(GeoPoint const & u) const;
-	// Return \f$(u,v) = T^{-1}(x)$
-	GeoPoint invMap(GeoPoint const & x) const;
-	// The area
-	double measure() const{return 0.5*detJ;};
-	//\f$J=dx/d(u,v)\f$ at a point in the ref plane return det(J)
-	Eigen::Matrix<double,myDim,ndim>
-	jacobian(double u=0, double v=0) const;
-	//\f$(d(xy)/d(uv))^{-1}\f$ at a point in the ref plane
-	// \f$J_{ij}=dx_i/du_j\f$
-	Eigen::Matrix<double,myDim,ndim>
-	invJac(double u=0, double v=0) const;
-	bool empty()const;
-	// verifies orientation
-	bool orientation()const{return detJ>0.0;};
-	//! sets orientation
-	void setOrientation(bool positive=true);
-	//! Mass Matrix (unitary density)
-	Eigen::Matrix<double,3,3> localMassMatrix() const;
-	//! Stiffness matrix (unitary diffusivity)
-	Eigen::Matrix<double,3,3> localStiffMatrix() const;
-private:
-	// I don't store directly a eigen matrix because of the limitations
-	// of the current problems of alignement (see eigen 3.0 documentation)
-	// It is not very efficient and should be changed asap
-	SMatrix<Point *, numVertices, 1> M_points;
-	SMatrix<double,myDim,ndim> M_J;
-	SMatrix<double,myDim,ndim> M_invJ;
-	double detJ;
-	void M_setJac();
-};
-	std::ostream & operator <<(std::ostream&, Triangle const &);
+    using size_type=std::size_t;
+    using Point=Fem::Point; // Not needed but clearer!
+    /*!\defgroup Constructors Constructors and assignement ops
+      @{
+    */
+    //!Constructs an empty triangle
+    Triangle();
+    //! Triangle by points. Id not compulsory
+    Triangle(Point & a,Point & b,Point & c, Id id=NVAL);
+    /*! 
+      If a copy a triangle I copy the pointers to Point.
+      So I have a triangle with the same points
+    */
+    Triangle(const Triangle&)=default;
+    //! If I assign a triangle I copy the pointers to Point
+    /*! Since the semantic is ambiguous I keep it private.
+     */
+    Triangle & operator=(const Triangle&)=default;
+    //! A triangle can be moved. No risk here
+    Triangle(Triangle&&)=default;
+    //! Move copy
+    Triangle & operator=(Triangle&&)=default;
+    /*!@}*/
+    //! Changing Points
+    /*!
+      I need to hide that on Geometry::Triangle
+      since I need to recompute the Jacobian
+     */
+    void changePoint(int i, Point & p);
+    //! Cloning a triangle
+    /*!
+      Creates a new triangle with the same gometrical points
+      represented by different objects
+     */
+    
+    //! Return x=T(u,v)
+    GeoPoint map(GeoPoint const & u) const
+    {
+      Triangle const & t = *this;
+      return GeoPoint(t[0][0] + M_J(0,0) * u[0] + M_J(0,1) * u[1],
+		      t[0][1] + M_J(1,0) * u[0] + M_J(1,1) * u[1]);
+    }
+    //! Return \f$(u,v) = T^{-1}(x)$
+    GeoPoint invMap(GeoPoint const & x) const;
+    //! The area
+    /* It hides the same method in the base class, since it is more efficient.
+     */
+    double measure() const{return 0.5*detJ;};
+    //\f$J=dx/d(u,v)\f$ at a point in the ref plane return det(J)
+    Eigen::Matrix<double,myDim,ndim>
+    jacobian(double u=0, double v=0) const;
+    //\f$(d(xy)/d(uv))^{-1}\f$ at a point in the ref plane
+    // \f$J_{ij}=dx_i/du_j\f$
+    Eigen::Matrix<double,myDim,ndim>
+    invJac(double u=0, double v=0) const;
+    bool empty()const;
+    // verifies orientation
+    bool orientation()const{return detJ>0.0;};
+    //! sets orientation
+    void setOrientation(bool positive=true);
+    //! Mass Matrix (unitary density)
+    Eigen::Matrix<double,3,3> localMassMatrix() const;
+    //! Stiffness matrix (unitary diffusivity)
+    Eigen::Matrix<double,3,3> localStiffMatrix() const;
+  private:
+    // I don't store directly a eigen matrix because of the limitations
+    // of the current problems of alignement (see eigen 3.0 documentation)
+    // It is not very efficient and should be changed asap
+    SMatrix<double,myDim,ndim> M_J;
+    SMatrix<double,myDim,ndim> M_invJ;
+    double detJ;
+    void M_setJac();
+  };
+  std::ostream & operator <<(std::ostream&, Triangle const &);
+
+  //Edge comparision operators
+
 }
 
 #endif /* FEMMESH_HPP_ */
