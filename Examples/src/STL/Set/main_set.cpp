21da51bb8720fe1055e08cae34c45d81e365f75b
formaggia <luca.formaggia@gmail.com>
2015-01-17 18:01:47 +0100

    Bettered the example on the use of set
    Former-commit-id: e166d9417c305edaa38f048690957c83b188e798

---
+++ Examples/src/STL/Set/main_set.cpp
@@ -0,0 +1,50 @@
+//! An example on the use of set
+/*!
+  It gives and idea on how we can change the comparison operator for a set.
+  And some example on parameter list initialization and use of decltype.
+ */
+#include<set>
+#include<iostream>
+#include<functional>
+//! A function class
+/*!
+  We recall that a comparison operator has the signature
+  \code{.cpp}
+  bool ( T const &a, T const &b)
+  \endcode
+  and MUST satisfy:
+
+\li a< b && a>b == false
+\li !(a<b) && !(b<a) -> a==b
+
+ */
+struct comparison
+{
+  bool operator()(int const & a, int const & b)
+  {return a>b;}
+};
+
+int main()
+{
+  using namespace std;
+  // I create my comparison operator using a lambda
+  auto comp=[](int const & a,int const & b){return a>b;};
+  // I have to pass it explicitly since lambda has NO DEFAULT CONSTRUCTOR
+  set<int,decltype(comp)> s(comp);
+  s.insert(10);
+  s.insert(11);
+  for (auto i: s) cout<<i<<" ";
+  cout<<endl;
+  // Another possibility is to wrap the lambda in a function
+  std::function<bool (int const&, int const &)> compfun=comp;
+  //Initialization via initializer list
+  set<int,decltype(compfun)> t({1,2,3,4,5,6,7,},compfun);
+  for (auto i: t) cout<<i<<" ";
+  cout<<endl;
+  // using the functor (no need to give the functor explicitely as argument)
+  // I use initialization via parameter list 
+  set<int,comparison> other={5,6,7,8,9,10,11};
+  for (auto i: other) cout<<i<<" ";
+  cout<<endl;
+
+}
