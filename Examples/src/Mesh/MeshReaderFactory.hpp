236aef7e665512bd71301fe80056972b7292fe98
formaggia <luca.formaggia@gmail.com>
2015-01-29 19:49:43 +0100

    A forgotten file...
    Former-commit-id: dd434f8a267cb73fff68e128c53bd8d78c2d982c

---
+++ Examples/src/Mesh/MeshReaderFactory.hpp
@@ -0,0 +1,22 @@
+#ifndef __HH_MESHREADERFACTORY_HH
+#define __HH_MESHREADERFACTORY_HH
+#include "Factory.hpp"
+#include "MeshReaders.hpp"
+#include <string>
+namespace Fem{
+  using MeshReaderFactory=GenericFactory::Factory<MeshReader,std::string>;
+  //! Get the factory of mesh readers
+  /*
+    To be called only once in the program.
+   */
+  MeshReaderFactory const & getMeshReaderFactory()
+  {
+    MeshReaderFactory & mRF(MeshReaderFactory::instance());
+    mRF.add("Simple"  ,auto [](){return std::unique_ptr<MeshReader>(new MeshReadSimple);});
+    mRF.add("Triangle",auto [](){return std::unique_ptr<MeshReader>(new MeshReadTriangle);});
+    mRF.add("Dummy"   ,auto [](){return std::unique_ptr<MeshReader>(new DummyMesh);});
+    return mRF;
+  }
+}
+
+#endif
