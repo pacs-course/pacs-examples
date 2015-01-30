d6e6f43b1f0454b21ed06154be9da3387da91de9
formaggia <luca.formaggia@gmail.com>
2015-01-30 19:37:05 +0100

    cleaned up example on fpe handling

--- Examples/src/FPExceptions/main_fpe.cpp
+++ Examples/src/FPExceptions/main_fpe.cpp
@@ -2,65 +2,75 @@
 #include <cmath>
 #include <string>
 #include "trapfpe.hpp"
-#include "rational.hpp"
-double f1(double);
-double f2(double);
-int main(){
-  using namespace std;
-  bool theEnd=false;
-  double a,b;
-  cout<<"Type 1 if you want to trap FPEs ";
-  int ans;
-  cin>>ans;
-  bool on=(ans==1);
-  if (on) cout<< "FPE TRAP ACTIVATED"<<endl;
-  else cout<< "FPE TRAP NOT ACTIVATED"<<endl;
-  do{
-    std::cout<<"Gimme 2 numbers a and b"<<std::endl<<
-      "I will compute 1/a and log(b)"<<std::endl;
-    std::cin>>a>>b;
-    try{
-      double z=f1(a);
-      std::cout<<" 1/"<<a<<" is "<<z<<std::endl;
-      std::cout.flush();
-      test_fpe_exception(on);
-      double x=f2(b);
-      std::cout<<" log("<<b<<") is "<<x<<std::endl;
-      std::cout.flush();
-      test_fpe_exception(on);
+double f1 (double);
+double f2 (double);
+int main()
+{
+    using namespace std;
+    bool theEnd (false);
+    bool isFpe (false);
+    double a, b;
+    bool on(false);
+#ifndef FPE_ABORT
+    cout << "Type 1 if you want to launch exception if a FPEs occurs: ";
+    int ans;
+    cin >> ans;
+    on = (ans == 1);
+    if (on)
+    {
+        cout << "FPE TRAP ACTIVATED" << endl;
     }
-    catch (BadFPOper & x){
-      std::cout<<x.what()<<std::endl;
+    else
+    {
+        cout << "FPE TRAP NOT ACTIVATED" << endl;
     }
-    catch (BadDivision & x){
-      std::cout<<x.what()<<std::endl;
+#else
+    cout<<"IT WILL ABORT ON FPE"<<std::endl;
+#endif
+    do
+    {
+        std::cout << "Gimme 2 numbers a and b" << std::endl <<
+                  "I will compute 1/a and log(b)" << std::endl;
+        std::cin >> a >> b;
+        try
+        {
+            double z = f1 (a);
+            std::cout << " 1/" << a << " is " << z << std::endl;
+            std::cout.flush();
+            test_fpe_exception (on);
+            double x = f2 (b);
+            std::cout << " log(" << b << ") is " << x << std::endl;
+            std::cout.flush();
+            isFpe = test_fpe_exception (on);
+            if (isFpe)
+            {
+	      std::cout << "Some floating point exception has occurred (undetected)" << std::endl;
+            }
+        }
+        catch (std::runtime_error& x)
+        {
+            std::cout << x.what() << std::endl;
+        }
+        catch (...)
+        {
+            std::cout << "Another (unknown) exception occurred" << std::endl;
+        }
+	std::cout << "Do you want to exit? (Y for yes) default=no" << std::endl;
+	std::string input;
+	cin.clear();
+	cin>>input;
+        theEnd = (input[0] == 'Y');
     }
-    catch (...){
-      std::cout<<"Another (unknown) exception occurred"<<std::endl;
-    }
-    
-    std::cout<<" Give me a rational, first numerator and then denomintor"<<std::endl;
-    int n,d;
-    std::cin>>n>>d;
-    try{
-      Rational r(n,d);
-    }
-    catch (ZeroDivision & re){
-      cout<<re.what()<<endl;
-      feclearexcept(FE_INVALID |FE_DIVBYZERO|FE_OVERFLOW);
-    }
-    cout<<"Type yes is you want to continue: ";
-    string answer;
-    cin>>answer;
-    theEnd=answer.find("yes")==string::npos;
-  }while(!theEnd);
+    while (!theEnd);
 }
 
-  double f1(double a){
-    return 1./a;
-  }
-  
-  double f2(double c){
-    return std::log(c);
-  }
-  
+double f1 (double a)
+{
+    return 1. / a;
+}
+
+double f2 (double c)
+{
+    return std::log (c);
+}
+
--- Examples/src/FPExceptions/trapfpe.hpp
+++ Examples/src/FPExceptions/trapfpe.hpp
@@ -8,34 +8,70 @@
   In addition, if the cpp macro FPE_ABORT is set, it will make sure
   that a floating point exceptions lauches an exception object of type.
  */
-#include "myexceptions.hpp"
+#include <stdexcept>
 // The C++11 standard requires this pragma to be activated
 // Only some compilers have this feature however.
 #pragma STDC FENV_ACCESS ON
 #ifdef FPE_ABORT
 #warning "ABORTING ON FPE IS ACTIVATED" 
 //! If FPE_ABORT is set a fp exception causes the progam to terminate
-/* C style implementation
+/* C style implementation */
    static void __attribute__ ((constructor))
    trapfpe (){
    //! Enable some exceptions.  At startup all exceptions are masked.
 feenableexcept (FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
 }
-*/
+//
 //! IMPLEMENTATION AS STATIC METHOD (More C++ style)
-
-namespace{
+/*
+  namespace
+{
   struct FpeTrap{
     static void __attribute__ ((constructor))
     trapfpe (){
-      /*! Enable some exceptions.  At startup all exceptions are masked.  */
+      //! Enable some exceptions.  At startup all exceptions are masked.
       feenableexcept (FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
     }
   };
 }
+*/
 #endif
 
-//!This function throw exceptions if a fpe has been raised
-// \param on if true means that exceptions will be thrown
-void test_fpe_exception(bool on=true);
+//!This function may throw exceptions if a fpe has been raised.
+//! @param on if true means that exceptions will be thrown
+//! @return if on is false, returns true if a floating point exception has occurred
+
+bool test_fpe_exception(bool on=true);
+
+//! Exception for invalid operations
+/*
+  It launches a runtime exception with a message.
+ */
+struct 
+InvalidFPOperation: public std::runtime_error
+{
+  InvalidFPOperation():std::runtime_error("Invalid floating point operation"){};
+};
+
+//! Exception for floating point overflow
+/*
+  It launches a runtime exception with a message.
+ */
+struct 
+FloatOverflow: public std::runtime_error
+{
+  FloatOverflow():std::runtime_error("Floating point overflow"){};
+};
+
+//! Exception for zero division
+/*
+  It launches a runtime exception with a message.
+ */
+struct 
+ZeroDivision: public std::runtime_error
+{
+  ZeroDivision():std::runtime_error("Division by zero"){};
+};
+
+
 #endif
