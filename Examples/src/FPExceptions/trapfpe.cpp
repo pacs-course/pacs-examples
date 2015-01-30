fab2e07a84e5a3bed90fae0afcde56830cdcd299
formaggia <luca.formaggia@gmail.com>
2015-01-30 19:37:05 +0100

    cleaned up example on fpe handling
    Former-commit-id: 63a2b7c3b131071e7841ca433c4a53664d7f8141

--- Examples/src/FPExceptions/trapfpe.cpp
+++ Examples/src/FPExceptions/trapfpe.cpp
@@ -4,26 +4,29 @@
 /*
   This function throw an exception if some FPE have been raised
  */
-void test_fpe_exception(bool on)
+bool test_fpe_exception(bool on)
 {
   using namespace std;
-  if (!on) return;
-  // maybe some exceptions have been raised
+    // maybe some exceptions have been raised
   // I test only the "fatal exceptions"
   auto set_excepts = fetestexcept(FE_INVALID|FE_DIVBYZERO|FE_OVERFLOW);
   // clear exception flags
   feclearexcept(FE_INVALID |FE_DIVBYZERO|FE_OVERFLOW);
   switch(set_excepts){
   case FE_INVALID:
+    if (!on) return true;
     throw InvalidFPOperation();
     break;
   case FE_OVERFLOW:
+    if (!on) return true;
     throw FloatOverflow();
     break;
   case FE_DIVBYZERO:
+    if (!on) return true;
     throw ZeroDivision();
     break;
   }
+  return false;
 }
 
 
