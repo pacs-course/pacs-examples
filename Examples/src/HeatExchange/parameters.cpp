96c2d8d5f9cddf7e52fe35dfa969a2a5249eb7f1
formaggia <luca.formaggia@gmail.com>
2015-02-01 00:25:50 +0100

    Bettered example Heat Exchange
    Introduced an example of the use of GetPot
    GetPot.hpp modified to eliminate warning (a possible bug)

---
+++ Examples/src/HeatExchange/parameters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "parameters.hpp"
+std::ostream & operator << (std::ostream & out,const parameters & p)
+{
+  out<<"PARAMETER VALUES:"<<"\n";
+  out<<"itermax= "<<p.itermax<<"\n";
+  out<<"toler= "<<p.toler<<"\n";
+  out<<"L= "<<p.L<<"\n";
+  out<<"a1= "<<p.a1<<"\n";
+  out<<"a2= "<<p.a2<<"\n";
+  out<<"To= "<<p.To<<"\n";
+  out<<"Te= "<<p.Te<<"\n";
+  out<<"k= "<<p.k<<"\n";
+  out<<"hc= "<<p.hc<<"\n";
+  out<<"M= "<<p.M<<"\n\n";
+  return out;
+}
