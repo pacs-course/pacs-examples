02324ebb799c265704c3c2f4f322e0a31f9aadfa
formaggia <luca.formaggia@gmail.com>
2015-02-15 20:42:14 +0100

    Coverts expressions to string
    A simple lexical cast that uses string streams to to the work.

---
+++ Examples/src/Utilities/test_toString.cpp
@@ -0,0 +1,15 @@
+#include "tostring.hpp"
+#include <iostream>
+#include <string>
+int main()
+{
+  using namespace std;
+  using namespace Utilities;
+  string a;
+  int n;
+  double x;
+  n=10;
+  x=40.5;
+  a=string("The value of n is ") + Utilities::toString(n) + string("and of x is")+ toString(x,ios::scientific);
+  cout<<a<<"\n";
+}
---
+++ Examples/src/Utilities/tostring.hpp
@@ -0,0 +1,39 @@
+#ifndef HH_TOSTRING__HH
+#define HH_TOSTRING__HH
+#include<sstream>
+/*! @brief A simple utility the converts to sting.
+
+  The boost library lexical_cast provides a very extensive set of tool to cast
+  expressions to a sting object. Her the scope is more limited. This utility
+  produces a string from anything for which the streaming operator 
+  has been defined.
+
+  @param input An expression for which the << operator is defined
+  @param flag std::ios_base::fmtflags for formatting
+  @return the sting produced by <<input 
+
+*/
+namespace Utilities
+{
+  template<typename T>
+  std::string toString(
+		       T const & input, 
+		       std::ios_base::fmtflags flag)
+  {
+    std::stringstream stream;
+    stream.flags(flag);// set formatting flags
+    stream<<input;
+    return stream.str();
+  }
+
+  //! Version without specifying flags
+  template<typename T>
+  std::string toString(
+		       T const & input)
+  {
+    std::stringstream stream;
+    stream<<input;
+    return stream.str();
+  }
+}
+#endif
