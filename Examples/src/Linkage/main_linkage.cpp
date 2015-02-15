626d6e0a1350a9b97c260c0ca334a91118db0af9
formaggia <luca.formaggia@gmail.com>
2015-02-15 20:37:13 +0100

    A simple example on linkage

---
+++ Examples/src/Linkage/main_linkage.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "globals.hpp"
+#include "locals.hpp"
+// define globals
+GlobalParameters globalParameters;
+int globalValue=5;// I ca also initialize the global
+//! a program to show linkage
+int main()
+{
+  globalParameters.negate=false;
+  globalParameters.g2=-3;
+  // I can use fun, but NOT fun2 or fun3 becouse they have local linkage
+  std::cout<<fun(5.0)<<std::endl;
+  //! I can change the value of the global variables
+  globalParameters.negate=true;
+  std::cout<<fun(5.0)<<std::endl;
+  //std::cout<<fun2(5.0)<<std::endl; //This is an error fun2 has local linkage
+}
