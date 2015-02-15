f814e78a1ad5bed6dc9a493e5f3fa0954ba8d7d1
formaggia <luca.formaggia@gmail.com>
2015-02-15 20:41:27 +0100

    Examples on generating 1D meshes
    Functions that generated uniform grids or grids according to a spacing
    criterion.
    
    
    Former-commit-id: b144bfdd0e241a475c151228ee987d54cd478dbd

---
+++ Examples/src/OneDMesh/main_testGenerators.cpp
@@ -0,0 +1,32 @@
+#include "meshGenerators.hpp"
+#include <iostream>
+#include <fstream>
+#include <cmath>
+int main()
+{
+  using namespace Generator;
+  using namespace Geometry;
+  Domain1D domain(0.,10.);
+  // Uniform mesh
+  Uniform uniformMeshGenerator(domain,100);
+  auto meshNodesUniform=uniformMeshGenerator();
+  std::ofstream out1("uniform.dat");
+  for (auto i : meshNodesUniform)
+    {
+      out1<<i<<" 0"<<std::endl;
+    }
+  out1.close();
+  //auto h = [](double const & x){return 0.01+0.005*x*x;};
+  auto h = [](double const & x){return (0.05+ 0.1*std::sin(x*3.1415/10.));};
+  //auto h = [pi](double const & x){return (0.05+ 100*x);};
+  VariableSize variableMeshGenerator(domain, h,10000);
+  auto meshNodesVariable=variableMeshGenerator();
+
+  std::ofstream out2("variable.dat");
+  for (auto i : meshNodesVariable)
+    {
+      out2<<i<<" 0"<<std::endl;
+    }
+  out2.close();
+  
+}
