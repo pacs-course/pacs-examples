94ead9811d303e4582c6c8350b611b4769a33dd3
formaggia <luca.formaggia@gmail.com>
2015-03-08 21:04:28 +0100

    Added muparser and some other example
    Beware MAkefile.user changed to account for the new module system.

---
+++ Extras/muParser/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./configure --enable-shared=yes --prefix=`pwd`/../../Examples/
+make
+make install
+make distclean
+./configure --enable-shared=no --prefix=`pwd`/../../Examples/
+make
+make install
