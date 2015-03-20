e6496ba235f1090afecf7014780559365424ac99
Carlo de Falco <carlo.defalco@polimi.it>
2015-03-20 12:49:01 +0100

    clean up example

--- Exercises/99-makefile/preliminary_example/src/hellomake.cpp
+++ Exercises/99-makefile/preliminary_example/src/hellomake.cpp
@@ -2,11 +2,15 @@
 // --------------------------
 
 #include <hellomake.h>
+#include <iostream>
 
 int main() 
 {
+ 
   // call function from other file
   myPrintHelloMake ();
+  std::cout << "Hello from C++ as well!"
+	    << std::endl;
 
-  return(0);
+  return (0);
 }
