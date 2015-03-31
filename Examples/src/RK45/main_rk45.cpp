d601cb849eeb93ac4fc22a49df48d4d1147d1600
formaggia <luca.formaggia@gmail.com>
2015-03-31 09:29:44 +0200

    Bettered RK45 example with computation of the discretization error

--- Examples/src/RK45/main_rk45.cpp
+++ Examples/src/RK45/main_rk45.cpp
@@ -1,5 +1,7 @@
 #include "rk45.hpp"
+#include<iostream>
 #include <fstream>
+
 #include <cmath>
 int main()
 {
@@ -11,12 +13,22 @@ int main()
   double y0=1;
   double T=100;
   double h_init=0.2;
-  double errorDesired=1.e-4;
+  double errorDesired=1.e-7;
   int status;
   auto result= 
     rk45(fun,t0,T,y0,h_init,(T-t0)/4.,errorDesired,status,10000);
   ofstream file("result.dat");
   for (auto v : result)
     file<<v.first<<" "<<v.second<<std::endl;
-	 
+  file.close();
+
+  // Only if I know the exact solution
+  //auto exact=[](double const &t){return std::exp(-10.*t);}
+  auto exact=[](double const &t){return std::cos(t);};
+  double max_error(0);
+  for (auto i : result)
+    max_error=std::max(max_error,std::abs(exact(i.first)-i.second));
+  std::cout.setf(std::ios::scientific);
+  std::cout<<"Max error "<<max_error<<" Desired max error "<<errorDesired;
+  std::cout<<std::endl;
 }
