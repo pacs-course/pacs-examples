98370d08dd7f568588c23824ccebe3a4b582dda4
formaggia <luca.formaggia@gmail.com>
2015-04-26 15:36:52 +0200

    bettered example on Polygons

--- Examples/src/Polygon/Polygon.hpp
+++ Examples/src/Polygon/Polygon.hpp
@@ -2,10 +2,11 @@
 #define HH_POLYGON_HH
 #include <iostream>
 #include <vector>
+#include <array>
 #include <utility>
 
 /*!  @file Polygon.hpp 
-  @briefThis is an example of class hierarchy that
+  @brief This is an example of class hierarchy that
   represents poligonal object.
 
   @detail This module is wrapped in the namespace `Geometry`. It
@@ -14,42 +15,43 @@
   interface of all other classes representing polygonal objects.
   
   We make use of some syntax of the C++11 new standard so you need to
-  compile with *-std=c++0x* or (since gcc 4.7) *-std=c++11*
+  compile with *-std=c++11*
    */
 namespace Geometry
 {
   
-  using std::cout;
-  using std::endl;
-  using std::ostream;
-  using std::vector;
-  using std::pair;
-  
 
-  //! A class that holds points
+  //! A class that holds 2D points
+  /*! It also represents a vector in R2
+   */
   class Point2D
   {
   public:
     //! Constructor giving coordinates.
-    Point2D(double xx=0.0, double yy=0.0):coor{xx,yy}{} //C++11 syntax
-    //! Constructor with a pair.
-    Point2D(pair<double,double> c):coor{c} {}
+    Point2D(double xx=0.0, double yy=0.0):coor{{xx,yy}}{}
+    //! Copy constructor
+    Point2D(const Point2D&)=default;
     //! Returns coordinates in a pair<double>.
-    std::pair<double,double> get() const { return coor;}
+    std::array<double,2> get() const { return coor;}
     //! Sets point coordinates
-    void set(double const &xx, double const &yy){
-      coor=std::make_pair(xx,yy);}
+    void set(double const &xx, double const &yy)
+    {
+      coor={{xx,yy}};
+    }
     //! x coordinate
-    double x() const {return coor.first;}
+    double x() const {return coor[0];}
     //! y coordinate
-    double y() const {return coor.second;}
+    double y() const {return coor[1];}
     //! Subtraction is implemented as an external friend function
     friend Point2D operator - (Point2D const & a, Point2D const & b);
     //! Addition is implemented as an external friend function
     friend Point2D operator + (Point2D const & a, Point2D const & b);
   private:
-    std::pair<double,double> coor;
+    std::array<double,2> coor;
   };
+
+  //! An alias
+  using R2Vector=Point2D;
 
   //! subtraction operator.
   /*!  
@@ -57,8 +59,8 @@ namespace Geometry
     inline.
   */
   inline Point2D operator - (Point2D const & a, Point2D const & b){
-    return Point2D(a.coor.first-b.coor.first,
-                   a.coor.second-b.coor.second);
+    return Point2D(a.coor[0]-b.coor[0],
+                   a.coor[1]-b.coor[1]);
   }
 
   //! Addition operator.
@@ -67,22 +69,38 @@ namespace Geometry
     inline.
   */
   inline Point2D operator + (Point2D const & a, Point2D const & b){
-    return Point2D(a.coor.first+b.coor.first,
-                   a.coor.second+b.coor.second);
+    return Point2D(a.coor[0]+b.coor[0],
+                   a.coor[1]+b.coor[1]);
   }
 
   //! Distance between points
   double distance(Point2D const & a, Point2D const & b);
  
   //! Polygon vertices are just vectors of points.
-  typedef vector<Point2D> Vertices;
+  using Vertices=std::vector<Point2D>;
 
   //! Defines the common interface of polygons.
   class AbstractPolygon
   {
   public:
-    //! Empty polygon
-    AbstractPolygon():vertexes(),isconvex(false){};
+    //! Constructor taking vertices
+    /*! 
+      It checks convexity if check=true
+     */
+    AbstractPolygon(Vertices const & v, bool check=true);
+    //! Default constructor is defaulted
+    /*! 
+      It is up to the derived classes to fill the vertexex and other info correctly
+    */
+    AbstractPolygon()=default;
+    //! Assignment
+    AbstractPolygon & operator=(AbstractPolygon const&)=default;
+    //! Copy constructor
+    AbstractPolygon(AbstractPolygon const &)=default;
+    //! Move constructor
+    AbstractPolygon(AbstractPolygon&&)=default;
+    //! Move constructor
+    AbstractPolygon & operator=(AbstractPolygon&&)=default;
     //! virtual destructor
     virtual ~AbstractPolygon(){};
     //! Returns the number of vertices.
@@ -99,7 +117,7 @@ namespace Geometry
     //! Returns the vertices (read only)
     Vertices const & theVertices()const {return vertexes;}
     //! Outputs some info on the polygon
-    virtual void showMe(ostream & out=cout) const;
+    virtual void showMe(std::ostream & out=std::cout) const;
     //! The area of the polygon (with sign!).
     /*!
       It is a pure virtual function.
@@ -107,13 +125,6 @@ namespace Geometry
     */
     virtual double area() const=0;
   protected:
-    //! Protected constructor taking vertices.
-    /* 
-       This constructor is kept protected because it is not part of
-       the interface of AbstractPolygon, yet some derived classes may
-       make use of it
-    */
-    AbstractPolygon(Vertices const & v);
     Vertices vertexes;
     bool isconvex;
     //! Test convexity of the polygon
@@ -129,33 +140,27 @@ namespace Geometry
   {
   public:
     //! Default constructor.
-    /*!
-      It does nothing.
-     */
-    Polygon(){};
     //! Polygon may be constructed giving Vertices;
-    Polygon(Vertices const &);
+    Polygon(Vertices const & v);
     //! Destructor
     virtual ~Polygon(){};
     /*!
       The area is positive if vertices are given in 
       counterclockwise order
-     */
+    */
     virtual double area() const;
-    //! Set vertices for a Polygon.
-    void set(Vertices const & v);
     //! Specialised version for generic polygons.
-    virtual void showMe(ostream & out=cout) const;
+    virtual void showMe(std::ostream & out=std::cout) const;
   };
 
   //! A square
   /*!
     The square is a final class derived from polygon.
    */
-  class Square: public AbstractPolygon
+  class Square final: public AbstractPolygon
   {
   public:
-    Square(){this->isconvex=true;};
+    Square(Vertices const & v);
     //!Special constructor valid only for squares.
     /*!
       /param origin Point which gives the first vertex of the square.
@@ -163,30 +168,29 @@ namespace Geometry
       /param angle In radians, tells how the square is  rotated. 
      */
     Square(Point2D origin, double length,double angle=0.0);
+    Square(Square const &)=default;
+    Square(Square&&)=default;
+    Square & operator=(const Square &)=default;
+    Square & operator=(Square &&)=default;
     //! Specialised version for squares
     virtual double area() const;
-    virtual ~Square(){};
     //! Specialised version for squares.
-    virtual void showMe(ostream & out=cout) const;
+    virtual void showMe(std::ostream & out=std::cout) const;
   };
   
   //! A triangle
-  class Triangle: public AbstractPolygon
+  class Triangle final: public AbstractPolygon
   {
   public:
-    Triangle(){this->isconvex=true;};
-    //!Constructor for triangles.
-    /*!
-      It uses the general contructor but it verifies if
-      we give 3 points, if not the program exits with exit(1)
-      It may be an interesting case to implement exceptions
-     */
     Triangle(Vertices const &);
+    Triangle(Triangle const &)=default;
+    Triangle(Triangle&&)=default;
+    Triangle & operator=(const Triangle &)=default;
+    Triangle & operator=(Triangle &&)=default;
     //! Specialised for Triangles
     virtual double area() const;
-    virtual ~Triangle(){};
     //! Specialised for Triangles
-    virtual void showMe(ostream & out=cout) const;
+    virtual void showMe(std::ostream & out=std::cout) const;
   };
   
 }
