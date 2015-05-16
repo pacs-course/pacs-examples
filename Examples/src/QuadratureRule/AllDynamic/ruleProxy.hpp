69f948e6babeba7debabe772112cf7ff51dbcb1c
formaggia <luca.formaggia@gmail.com>
2015-05-16 21:42:23 +0200

    cleaned up code for Quadrature Rules
    Former-commit-id: c61501d585ed8be1025968a654fc0a55e25cab09

--- Examples/src/QuadratureRule/AllDynamic/ruleProxy.hpp
+++ Examples/src/QuadratureRule/AllDynamic/ruleProxy.hpp
@@ -2,6 +2,7 @@
 #define H_RULEPROXY_HH
 #include <string>
 #include "QuadratureRule.hpp"
+#include "QuadratureRuleAdaptive.hpp"
 #include "Proxy.hpp"
 #include "Factory.hpp"
 //!Specialization of Factory and Proxy for QuadratureRules
@@ -11,4 +12,27 @@ namespace QuadratureRuleFactory
   //! Only C++11
   template <typename ConcreteRule> using RuleProxy=GenericFactory::Proxy<RulesFactory,ConcreteRule>;
 }
+
+//! If I am using adaptive quadrature rule I need to set tolerance and iteration
+template< typename RULE>
+struct ruleWrapper
+{
+  static void setTargetError(RULE & r, double t){};
+  static void setMaxIter(RULE &, unsigned int n){};
+};
+
+//! Specialization 
+template< typename RULE>
+struct ruleWrapper<NumericalIntegration::QuadratureRuleAdaptive<RULE>>
+{
+  static void setTargetError( NumericalIntegration::QuadratureRule& r, double const t)
+  {
+    dynamic_cast<NumericalIntegration::QuadratureRuleAdaptive<RULE>&>(r).setTargetError(t);
+  };
+  static void setMaxIter(NumericalIntegration::QuadratureRule & r, unsigned int n)
+  {
+    dynamic_cast<NumericalIntegration::QuadratureRuleAdaptive<RULE>&>(r).setMaxIter(n);
+  };
+};
+
 #endif
--- Examples/src/QuadratureRule/baseVersion/helperfunction.hpp
+++ Examples/src/QuadratureRule/baseVersion/helperfunction.hpp
@@ -1,13 +1,40 @@
 #ifndef HELPERFUNCTION_HPP
 #define HELPERFUNCTION_HPP
 #include <iostream>
+#include <string>
 //! Helper function to read data from terminal
 namespace NumericalIntegration
 {
-void readParameters(const int argc, char** argv,
-		    double &a, double &b, int & nint,double & TargetError,
-		    std::ostream & out=std::cout);
-void printout(double const value,double const exact,double const error,
-	      std::string const & title);
+  //! Reads parameters given as option
+  //! @param argc number of options
+  //! @param argv Options string
+  //! @param a left boundary of integration interval
+  //! @param b right boundary of integration interval
+  //! @param nint of integration sub-interval
+  //! @param targetError Target error (for adaptive)
+  //! @param cout Output stream for the '-h' option
+  void readParameters(const int argc, char** argv,
+                      double &a, double &b, int & nint,double & targetError,
+                      std::ostream & out=std::cout);
+  
+  // A simple utility to output some results
+  void printout(double const value,double const exact,double const error,
+                std::string const & title,std::ostream & out=std::cout);
+  
+  //! Reads parameters given as option
+  //! @param argc number of options
+  //! @param argv Options string
+  //! @param a left boundary of integration interval
+  //! @param b right boundary of integration interval
+  //! @param nint of integration sub-interval
+  //! @param targetError Target error (for adaptive)
+  //! @param library Library of integrands
+  //! @param integrand the desired integrand
+  //! @param cout Output stream for the '-h' option
+  void readParameters(const int argc, char** argv,
+		      double &a, double &b, int & nint, double & targetError,
+                      std::string & library, std::string & integrand,
+		      std::ostream & out);
+
 }
 #endif
