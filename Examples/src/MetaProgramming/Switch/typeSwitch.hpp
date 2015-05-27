e64ef350827f3fe6aa6e072023bca486ea029e09
formaggia <luca.formaggia@gmail.com>
2015-05-27 21:27:43 +0200

    Added example on reference binding and bettered example on move semantic and type traits

--- Examples/src/MetaProgramming/Switch/typeSwitch.hpp
+++ Examples/src/MetaProgramming/Switch/typeSwitch.hpp
@@ -2,86 +2,85 @@
 #define _TYPESWITCH_HPP__
 #include <vector>
 #include <iostream>
-//! We can use the type to switch between implementations
-/*! Is an alternative to template specialization
- */
-class Edge {
+
+class Point
+{
+public:
+  using BoundaryType=void;
+};
+
+class Edge 
+{
+public:
+  using BoundaryType=Point;  
 };
 
 class Triangle{
 public:
-  typedef Edge BoundaryType;
+  using BoundaryType=Edge;
 };
 
 class Tetra{
 public:
-  typedef Triangle BoundaryType;
+  using BoundaryType=Triangle;
 };
 
-//A simple empty struct that allows to select types
-template<typename T>
-struct TypeToType{};
-
-//! May be build with  T=Triangle or T=Tetra
+//! May be build with  T=Edges or T=Triangle or T=Tetra
 template<typename GeoElement>
-class mesh{
+class Mesh{
 
 public:
-  typedef typename GeoElement::BoundaryType BoundaryType;
+  using ElementType = GeoElement;
+  using BoundaryType= typename GeoElement::BoundaryType;
   //! estracts list of boundary elements
-  /*! The correct list is selected using the type
-   */
-  std::vector<BoundaryType> &
-  boundaryElementList(){ return 
-      _boundaryElementList(TypeToType<BoundaryType>());
-      }
-
+  std::vector<BoundaryType> const &
+  boundaryElementList() const
+  {
+    return M_boundaryElements;
+  }
+  // Other methods .....
  private:
-  std::vector<Edge> _edges;
-  std::vector<Triangle> _triangles;
-  std::vector<Tetra> _tetras;
-  std::vector<Edge>& _boundaryElementList(TypeToType<Edge>){
-    std::cout<<"Returning Edges"<<std::endl;
-    return _edges;}
-  std::vector<Triangle>& _boundaryElementList(TypeToType<Triangle>){
-    std::cout<<"Returning Triangles"<<std::endl;
-    return _triangles;}
+  std::vector<ElementType> M_elements;
+  std::vector<BoundaryType> M_boundaryElements;
 };
-//
-// Of course as an alternative I can use template specialization
-/*
-template<>
-vector<Edge> Mesh<Triangle>::boundaryElementList(){
-  return this->_edges;
-}
-
-template<>
-vector<Triangle> Mesh<Tetra>::boundaryElementList(){
-  return this->_tetras;
-}
-*/
 
 //! A different implementation using special traits
+/*! 
+  Pure declaration of primary template
+*/
+template <int> struct IntToElement; 
 
-template <int T> struct
-IntToElement;
+//! Speciatization for dimension 0
+template<>
+struct IntToElement<0>{
+  using ElementType=Point;
+};
+
+//! Speciatization for dimension 1
 template<>
 struct IntToElement<1>{
-  typedef Edge ElementType;
-};
-template<>
-struct IntToElement<2>{
-  typedef Triangle ElementType;
-};
-template<>
-struct IntToElement<3>{
-  typedef Tetra ElementType;
+  using ElementType=Edge;
 };
 
-template<int T> class 
-Mesh2{
-  typedef typename IntToElement<T>::ElementType ElementType;
-  typedef typename IntToElement<T-1>::ElementType BElementType;
+//! Speciatization for dimension 2
+template<>
+struct IntToElement<2>{
+  using ElementType=Triangle;
+};
+
+//! Speciatization for dimension 3
+template<>
+struct IntToElement<3>{
+  using ElementType=Tetra;
+};
+
+template<int N> 
+class Mesh2
+{
+public:
+  using ElementType = typename IntToElement<N>::ElementType;
+  using BoundaryType= typename IntToElement<N-1>::ElementType;
+  // AS BEFORE!
 };
 
 
