f50658f8f68ef6a7d0fa6eae4e7fd754dc139658
formaggia <luca.formaggia@gmail.com>
2015-05-27 21:27:43 +0200

    Added example on reference binding and bettered example on move semantic and type traits

--- Examples/src/MetaProgramming/TypeTraitsC11/move_swap.hpp
+++ Examples/src/MetaProgramming/TypeTraitsC11/move_swap.hpp
@@ -2,17 +2,23 @@
 #define HH_MOVE_SWAP_HH__
 #include <type_traits>
 #include <cassert>
+//! Swap function
+/*
+  If the class has a move constructor and a move assignment it is 
+  used instead of the copy equivalent
+  Note the std::swap is now defined this way!
+  @param a lvalue reference of type T
+  @param b lvalue refetence of type T
+*/
 template <class T>
-class Move_swap{
-public:
-  static_assert(std::is_move_constructible<T>::value,
-		"Move_swap requires move constructor");
-  static_assert(std::is_move_assignable<T>::value,
-		"Move_swap requires move assignment");
-  void operator()(T& a, T& b){
-    auto c(std::move(b));
-    b = std::move(a);
-    a = std::move(c);
-  }
-};
+void Swap (T&& a, T&& b)
+{
+  // If move constructor is defined it is used. Otherwise
+  // c is copyconstructed
+  auto c(std::move(b));
+  // If move assigment is defined it is used, otherwise
+  // copy assignment is used.
+  b = std::move(a);
+  a = std::move(c);
+}
 #endif
--- Examples/src/MoveSemantic/main_myMat0.cpp
+++ Examples/src/MoveSemantic/main_myMat0.cpp
@@ -16,52 +16,20 @@ int main()
   MyMat0<> b(5,5);
 
   b.fillRandom();
-  a(0,0)=5;
-  a(1,1)=10;
-  a(2,2)=20;
 
-  //! file to output the matrix
-  std::ofstream ofile;
-  ofile.open("Matrix.dat");
-  std::cout<<"Some output is in the file Matrix.dat"<<std::endl;
-  /// I want a certain format and precision
-  ofile.setf(std::ios_base::scientific, std::ios_base::floatfield);
-  ofile.precision(16);
-  std::cout<<"MATRIX a:"<<std::endl;
-  a.showMe(); //On screen (default is cout)
-  std::cout<<"Norm1     ="<<a.norm1()<<std::endl;
-  std::cout<<"NormInf   ="<<a.normInf()<<std::endl;
-  std::cout<<"Frob. Norm="<<a.normF()<<std::endl<<std::endl;
-  a.showMe(ofile); //Matrix stored on file
-  std::cout<<"Matrix b"<<std::endl;
-  b.showMe(); 
-  b.showMe(ofile); //on file
-
-  ofile.close();// close file
-
-  std::vector<double> va(4,1.0);
-  std::vector<double> res(a*va);
-  std::cout<<"result of a*[";
-  for (std::vector<double>::iterator it=va.begin();it<va.end()-1;
-       ++it)std::cout<<*it<<",";
-  std::cout<<*(va.end()-1)<<"]^T =";
-  std::cout<<"[";
-  for (std::vector<double>::iterator i=res.begin();i<res.end()-1;++i)std::cout<<*i<<", ";
-  std::cout<<*(res.end()-1)<<"]"<<std::endl;
-
-  std::cout<< "Now testing copy constructors and move semantic"<<std::endl;
+  std::cout<< "Testing copy constructors and move semantic"<<std::endl;
   MyMat0<ROWMAJOR> t(a);
   std::cout<<" This matrix should be equal to a"<<std::endl;
   t.showMe();
   MyMat0<ROWMAJOR> tc(std::move(t));
   std::cout<<" This matrix should be also equal to a"<<std::endl;
   tc.showMe();
-  std::cout<<" This matrix is now empty?"<< std::endl;
+  std::cout<<" This matrix is now empty"<< std::endl;
   t.showMe();
   t=std::move(tc);
-  std::cout<<" Now is back"<<std::endl;
+  std::cout<<" Now it is back"<<std::endl;
   t.showMe();
-  std::cout<<" And this is empty?"<<std::endl;
+  std::cout<<" And this is empty"<<std::endl;
   tc.showMe();
 
   /* SHOW THE ADVANTAGES OF MOVE SEMANTIC */
