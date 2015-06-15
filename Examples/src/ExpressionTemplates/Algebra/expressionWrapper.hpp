ce55f7c1cdb2d7ce502956e4e70d55f71f528443
formaggia <luca.formaggia@gmail.com>
2015-06-15 21:46:25 +0200

    Comments and additions
    Now I can handle also scalar OP Vector operations
    
    
    Former-commit-id: 4e9a2ef95408e7aef645ccd9f23af5e240511594

--- Examples/src/ExpressionTemplates/Algebra/expressionWrapper.hpp
+++ Examples/src/ExpressionTemplates/Algebra/expressionWrapper.hpp
@@ -3,10 +3,19 @@
 #include <utility>
 namespace ET
 {
+  //! A wrapper for expressions
+  /*! 
+     This class is an example of use of CPRT (curiosly recursive template pattern). Indeed
+     any class that encapsulates an expression should derive from it using 
+     class AnyExpression: public Expr<AnyExpression>
+     
+     The cast operator enable the use of static polymorphism in a easy way.
+
+   */
   template <class E>
   struct Expr
   {
-    //! Cast operator to derived class
+    //! Cast operator to derived class (const version)
     operator const E & () const 
     {
       return static_cast<const E &>(*this);
@@ -16,7 +25,7 @@ namespace ET
     {
       return static_cast<E &>(*this);
     }
-    std::size_t size()const {return  static_cast<const E &>(*this).size();}
+    std::size_t size() const {return static_cast<const E &>(*this).size();}
   };
 }
 #endif
