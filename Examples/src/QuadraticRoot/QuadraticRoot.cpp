c22cd1687677562f799dd759224468c7908d58fc
formaggia <luca.formaggia@gmail.com>
2016-02-09 23:02:00 +0100

    using complex
    Former-commit-id: b05b46717acd3a05485085173bfd81f3776e6073

--- Examples/src/QuadraticRoot/QuadraticRoot.cpp
+++ Examples/src/QuadraticRoot/QuadraticRoot.cpp
@@ -6,37 +6,38 @@
  */
 #include <cmath>
 #include <utility>
+#include <stdexcept>
 #include "QuadraticRoot.hpp"
 
-std::pair<Real,Real>
+std::pair<result_type,result_type>
 quadraticRoot_simple(const Real & a, const Real & b, const Real & c)
-throw(negativeDiscriminant)
 {
-  Real discriminant=b*b-4*a*c;
-  if (discriminant <0.0) throw(negativeDiscriminant() );
-  
-  discriminant=std::sqrt(discriminant);
+  //if (discriminant <0.0) throw(negativeDiscriminant() );
+  const Real zero(0.0);
   if (a==0){
-    return std::make_pair(-c/b,-c/b);
+    if(b==0) throw std::runtime_error("Degenerate quadratic form");
+    return std::make_pair(result_type{-c/b,zero},result_type{-c/b,zero});
   }
   else
     {
-      Real x1=(-b+discriminant)/(2*a);
-      Real x2=(-b-discriminant)/(2*a);
+      result_type discriminant{b*b-4*a*c,static_cast<Real>(0.0)};
+      discriminant=std::sqrt(discriminant);
+      result_type x1=(-b+discriminant)/(2*a);
+      result_type x2=(-b-discriminant)/(2*a);
       return std::make_pair(x1,x2);
     }
 }
 
-std::pair<Real,Real>
+std::pair<result_type,result_type>
 quadraticRoot(const Real & a, const Real & b, const Real & c)
-  throw(negativeDiscriminant)
 {
-  Real x1,x2;
-  Real discriminant=b*b-4*a*c;
-  if (discriminant <0.0) throw(negativeDiscriminant() );
+  result_type x1,x2;
+  result_type discriminant=b*b-4*a*c;
+  const Real zero(0.0);
   discriminant=std::sqrt(discriminant);
   if (a==0){
-    return std::make_pair(-c/b,-c/b);
+    if(b==0) throw std::runtime_error("Degenerate quadratic form");
+    return std::make_pair(result_type{-c/b,zero},result_type{-c/b,zero});
   }
   else
     // I consider two cases to reduce errors
@@ -55,8 +56,4 @@ quadraticRoot(const Real & a, const Real & b, const Real & c)
     }
 }
 
-const char *negativeDiscriminant::what() const throw()
-		{
-	return "Negative Discriminant";
-		}
 
