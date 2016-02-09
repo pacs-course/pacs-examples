550acca4a4f9906d710ca14b6c8157fc039f3b28
formaggia <luca.formaggia@gmail.com>
2016-02-09 23:02:00 +0100

    using complex
    Former-commit-id: b05b46717acd3a05485085173bfd81f3776e6073

--- Examples/src/QuadraticRoot/QuadraticRoot.hpp
+++ Examples/src/QuadraticRoot/QuadraticRoot.hpp
@@ -7,14 +7,14 @@
 
 #ifndef QUADRATICROOT_HPP_
 #define QUADRATICROOT_HPP_
-#include <exception>
+#include <complex>
 // This is a trick to allow with -DREAL=XXX to 
 // set the float number type
 #ifndef REAL
 #define REAL double
 #endif
 typedef REAL Real;
-
+using result_type=std::complex<Real>;
 //! Negative discriminant
 /*!
   An exception that will be trown if the discriminant is negative
@@ -26,15 +26,13 @@ public:
 };
 
 //! Computer the root of a quadratic polynomial using basic rule
-std::pair<Real,Real>
+std::pair<result_type,result_type>
 quadraticRoot_simple(
-		Real const & a, Real const & b, Real const & c)
-		throw(negativeDiscriminant);
+                     Real const & a, Real const & b, Real const & c);
 
 
 //! Computer the root of a quadratic polynomial using a smarter rule
-std::pair<Real,Real>
+std::pair<result_type,result_type>
 quadraticRoot(
-		Real const & a, Real const & b, Real const & c)
-		throw(negativeDiscriminant);
+              Real const & a, Real const & b, Real const & c);
 #endif /* QUADRATICROOT_HPP_ */
