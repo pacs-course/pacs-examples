0e565901c13583e7e0454377dc320477211c5439
formaggia <luca.formaggia@gmail.com>
2016-02-19 09:17:18 +0100

    Minichanges

--- Examples/src/OneDMesh/meshGenerators.cpp
+++ Examples/src/OneDMesh/meshGenerators.cpp
@@ -6,13 +6,13 @@ namespace Geometry
 {
   MeshNodes Uniform::operator()() const
   {
-    unsigned int const & n=this->M_num_elements;
-    double const a= this->M_domain.left();
-    double const b= this->M_domain.right();
+    auto const n=this->M_num_elements;
+    auto const a= this->M_domain.left();
+    auto const b= this->M_domain.right();
     if (n==0) throw std::runtime_error("At least two elements");
     MeshNodes mesh(n+1);
     double h=(b-a)/static_cast<double>(n);
-    for(int i=0;i<n;++i)mesh[i]=a+h*static_cast<double>(i);
+    for(auto i=0;i<n;++i)mesh[i]=a+h*static_cast<double>(i);
     mesh[n]=b;
     return mesh;
     
@@ -22,11 +22,11 @@ namespace Geometry
   {
     // calculate dy/dx = h^-1(x)
     MeshNodes mesh;
-    double const t0=this->M_domain.left();
-    double const T =this->M_domain.right();
-    double const y0=0;
-    double const h_max=(T-t0)/4.;
-    double const h_initial=h_max/100.;
+    auto const t0=this->M_domain.left();
+    auto const T =this->M_domain.right();
+    auto const y0=0;
+    auto const h_max=(T-t0)/4.;
+    auto const h_initial=h_max/100.;
     double constexpr final_error=1e-2;
     int status;
     std::size_t maxSteps = 20000;
@@ -34,7 +34,7 @@ namespace Geometry
       ODE::rk45(
 	   [this](double const & x,double const &){return 1./this->M_h(x);},
 	   t0,T,y0,h_initial,h_max,final_error,status,maxSteps);
-    double lastValue=solution.back().second;
+    auto lastValue=solution.back().second;
     // make it an integer
     std::size_t numElements=std::max(static_cast<std::size_t>(std::round(lastValue)),static_cast<std::size_t>(2));
     if(numElements>M_num_elements) throw std::runtime_error("VariableSize: too many elements");
@@ -63,14 +63,14 @@ namespace Geometry
 				  );
 	if (found == solution.end()) throw std::runtime_error("variableSize: Something wrong: cannot find node!");
 	pos          = found-1;
-	double xpos1 = pos->first;
-	double ypos1 = pos->second;
-	double xpos2 = found->first;
-	double ypos2 = found->second;
+	auto xpos1 = pos->first;
+	auto ypos1 = pos->second;
+	auto xpos2 = found->first;
+	auto ypos2 = found->second;
 	// Linear interpolation
 	// Division by zero should not happen. But.. just in case
 	if((ypos2-ypos1)==0) throw std::runtime_error("variableSize: something wring in the spacing function h");
-	double xpos=(xpos1*(ypos2-i)+xpos2*(i-ypos1))/(ypos2-ypos1);
+	auto xpos=(xpos1*(ypos2-i)+xpos2*(i-ypos1))/(ypos2-ypos1);
 	mesh.push_back(xpos);
       }
     // The last node
