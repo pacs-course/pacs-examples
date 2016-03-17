d924aff47a2afc1c7069a052150446ce08e9cf60
Carlo de Falco <carlo.defalco@polimi.it>
2016-03-17 18:15:39 +0100

    include refactoring example

--- Exercises/2016/01_intro/fem1d/v0.2/build.sh
+++ Exercises/2016/01_intro/fem1d/v0.2/build.sh
@@ -1,3 +1,5 @@
 g++ -std=c++11 -c fem1d.cpp -I.
-g++ -o fem1d fem1d.o
-rm fem1d.o
+g++ -std=c++11 -c mesh.cpp -I.
+g++ -std=c++11 -c gauss_seidel.cpp -I.
+g++ -o fem1d fem1d.o gauss_seidel.o mesh.o
+rm fem1d.o gauss_seidel.o mesh.o
