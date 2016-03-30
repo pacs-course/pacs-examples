e9c915ca605ef5d0de599abc8d1460307e59927d
formaggia <luca.formaggia@gmail.com>
2016-03-30 10:12:24 +0200

    New test for chrono

--- Examples/src/Utilities/test_chrono.cpp
+++ Examples/src/Utilities/test_chrono.cpp
@@ -9,7 +9,7 @@ int main()
   for(std::size_t i=0;i<100000;++i) bigVector.push_back(3.0);
   double sum(0);
   for (auto i:bigVector) sum+=i;
-  std::cout<<"Elapsed time"<<clock.wallTimeNow()<<std::endl;
+  std::cout<<"Elapsed time "<<clock.wallTimeNow()<<" microseconds"<<std::endl;
   double prod(1);
   for (auto i:bigVector) prod*=i;
   clock.stop();
