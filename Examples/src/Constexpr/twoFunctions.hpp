ea5d44c2a660e3320e7141bd850273e26be1c84e
formaggia <luca.formaggia@gmail.com>
2016-03-30 10:06:00 +0200

    Examples on constexpr

---
+++ Examples/src/Constexpr/twoFunctions.hpp
@@ -0,0 +1,19 @@
+#ifndef HH_TWOFUNCTIONS_HH
+#define HH_TWOFUNCTIONS_HH
+/*! \file twofunctions.hpp 
+  An example of use of constexpr functions
+*/
+
+//! A pure declaration of a normal function
+double cube (double const & x) noexcept; // a pure declaration
+
+/*!
+  The definition of a constexpr function must be in the header file
+  since a constexpr function is implicitly an inline function
+ */
+constexpr double cubeconst (double x)
+{
+  return x*x*x;
+}
+
+#endif
