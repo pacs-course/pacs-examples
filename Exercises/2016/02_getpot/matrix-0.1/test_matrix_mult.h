5c5065b49cf8edc58871163c153580b5f1c812c5
Carlo de Falco <carlo.defalco@polimi.it>
2016-04-05 00:11:15 +0200

    add header safeguard

--- Exercises/2016/02_getpot/matrix-0.1/test_matrix_mult.h
+++ Exercises/2016/02_getpot/matrix-0.1/test_matrix_mult.h
@@ -1,3 +1,6 @@
+#ifndef HAVE_TEST_MATRIX_MULT_H
+#define HAVE_TEST_MATRIX_MULT_H
+
 #ifndef msize
 #define msize 100
 #endif
@@ -8,3 +11,5 @@ static double c_msec;
 #define toc(x) c_diff = clock () - c_start;                   \
   c_msec = (double)c_diff * 1000 / (double)CLOCKS_PER_SEC;    \
   std::cout << x << c_msec << std::endl;
+
+#endif
