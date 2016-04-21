a24f5bab0f64905825c14975586d386280218b36
formaggia <luca.formaggia@gmail.com>
2016-04-21 08:58:37 +0200

    Small correction to some examples.

--- Examples/src/STL/SetEdge/Edges.cpp
+++ Examples/src/STL/SetEdge/Edges.cpp
@@ -1,12 +1,12 @@
 #include "Edges.hpp"
 #include <algorithm>
 #include <array>
-  bool yesOrient::operator()(Edge const& a, Edge const & b) const
-  {
-    // I rely on the fact that the < operator
-    // is defined for arrays and does lexicografic comparison
-    return a.getPointId()> b.getPointId();
-  }
+bool yesOrient::operator()(Edge const& a, Edge const & b) const
+{
+  // I rely on the fact that the < operator
+  // is defined for arrays and does lexicografic comparison
+  return a.getPointId()> b.getPointId();
+}
 
 bool noOrient::operator()(Edge const& a, Edge const & b) const
 {
@@ -14,10 +14,10 @@ bool noOrient::operator()(Edge const& a, Edge const & b) const
   auto bmax= std::max(b[0],b[1]);
   if (amax != bmax) 
     return amax>bmax;
-    else
-      {
-	auto amin= std::min(a[0],a[1]);
-	auto bmin= std::min(b[0],b[1]);
-	return amin > bmin;
-      }
-  }
+  else
+    {
+      auto amin= std::min(a[0],a[1]);
+      auto bmin= std::min(b[0],b[1]);
+      return amin > bmin;
+    }
+}
