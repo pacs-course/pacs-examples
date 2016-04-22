3d2a6606d847102ec23898f2b217469838906870
Carlo de Falco <carlo.defalco@polimi.it>
2016-04-22 12:53:00 +0200

    starting point for exercise on sparse matrix

--- Exercises/2016/04_sparsematrix_map/sparse_matrix-0.1/sparse.h
+++ Exercises/2016/04_sparsematrix_map/sparse_matrix-0.1/sparse.h
@@ -18,19 +18,9 @@ private :
   
 public :
 
-  sparse_matrix (unsigned int size)
-  {
-    this->resize (size);
-    nrows = size;
-    ncols = size;
-  }
+  sparse_matrix (unsigned int size);
 
-  sparse_matrix (unsigned int m, unsigned int n)
-  {
-    this->resize (m);
-    nrows = m;
-    ncols = n;
-  }
+  sparse_matrix (unsigned int m, unsigned int n);
 
   unsigned int
   get_rows () const
@@ -41,78 +31,20 @@ public :
   { return ncols; };
 
   double &
-  operator() (unsigned int irow, unsigned int jcol)
-  {
-    assert (irow < nrows);
-    assert (jcol < ncols);
-    return (*this)[irow][jcol];    
-  };
-
+  operator() (unsigned int irow, unsigned int jcol);
+  
   const double &
-  operator()  (unsigned int irow, unsigned int jcol) const
-  {
-    assert (irow < nrows);
-    assert (jcol < ncols);
-    auto retval = (*this)[irow].find (jcol);
-    if (retval != (*this)[irow].end ())
-      return retval->second;
-    else
-      return zero;
-  };
+  operator()  (unsigned int irow, unsigned int jcol) const;
 
   sparse_matrix
-  transpose () const
-  {
-    sparse_matrix retval (this->get_cols (),
-                          this->get_rows ());
-    for (unsigned int ii = 0; ii < this->nrows; ++ii)
-      for (auto jj = (*this)[ii].begin ();
-           jj != (*this)[ii].end (); ++jj)
-        retval(jj->first, ii) = jj->second;
-    return retval;
-  };
+  transpose () const;
 
   void
   gauss_seidel
   (const matrix &f,
    matrix &uh,
    const unsigned int maxit = 100,
-   const double tol = 1.e-9)
-  {
-
-    assert (uh.get_rows () == f.get_rows ());
-    assert (uh.get_rows () == this->get_rows ());
-    assert (uh.get_cols () == 1);
-  
-    double uh_new = 0;
-    double incrnorm = 0;  
-  
-    for (unsigned int ii = 0; ii < maxit; ++ii)
-      {
-        incrnorm = 0;
-        for (unsigned int jj = 0; jj < get_rows (); ++jj)
-          {
-            double res = f(jj, 0);
-
-            for (auto kk = (*this)[jj].begin ();
-                 kk != (*this)[jj].end (); ++kk)
-              if (kk->first != jj)
-                res -= kk->second * uh(kk->first, 0);
-          
-            uh_new = res / (*this)(jj,jj);
-
-            incrnorm = std::abs (uh_new - uh(jj, 0)) > incrnorm ?
-              std::abs (uh_new - uh(jj, 0)) :
-              incrnorm;
-
-            uh(jj, 0) = uh_new;
-          }
-        
-        if (incrnorm < tol)
-          break;
-    }
-    
-  };
+   const double tol = 1.e-9);
 
   void
   solve (sparse_matrix &rhs)
@@ -121,55 +53,12 @@ public :
 };
 
 sparse_matrix
-operator* (const sparse_matrix& A, const sparse_matrix& B)
-{
-  assert (A.get_cols () == B.get_rows ());
-  sparse_matrix retval (A.get_rows (),
-                        B.get_cols ());
-
-  for (unsigned int ii = 0; ii < A.get_rows (); ++ii)
-    for (unsigned int kk = 0; kk < B.get_cols (); ++kk)
-      {
-        double tmp = 0.0;
-        for (auto jj = A[ii].begin ();
-             jj != A[ii].end (); ++jj)
-          tmp += jj->second * B(jj->first, kk);
-
-        if (tmp != 0.0)
-          retval(ii, kk) = tmp;
-      }
-  return retval;
-};
+operator* (const sparse_matrix& A, const sparse_matrix& B);
 
 matrix
-operator* (const sparse_matrix& A, const matrix& B)
-{
-  assert (A.get_cols () == B.get_rows ());
-  matrix retval (A.get_rows (), B.get_cols ());
-
-  for (unsigned int ii = 0; ii < A.get_rows (); ++ii)
-    for (unsigned int kk = 0; kk < B.get_cols (); ++kk)
-      {
-        retval(ii, kk) = 0;
-        for (auto jj = A[ii].begin ();
-             jj != A[ii].end (); ++jj)
-          retval(ii, kk) += jj->second * B(jj->first, kk);
-      }
-  return retval;
-  
-};
+operator* (const sparse_matrix& A, const matrix& B);
 
 matrix
-operator* (const matrix& A, const sparse_matrix& B)
-{
-
-  assert (A.get_cols () == B.get_rows ());  
-  auto At = A.transpose ();
-  auto Bt = B.transpose ();
-  matrix tmp = (Bt * At);
-  matrix retval = tmp.transpose ();
-  return retval;
-  
-};
+operator* (const matrix& A, const sparse_matrix& B);
 
 #endif
