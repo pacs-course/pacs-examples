5e91e19a1e0545e5a1e00e7a3604d8458b45c2d2
Carlo de Falco <carlo.defalco@polimi.it>
2016-05-20 13:49:47 +0200

    new version

---
+++ Exercises/2016/07_quadrature_factory/fem1d-0.9/trapezoidal.h
@@ -0,0 +1,15 @@
+
+#ifndef HAVE_TRAPEZOIDAL_H
+#define HAVE_TRAPEZOIDAL_H
+
+#include "abstract_quadrature.h"
+#include <functional>
+
+class
+trapezoidal_quadrature : public abstract_quadrature
+{
+  double operator()
+  (std::function<double (double)> f, double a, double b);
+};
+
+#endif
