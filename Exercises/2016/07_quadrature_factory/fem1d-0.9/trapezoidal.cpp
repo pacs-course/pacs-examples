41eca2dfe1b122217d549317b00224511b20091d
Carlo de Falco <carlo.defalco@polimi.it>
2016-05-20 13:49:47 +0200

    new version
    Former-commit-id: 5e91e19a1e0545e5a1e00e7a3604d8458b45c2d2

---
+++ Exercises/2016/07_quadrature_factory/fem1d-0.9/trapezoidal.cpp
@@ -0,0 +1,9 @@
+#include "trapezoidal.h"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+
+double trapezoidal_quadrature::operator()
+  (std::function<double (double)> f, double a, double b)
+{ return ((b - a) * (.5 * f(b) + .5 * f(a))); };
+
