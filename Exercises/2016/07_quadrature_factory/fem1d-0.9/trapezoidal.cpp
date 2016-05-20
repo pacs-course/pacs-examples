4f07d37890584c8a9e13672d3d516be5981bfc8f
Carlo de Falco <carlo.defalco@polimi.it>
2016-05-20 14:14:14 +0200

    add register method

--- Exercises/2016/07_quadrature_factory/fem1d-0.9/trapezoidal.cpp
+++ Exercises/2016/07_quadrature_factory/fem1d-0.9/trapezoidal.cpp
@@ -7,3 +7,8 @@ double trapezoidal_quadrature::operator()
   (std::function<double (double)> f, double a, double b)
 { return ((b - a) * (.5 * f(b) + .5 * f(a))); };
 
+void
+register_rules ()
+{
+
+};
