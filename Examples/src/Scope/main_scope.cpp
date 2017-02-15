6e76fc3324f15f3c185f7a7a3eb92973beb85831
formaggia <luca.formaggia@gmail.com>
2017-02-15 17:17:35 +0100

    bettered some examples
    Former-commit-id: 8d6ad7d6fb2af36663a29cd3f4e11e52a482eaf4

--- Examples/src/Scope/main_scope.cpp
+++ Examples/src/Scope/main_scope.cpp
@@ -7,16 +7,30 @@ Are you able to guess what will be printed on the terminal?
  // Global variables
 int g=10;
 int f=30;
+// Namespace variable
+namespace pippo
+{
+  int g=40;// it overrides ::g
+  namespace pluto
+  {
+    int e=30; // it overrides pippo::e
+    int g=15; // it overrides ::g;
+  }
+}
 int main()
 {
-  using namespace std;
-  int g=20;
-  int f=70;
-  cout<< g<<" "<<::g<<endl;
+  using namespace std; // std names in the current scope
+  int g=20; // it overrides
+  int f=55;
+  cout<<"Local g "<< g<<", Global g "<<::g<<endl;
+  cout<<"pippo g "<< pippo::g<<", pluto g "<<pippo::pluto::g<<endl;
   {
-    double f=9.9;
-    cout << f<<" "<<::f<<" "<<g<<endl;
+    double f=9.9;// overrides f in the outer scope, which is now inaccessible!
+    // While the global f is still accessible using full qualified name
+    cout << "Local f "<<f<<", Global f "<<::f<<" "<<g<<endl;
   }
+  // for loops variables are in a local scope!
   for (int g=1;g<3;++g) cout<<g<<" ";
   cout<<endl;
+  
 }
