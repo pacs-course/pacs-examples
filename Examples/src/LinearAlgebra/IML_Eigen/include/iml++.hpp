cce3a3375e5311e98c42fb92b487586dd9546879
formaggia <luca.formaggia@gmail.com>
2017-03-12 16:46:22 +0100

    Fixed a lot of stuff for linear algebra examples
    IN particular added a test program for different iterative and direct
    solvers.

---
+++ Examples/src/LinearAlgebra/IML_Eigen/include/iml++.hpp
@@ -0,0 +1,12 @@
+#ifndef HH_IMLPLUSPLUS_HH
+#define HH_IMLPLUSPLUS_HH
+#include "LinearAlgebraTraits.hpp"
+#include "bicg.hpp"
+#include "cg.hpp"
+#include "cheby.hpp"
+#include "bicgstab.hpp"
+#include "cgs.hpp"
+#include "gmres.hpp"
+#include "ir.hpp"
+#include "qmr.hpp"
+#endif
