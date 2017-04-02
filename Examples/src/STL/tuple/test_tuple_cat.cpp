373b74f40db91b15c37fe030649be2c592a0bc06
formaggia <luca.formaggia@gmail.com>
2017-04-02 16:26:26 +0200

    Modified example on tuple

---
+++ Examples/src/STL/tuple/test_tuple_cat.cpp
@@ -0,0 +1,23 @@
+// from cppreference.com
+
+// With tuple_cat you can concateneta tuples but not only tuples!
+
+#include <iostream>
+#include <tuple>
+#include <string>
+#include "printtuple.hpp" 
+int main()
+{
+    std::tuple<int, std::string, float> t1(10, "Test", 3.14);
+    std::cout<<"t1= "<<t1<<std::endl;
+    int n = 7;
+    // u=you can concatenete tuples with tuples, pairs or individual variables usinf std::tie!!
+    
+    auto t2 = std::tuple_cat(t1, std::make_pair("Foo", "bar"), t1, std::tie(n));
+    // Now t2 is a tuple<int,string,float,string,string,int,string.float,int&>!!!!!
+    std::cout<<"t2= "<<t2<<std::endl;
+    // Beware tie ties references!
+    n = 10;
+    std::cout<<"t2= "<<t2<<std::endl;
+    
+}
