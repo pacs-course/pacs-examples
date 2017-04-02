39f85d12fa29d41a52f09dd21441422f18c408db
formaggia <luca.formaggia@gmail.com>
2017-04-02 16:26:26 +0200

    Modified example on tuple

--- Examples/src/STL/tuple/test_tuple.cpp
+++ Examples/src/STL/tuple/test_tuple.cpp
@@ -12,35 +12,55 @@
 #include <iostream>
 #include <complex>
 #include <string>
+#include "printtuple.hpp" 
 using namespace std;
 
 int main()
 {
-    // create a four-element tuple
-    // - elements are initialized with default value (0 for fundamental types)
-    tuple<string,int,int,complex<double>> t;
+  // create a four-element tuple
+  // - elements are initialized with default value (0 for fundamental types)
+  tuple<string,int,int,complex<double>> t;
+  
+  // create and initialize a tuple explicitly
+  tuple<int,float,string> t1(41,6.3,"nico");
+  
+  // "iterate" over elements:
+  cout << get<0>(t1) << " ";
+  cout << get<1>(t1) << " ";
+  cout << get<2>(t1) << " ";
+  cout << endl;
+  // Use the streaming operator to print
+  cout<<" Tuple t1:"<<t1<<std::endl;
 
-    // create and initialize a tuple explicitly
-    tuple<int,float,string> t1(41,6.3,"nico");
+  
+  // create tuple with make_tuple()
+  // - auto declares t2 with type of right-hand side
+  // - thus, type of t2 is tuple<int, float, char[]>
+  // Note the suffix f to indicate that 44 is a float (and not a double)
+  auto t2 = make_tuple(22,44.0f,"nico");
+  cout<<" Tuple t2:"<<t2<<std::endl;
 
-    // "iterate" over elements:
-    cout << get<0>(t1) << " ";
-    cout << get<1>(t1) << " ";
-    cout << get<2>(t1) << " ";
-    cout << endl;
+  
+  // assign second value in t2 to t1
+  get<1>(t1) = get<1>(t2); 
+  cout<<" Tuple t2 after change:"<<t2<<std::endl;
+  
+  // comparison and assignment
+  // - including type conversion from tuple<int,int,const char*>
+  //   to tuple<int,float,string>
+  if (t1 < t2) {  // compares value for value
+    t1 = t2;    // OK, assigns value for value
+  }
+  // SInce 41 is not smaller than 22 and < does lexicografic comparison
+  // t1 should be unchanged
 
-    // create tuple with make_tuple()
-    // - auto declares t2 with type of right-hand side
-    // - thus, type of t2 is tuple
-    auto t2 = make_tuple(22,44,"nico");
+  cout<<" Tuple t1 after comparison:"<<t1<<std::endl;
 
-    // assign second value in t2 to t1
-    get<1>(t1) = get<1>(t2); 
-
-    // comparison and assignment
-    // - including type conversion from tuple<int,int,const char*>
-    //   to tuple<int,float,string>
-    if (t1 < t2) {  // compares value for value
-        t1 = t2;    // OK, assigns value for value
-    }
+  // Now tie.. tie ties a tuple (ora a pair!) to references
+  // and you can use the magic of std::ignore to ignore parts
+  float x;
+  int i;
+  std::tie(i,x,std::ignore)=t1;
+  std::cout<<"i= "<<i<<" x= "<<x<<std::endl;
+  
 }
