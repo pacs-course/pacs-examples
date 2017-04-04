fee6ae118adc79a2012ba807c98427e69e8da021
Carlo de Falco <carlo.defalco@polimi.it>
2017-04-04 15:05:50 +0200

    fd example
    Former-commit-id: 944a18a7365b5113d3f46928dfe99a2ea4152404

---
+++ Exercises/2017/05-stdfunction-ad-dd/newton-dd/newton.h
@@ -0,0 +1,69 @@
+#ifndef HAVE_NEWTON_H
+#  define HAVE_NEWTON_H
+
+#include <iostream>
+#include <limits>
+#include <cmath>
+#include <functional>
+
+class
+newton_solver
+{
+ private:
+
+  std::function<double (const double)> fun;
+
+  int    maxit;
+  double funtol;
+  double xtol;
+  double x;
+  double dfdx;
+  double dx;
+  double r;
+  int    iter;
+
+  double
+  prime (double y)
+  { return (fun (y + dx/2) - fun (y - dx/2)) / (dx); }
+  
+ public:
+  
+  newton_solver (std::function<double (const double)> fun_,
+                 const int maxit_ = 100,
+                 const double funtol_ = std::numeric_limits<double>::epsilon () * 1000.0,
+                 const double xtol_ = std::numeric_limits<double>::epsilon () * 1000.0,
+                 const double dx_ = 10.0 * std::numeric_limits<double>::epsilon ())
+    : fun (fun_), maxit (maxit_), funtol (funtol_), xtol (xtol_)
+  { x = 0; dfdx = 0; dx = 10.0 * xtol; r = 0; iter = 0; }
+    
+
+  void
+  solve (const double x0)
+  {
+    x = x0;
+    for (iter = 0; iter < maxit; ++iter)
+      {
+        r    = fun (x);
+        if (std::abs (r) < funtol) break;
+        dfdx = prime (x);
+        dx   = - r / dfdx;
+        if (std::abs (dx) < xtol) break;
+        x += dx;
+      }
+  }
+
+  double
+  get_result ()
+  { return x; };
+
+  double
+  get_residual ()
+  { return r; };
+
+  int
+  get_iter ()
+  { return iter; };
+
+};
+
+#endif 
