6f94bae430a6e6a73d53c000cbf4e4273d40ba7a
Carlo de Falco <carlo.defalco@polimi.it>
2017-05-12 15:30:02 +0200

    exercise solution
    Former-commit-id: 61ed6f7c1e9b3f319b4f145e1d914758309916f2

--- Exercises/2017/07-quadrature/test_adaptive_quadrature.cpp
+++ Exercises/2017/07-quadrature/test_adaptive_quadrature.cpp
@@ -15,14 +15,15 @@ int main (int argc, char ** argv)
 
   GetPot g2 (filename.c_str ());
   std::string fun_str   = g2 ("fun", "(sin(x^2))^2");
+  std::string int_str   = g2 ("integrator", "trapz");
   double start          = g2 ("start", 0.0);
   double finish         = g2 ("finish",  4.0*atan (1.0));
   double tol            = g2 ("tol",  1.e-12);
-  int maxdepth          = g2 ("tol",  40);
+  int maxdepth          = g2 ("maxdepth",  40);
 
   muparser_fun fun (fun_str);
 
-  adaptive_quadrature aq (fun, start, finish, tol, maxdepth);
+  adaptive_quadrature aq (fun, start, finish, tol, maxdepth, int_str);
   std::cout << "integration result = " << aq.integrate () << std::endl;
   
   return 0;
