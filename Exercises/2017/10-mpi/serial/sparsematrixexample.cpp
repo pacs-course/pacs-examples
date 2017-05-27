ef791cd66a3cb69fdd6318b25636e36b6f6e5be2
Carlo de Falco <carlo.defalco@polimi.it>
2017-05-27 07:45:03 +0200

    more detailed example
    Former-commit-id: fda79d9615b213037da8efe12d29e2b0a5edd17f

--- Exercises/2017/10-mpi/serial/sparsematrixexample.cpp
+++ Exercises/2017/10-mpi/serial/sparsematrixexample.cpp
@@ -3,17 +3,17 @@
 int
 main ()
 {
-  constexpr int N = 5;
+  constexpr int N = 10;
   matrix m (N);
+  std::vector<double> x (N);
+  
   for (int jcol = 0; jcol < N; ++jcol)
     {
-      // int startrow = jcol > 0 ? jcol - 1 : 0;
-      // int endrow = jcol < N-1 ? jcol + 1 : N-1;
-      // std::cout << "jcol = " << jcol << std::endl;
+      int startrow = jcol > 0 ? jcol - 1 : 0;
+      int endrow = jcol < N-1 ? jcol + 1 : N-1;
       for (int irow = startrow;           
            irow <= endrow; ++irow)
         {
-          std::cout << "  irow = " << irow << std::endl;
           if (irow == jcol)
             m[jcol][irow] = 2.0;
           else
@@ -21,6 +21,19 @@ main ()
         }
     }
 
+  for (int ii = 0; ii < N; ++ii)
+    x[ii] = static_cast<double> (ii);
+
+  std::cout << "m = " << std::endl;
   m.print ();
+
+  std::cout << "x = " << std::endl;
+  for (int ii = 0; ii < N; ++ii)
+    std::cout << x[ii] << std::endl;
+
+  std::vector<double> y = m * x;
+  std::cout << "y = " << std::endl;
+  for (int ii = 0; ii < N; ++ii)
+    std::cout << y[ii] << std::endl;
   
 }
