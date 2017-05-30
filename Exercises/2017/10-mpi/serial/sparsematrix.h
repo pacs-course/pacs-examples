26ac39fcdda3d0faac14f4d336f8eaeae59c62ad
Carlo de Falco <carlo.defalco@polimi.it>
2017-05-31 00:21:45 +0200

    fix bug in multièlication operator

--- Exercises/2017/10-mpi/serial/sparsematrix.h
+++ Exercises/2017/10-mpi/serial/sparsematrix.h
@@ -7,16 +7,23 @@
 
 class matrix : public std::vector<std::map<int, double>>
 {
-public:
-  matrix (int N) { (*this).resize (N); };
+private:
 
+  const int nrows;
+  const int ncols;
+
+public:
+
+  matrix (int N) : nrows (N), ncols (N) { (*this).resize (N); };
+  matrix (int NR, int NC) : nrows (NR), ncols (NC) { (*this).resize (NC); };
+  
   std::vector<double>
   operator* (std::vector<double> x)
   {
-    std::vector<double> out (x.size (), .0);
+    std::vector<double> out (nrows, .0);
     for (int icol = 0; icol < (*this).size (); ++icol)
       for (auto jrow = (*this)[icol].begin (); jrow != (*this)[icol].end (); ++jrow)
-        out[(*jrow).first] += (*jrow).second * x[(*jrow).first];
+        out[(*jrow).first] += (*jrow).second * x[icol];
     return out;
   };
 
