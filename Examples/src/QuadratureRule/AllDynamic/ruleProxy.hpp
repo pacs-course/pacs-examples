cccb9fdfe19421aa1bc7330fd982353116b5bffe
formaggia <luca.formaggia@gmail.com>
2017-06-05 22:27:43 +0200

    MOdief ruleProxy. Note that we are not using it in this implementation.
    Left only if you wish to use it

--- Examples/src/QuadratureRule/AllDynamic/ruleProxy.hpp
+++ Examples/src/QuadratureRule/AllDynamic/ruleProxy.hpp
@@ -1,18 +1,12 @@
 #ifndef H_RULEPROXY_HH
 #define H_RULEPROXY_HH
 #include <string>
-#include "QuadratureRule.hpp"
-#include "QuadratureRuleAdaptive.hpp"
+#include "ruleFactory.hpp"
 #include "Proxy.hpp"
-#include "Factory.hpp"
+
 //!Specialization of Factory and Proxy for QuadratureRules
 namespace QuadratureRuleFactory
 {
-  using RulesFactory= GenericFactory::Factory
-    <
-    NumericalIntegration::QuadratureRule,std::string
-    >;
-  //! Only C++11
   template <typename ConcreteRule> using 
   RuleProxy=GenericFactory::Proxy<RulesFactory,ConcreteRule>;
 }
