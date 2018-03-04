469687ad9141cb8e618827290a6cb08509e03040
formaggia <luca.formaggia@gmail.com>
2018-03-04 20:20:00 +0100

    corrected and added files for lecture on STL
    Former-commit-id: 5a1eae5cc1d038d2417c132501b14940cdd32f2c

--- Examples/src/Utilities/cxxversion.hpp
+++ Examples/src/Utilities/cxxversion.hpp
@@ -17,12 +17,18 @@ namespace Utilities
   //! True if you are using c++14
   constexpr bool is_cxx14()
   {
-    return bool(__cplusplus > 201103L);
+    return bool(__cplusplus == 201402L);
+  }
+  //! True if you are using c++17
+  constexpr bool is_cxx17()
+  {
+    return bool(__cplusplus == 201703L);
   }
   //! True if you are using c++98
+  // Only to avoid compilation errors
   constexpr bool is_cxx98()
   {
-    return bool(__cplusplus == 199711L);
+    return false;
   }
 #else
   //! True if you are using c++11
@@ -39,6 +45,11 @@ namespace Utilities
   inline bool is_cxx98()
   {
     return bool(__cplusplus == 199711L);
+  }
+  //! True if you are using c++17
+  inline bool is_cxx17()
+  {
+    return false;
   }
 #endif
 }
