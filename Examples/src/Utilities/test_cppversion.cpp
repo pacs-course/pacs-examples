702d506d3defed229e5ed93e9c1c81c852a74e04
formaggia <luca.formaggia@gmail.com>
2018-03-04 20:20:00 +0100

    corrected and added files for lecture on STL
    Former-commit-id: aa76921d147167adf41ea27a1a4b8e9bf5c54a09

--- Examples/src/Utilities/test_cppversion.cpp
+++ Examples/src/Utilities/test_cppversion.cpp
@@ -6,6 +6,7 @@ int main()
   std::string version;
   if (Utilities::is_cxx11()) version="C++11";
   else if (Utilities::is_cxx14()) version="C++14";
+  else if (Utilities::is_cxx17()) version="C++17";
   else if (Utilities::is_cxx98()) version="C++98";
   else version="UNKNOWN";
 
