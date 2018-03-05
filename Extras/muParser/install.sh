30bae9ab660ce0580fb5894796c87d0a27833ba5
formaggia <luca.formaggia@gmail.com>
2018-03-05 18:56:32 +0100

    deleted old version of muparser
    Former-commit-id: 38116d0c7e092c0af2b70c74038faf5d2e070f8e

--- Extras/muParser/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make distclean
-./configure --enable-shared=yes --prefix=`pwd`/../../Examples/
-make
-make install
-make distclean
-./configure --enable-shared=no --prefix=`pwd`/../../Examples/
-make
-make install
