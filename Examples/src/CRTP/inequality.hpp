e4b25d6934ee2921c88dc689d749847a56dc60eb
formaggia <luca.formaggia@gmail.com>
2018-04-06 19:08:28 +0000

    added new examples of crtp

---
+++ Examples/src/CRTP/inequality.hpp
@@ -0,0 +1,18 @@
+#ifndef HH_INEQUALITY_HH
+#define HH_INEQUALITY_HH
+// Another interesting examples of use of CRTP (from Discovering Modern C++: An intensive Course...)
+// I want to be able to define directly the != operator for any class that defines the == operator.
+// I create a base template class that just do that by delegating the == operator to the derived class
+template <typename T>
+struct inequality
+{
+  bool operator !=(const T& that) const
+  {
+    return !(static_cast<const T&>(*this) == that);
+  }
+};
+
+// See point.hpp for a possible use
+
+
+#endif
