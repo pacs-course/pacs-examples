88418f7536505779ff5b55204742a1ddc7ec1009
formaggia <luca.formaggia@gmail.com>
2018-04-18 07:44:48 +0000

    minor modifications
    Former-commit-id: 570a2445ea4666a24b5dac27dce6112a84822a6c

--- Examples/src/ForwardDeclaration/A.hpp
+++ Examples/src/ForwardDeclaration/A.hpp
@@ -3,11 +3,17 @@
 // I need a forward declaration
 // I cannot include B.hh since class B contains an member
 // of type A
-class B;
+
+class B; // Try to take it out
+// This is not good, try it
+// #include "B.hpp" 
 //! A class with a method that takes a reference to B
 class A{
 public:
-  //! Here the compiler needs only an incomplete (forward) declaration 
+  //! Here the compiler needs only an incomplete (forward) declaration
+  //! Beware: Cannot have definition here! In the definition I use
+  //! an object of type B and in this case the compiler needs a full
+  //! declaration!
   double f(B &);
 };
 #endif
