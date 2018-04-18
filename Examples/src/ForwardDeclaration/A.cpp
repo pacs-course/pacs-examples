9a4bf795d69b7b5661c84bcd7a22f5e4656fad5c
formaggia <luca.formaggia@gmail.com>
2018-04-18 06:43:43 +0000

    New example on forward declarations and some minor fixes

---
+++ Examples/src/ForwardDeclaration/A.cpp
@@ -0,0 +1,11 @@
+#include "A.hpp"
+// Here I need the full definition of B
+// since in the definition of the function I use
+// an object of type B, so the compiler must have
+// the full declaration visible.
+#include "B.hpp"
+double A::f(B & b)
+{
+  b.j+=2;
+  return b.x;
+}
