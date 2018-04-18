5315a5b46a97effda9e4859e6856ba98ecc7c8c9
formaggia <luca.formaggia@gmail.com>
2018-04-18 07:44:48 +0000

    minor modifications
    Former-commit-id: 570a2445ea4666a24b5dac27dce6112a84822a6c

--- Examples/src/ForwardDeclaration/B.hpp
+++ Examples/src/ForwardDeclaration/B.hpp
@@ -4,11 +4,15 @@
 // A since it stores an object of type A so I need to include
 // the corresponding header
 #include "A.hpp"
+//class A; // try just a forward declaration (comment #include A.hpp)
 struct B
 {
   int j{0};
   double x{3.15};
-  double funny(){return M_a.f(*this);} 
+  //! Pure declaration. This function uses M_a, of type A, but here I
+  //! could have put an in-class definition since the complete
+  //! definition of A is visible to the compiler.
+  double funny();
 private:
   A M_a;
 };
