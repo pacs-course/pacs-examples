7b813f2ca9151013fac3c1e01508ee8489d11229
formaggia <luca.formaggia@gmail.com>
2018-06-10 13:14:38 +0200

    added example of enable_shared

---
+++ Examples/src/SmartPointers/enable_shared.hpp
@@ -0,0 +1,14 @@
+#ifndef HH_ENABLE_SHARED_HH
+#define HH_ENABLE_SHARED_HH
+#include <memory>
+//! an example of use of shared_from_this
+//! Note the CRTP technique!
+class Try: public std::enable_shared_from_this<Try>
+{
+public:
+  double get ()const {return a;}
+  void  set(const double & b){a=b;}
+private:
+  double a={0};
+};
+#endif
