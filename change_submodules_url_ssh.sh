c62cfbbe8cd396ced8c94bbf3eb55ad0fb12331c
formaggia <luca.formaggia@gmail.com>
2019-01-13 21:38:23 +0100

    fixed gitmodules
    Former-commit-id: 360d4c779dc2b3ba7ceae139eb42fe0f4839ff26

--- change_submodules_url_ssh.sh
+++ change_submodules_url_ssh.sh
@@ -1,6 +1,8 @@
 git config --file=.gitmodules submodule.Examples/src/LinearAlgebra/CppNumericalSolvers.url git@github.com:lformaggia/CppNumericalSolvers.git
 git config --file=.gitmodules submodule.Examples/src/LinearAlgebra/spectra.url git@github.com:lformaggia/spectra.git
 git config --file=.gitmodules submodule.Examples/src/LinearAlgebra/redsvd-h.url git@github.com:lformaggia/redsvd-h.git
+git config --file=.gitmodules submodule.Extras/muparser.url git@github.com:lformaggia/muparser.git
+git config --file=.gitmodules submodule.Extras/muparserx.url git@github.com:lformaggia/muparserx.git
 git submodule sync
-git submodule update --init --recursive --remote --merge
+git submodule update --recursive --remote
 
