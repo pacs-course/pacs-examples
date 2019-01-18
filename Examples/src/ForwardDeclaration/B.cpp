f32b65b4202fd4371ede9156e584a1fb26b02a85
formaggia <luca.formaggia@gmail.com>
2019-01-18 12:33:39 +0100

    Example on forward declaration and module branch specification
    Former-commit-id: 0897f347f1e7672adc1c0c59f404d2db1d52b149

---
+++ Examples/src/ForwardDeclaration/B.cpp
@@ -0,0 +1,7 @@
+#include "B.hpp"
+//! this definition could have ben done in class, since it is very
+//! short and can be easily inlined, but I put it in a cpp file just
+//! to show that the problem of forward declaration not being possible
+//! is not related to the definition of the method.
+double
+B::funny(){return M_a.f(*this);} 
