b132eb24caff09fb475d03449bf252c2d6d3556b
formaggia <luca.formaggia@gmail.com>
2019-01-23 10:42:59 +0100

    added utility to load useful modules
    Former-commit-id: a31d0dc0025bdfc9e2aa134633cc2a252f19f9a6

---
+++ load_modules.sh
@@ -0,0 +1,11 @@
+# set up for modules. This file should be included using
+# source load_modules.sh
+# if you want to use the gcc toolchain + main utilities used in the examples
+source /u/sw/etc/bash.bashrc
+module load gcc-glibc/7
+module load superlu
+module load eigen
+module load openblas
+module load suitesparse
+# for the example on file i/o
+# module load hdf5 
