e3302cd810f84b64ea80a3c416f9ef6e6d16cc7d
formaggia <luca.formaggia@gmail.com>
2019-01-23 10:42:31 +0100

    Added a new example
    Former-commit-id: b0a7081cc40cbc193427fb7bc7da4a45f86b2067

---
+++ Examples/src/Polynomials/monomials.hpp
@@ -0,0 +1,44 @@
+#ifndef HH__MONOMIALS__HH
+#define HH__MONOMIALS__HH
+#include<type_traits>
+namespace LinearAlgebra
+{
+  namespace implementation
+  {
+    //! Implementation using constexpr functions
+    /*!  This implementation works also if the argument x is not a
+      integral constant.  The optimization is up to the compiler since
+      constexpr is only a "suggestion" to the compiler that the
+      expression may be optimized if the argument is a literal or a
+      constant value.
+      
+      Note the use of integral_constant to select the correct overloaded
+      function via tag dispatching.
+    */
+    // In c++11 I can use std::integral_constant
+    template<int N>
+    using IntToType=std::integral_constant<unsigned int,N>;
+    
+    //! Overload for N=0
+    template<class R>
+    constexpr R monomial(const R x, IntToType<0>)
+    {
+      return 1;
+    }
+
+    template<unsigned int N, class R>
+    constexpr R monomial(const R x, IntToType<N>)
+    {
+      return monomial(x, IntToType<N-1>()) * x;
+    }
+    
+  }  
+  //! the actual function
+  
+  template<unsigned int N, class R>
+  constexpr R monomial(const R x)
+  {
+    return implementation::monomial(x, implementation::IntToType<N>{} );
+  }
+}
+#endif
