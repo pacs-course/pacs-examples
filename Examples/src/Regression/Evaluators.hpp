3166dae809feb2c53d8557c097bb8a18711424f5
formaggia <luca.formaggia@gmail.com>
2019-01-27 15:28:34 +0100

    Added a new example
    Former-commit-id: a58aa6d6fb238454781db4e03452765c949f74ea

---
+++ Examples/src/Regression/Evaluators.hpp
@@ -0,0 +1,81 @@
+/*
+ * Evaluators.hpp
+ *
+ *  Created on: Jan 25, 2019
+ *      Author: forma
+ */
+
+#ifndef SRC_REGRESSION_EVALUATORS_HPP_
+#define SRC_REGRESSION_EVALUATORS_HPP_
+#include "RegressionTraits.hpp"
+#include "ModelBasis.hpp"
+#include <utility>
+namespace LinearAlgebra
+{
+  //! This class implements the evaluation of a model
+  /*
+   *  This version assumes that the model is a linear combination
+   *  of basis function.
+   */
+  template <class ModelBasis>
+  class PolynomialRegressionEvaluator
+  {
+  public:
+    using Parameters=LinearAlgebra::RegressionTraitsDefault::Parameters;
+    using Vector=LinearAlgebra::RegressionTraitsDefault::Vector;
+    //! The constructor that takes a well formed ModelBasis object
+    PolynomialRegressionEvaluator(ModelBasis const & modelbase):M_basis(modelbase){}
+    //! Maybe we want to move
+    PolynomialRegressionEvaluator(ModelBasis && modelbase):M_basis(std::move(modelbase)){}
+
+    //! Default constuctor
+    PolynomialRegressionEvaluator()=default;
+
+    //! evaluate at point x
+    /*!
+     * They must however comply with some rule
+     * @param x The evaluation point
+     * @param parameters, an object with the method dot()
+     * @note the size of parameters must match the number of basis functions in ModelBasis
+     */
+    double eval(double const & x, Parameters const & parameters) const
+    {
+      // dot is a method of Eigen vectors so model.eval() and parameters should be Eigen vectors
+      return parameters.dot(M_basis.eval(x));
+    }
+    //! Evaluates derivative w.r.t. x
+    /*!
+     * Not really necessary for our purposes. Only for completeness
+     */
+    double evalDerx(double const & x, Parameters const & parameters) const
+     {
+       // dot is a method of Eigen vectors so model.eval() and parameters should be Eigen vectors
+       return parameters.dot(M_basis.derivatives(x));
+     }
+    //! Derivative wrt the parameters
+    /*!
+     * @note Only required if we use a gradient scheme to compute the optimal parameters
+     */
+    Vector evalDerPar(double const & x, Parameters const & parameters) const
+     {
+       return M_basis.eval(x);;
+     }
+    //! Get the basis functions
+    ModelBasis getModelBasis() const {return M_basis;}
+    //! Get model basis so that they can be modified
+    /*!
+     *  This is a different way of making a setter!
+     *  I relies on function overloading.
+     */
+    ModelBasis & getModelBasis(){return M_basis;}
+
+    //! The capacity of the underlying model
+    std::size_t size()const {return M_basis.size();}
+  private:
+    ModelBasis M_basis;
+  };
+}
+
+
+
+#endif /* SRC_REGRESSION_EVALUATORS_HPP_ */
---
+++ Examples/src/Regression/ModelBasis.cpp
@@ -0,0 +1,68 @@
+/*
+ * ModelBasis.cpp
+ *
+ *  Created on: Jan 24, 2019
+ *      Author: forma
+ */
+#include "ModelBasis.hpp"
+
+//! this is used only internally so I use a special namespace
+/*!
+ *  I could have used an unnamed namespace
+ */
+namespace internal
+{
+  //! For standard monomial basis
+  inline double POW(const double & x, const unsigned int N)
+    {
+      return (N==0u) ? 1.0: x*POW(x,N-1);
+    }
+  //! Derivative of \f$ x^N \f$
+  inline double POWDer(const double &x, const unsigned int N)
+  {
+    return static_cast<double>(N)*POW(x,N-1);
+  }
+}
+
+LinearAlgebra::PolynomialMonomialBasisFunction::PolynomialMonomialBasisFunction (
+    std::size_t n)
+{
+  this->setFunctions(n);
+}
+
+LinearAlgebra::PolynomialMonomialBasisFunction::Vector
+LinearAlgebra::PolynomialMonomialBasisFunction::eval (const double& x) const
+{
+  Vector a;
+  a.resize(this->size());
+  size_t i=0;
+  // fill vector with values
+  for (auto f:this->M_basisFunctions) a(i++,0)=f(x);
+  return a;
+}
+
+void
+LinearAlgebra::PolynomialMonomialBasisFunction::setFunctions (std::size_t n)
+{
+  this->M_basisFunctions.resize(n+1);
+  this->M_derivatives.resize(n+1);
+
+  for  (std::size_t i=0; i<=n;++i)
+    {
+      this->M_basisFunctions[i] = [i](double const & x){return internal::POW(x,i);};
+      this->M_derivatives[i]    = [i](double const & x){return internal::POWDer(x,i);};
+    }
+}
+
+// @todo There is a duplication of code with eval. It can be done better.
+LinearAlgebra::PolynomialMonomialBasisFunction::Vector
+LinearAlgebra::PolynomialMonomialBasisFunction::derivatives (
+    const double& x) const
+{
+  Vector a;
+  a.resize(this->size());
+  size_t i=0;
+  // fill vector with values
+  for (auto f:this->M_derivatives) a(i++,0)=f(x);
+  return a;
+}
