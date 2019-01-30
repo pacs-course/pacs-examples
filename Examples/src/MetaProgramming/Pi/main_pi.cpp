4661b7e9e89fba7d1d2496089c6e2bc11692fe10
formaggia <luca.formaggia@gmail.com>
2019-01-30 18:51:20 +0000

    added a new metaprogramming example: computing pi
    Former-commit-id: 02a688b43cf22a15f5d654a4d7dc38a92fddd846

---
+++ Examples/src/MetaProgramming/Pi/main_pi.cpp
@@ -0,0 +1,13 @@
+#include "Pi.hpp"
+#include <iostream>
+#include <limits>
+int main()
+{
+  using namespace MathConstants;
+  std::cout.precision(std::numeric_limits<float>::digits10);
+  std::cout << "pi (float): " << Pi_f << std::endl;
+  std::cout.precision(std::numeric_limits<double>::digits10);
+  std::cout << "pi (double): " << Pi << std::endl;
+  std::cout.precision(std::numeric_limits<long double>::digits10);
+  std::cout << "pi (long double): " << Pi_l << std::endl;
+}
