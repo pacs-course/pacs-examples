da06b41c8ba88c98531542340df482c09d07015d
formaggia <luca.formaggia@gmail.com>
2019-02-04 20:09:26 +0100

    bettered example
    Former-commit-id: 05897e851779fb33a0a403df0a7b459d99343915

--- Examples/src/STL/SetEdge/main.cpp
+++ Examples/src/STL/SetEdge/main.cpp
@@ -2,6 +2,8 @@
 #include <set>
 #include <iostream>
 #include <vector>
+#include <algorithm> // for set_union
+#include <iterator>  // for inserter
 int main()
 {
   std::vector<Edge> myedges;
@@ -21,4 +23,26 @@ int main()
     {
       std::cout<<i[0]<<" "<<i[1]<<std::endl;
     }
+// an example of algorithm
+  std::set<Edge,yesOrient> newEdges={{1,2},{7,9}};
+  std::set<Edge,yesOrient> allEdges;
+// Note the use of inserter, it is necessary!
+// It is also necessary to give the object containing the
+// comparison operator to set_union. 
+// It is not required if you have specialised
+// less<T> or overloaded operator < for Edges.
+//
+  std::set_union(oEdges.begin(),oEdges.end(),
+                 newEdges.begin(),newEdges.end(),
+                 std::inserter(allEdges,allEdges.begin()),
+                 yesOrient()); // here the comparison object
+  std::cout<<" all edges after union (with orient.)"<<std::endl;
+  for (auto i : allEdges)
+    {
+      std::cout<<i[0]<<" "<<i[1]<<std::endl;
+    }
+
+
+
+
 }
