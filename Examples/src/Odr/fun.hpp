947ce12367ca8f0d6a5bfd8cfdc445c9b12f20ba
formaggia <luca.formaggia@gmail.com>
2019-02-04 21:20:13 +0100

    an example to one definition rule
    Former-commit-id: 1022c5f3072c11b482f69018ea429a8dce7d742e

---
+++ Examples/src/Odr/fun.hpp
@@ -0,0 +1,5 @@
+#ifndef H_FUN_H
+#define H_FUN_H
+#include "three.hpp"
+double fun(double const & x);
+#endif
---
+++ Examples/src/Odr/fun2.hpp
@@ -0,0 +1,5 @@
+#ifndef H_FUN2_H
+#define H_FUN2_H
+#include "newThree.hpp" //not necessary but better
+double fun2(double const & x);
+#endif
