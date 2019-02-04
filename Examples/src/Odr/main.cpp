4f998d1519932f82123be04479c4826321d3b13e
formaggia <luca.formaggia@gmail.com>
2019-02-04 21:20:13 +0100

    an example to one definition rule

---
+++ Examples/src/Odr/main.cpp
@@ -0,0 +1,11 @@
+#include "newThree.hpp"
+#include "fun2.hpp"
+#include "fun.hpp"
+#include <iostream>
+int main()
+{
+  //  double y=three(4.0);
+  double y=fun2(5.0);
+  double z=fun(5.0);
+  std::cout<<y<<" "<<z<<std::endl;
+}
