6d306f64426f0e57bce3f3263d2fc5823071674f
formaggia <luca.formaggia@gmail.com>
2019-02-06 09:32:19 +0100

    Simplified the example on linkage
    Former-commit-id: 502e91df9463175c355ac0794c28187a1ea6cbb6

--- Examples/src/Linkage/globals.hpp
+++ Examples/src/Linkage/globals.hpp
@@ -1,26 +1,31 @@
 #ifndef HH_GLOBALS_HH
 #define HH_GLOBALS_HH
 //! \file globals.hpp Defines use of globals with external linkage
-//! A global varibale with external linkage. Only declarated!
+//! A global variabale with external linkage. Only declarated!
 /*!
-  Must be defined elsewhere.
+  Must be defined elsewhere. It would have been better to create a namespace
+  (see below)
  */
-#ifndef NOGLOBAL
 extern int globalValue;
-#endif
-//! A global stuct that may represent global parameters of a program
+//! Declaration of a global stuct that may represent global parameters of a program
 /*!
-  Declaration 
+    It is a good practice to use namespaces. In this case the global variable
+    is a namespace variable.
  */
-struct GlobalParameters
+namespace Globals
 {
-  bool negate;
-  double g2;
-};
+  struct GlobalParameters
+  {
+    bool negate;
+    double g2;
+  };
 
-//! A variable with external linkage only declared!
-/*!
-  Must be defined elsewhere.
- */
-extern GlobalParameters globalParameters;
+  //! A variable with external linkage only declared!
+  /*!  Must be defined elsewhere, typically in the correspining source
+    file.  This variable will be available to all translation units
+    including this file (and linking with the source). Maybe you want to make
+    GlobalParameters a Singleton, we will discuss that in a specific lecture.
+  */
+  extern GlobalParameters globalParameters;
+}// end namespace Globals
 #endif
--- Examples/src/Linkage/locals2.cpp
+++
@@ -1,7 +0,0 @@
-#include "globals.hpp"
-#include "locals2.hpp"
-#ifdef  NOGLOBAL
-static int globalValue;
-#endif
-//! If you wish you can use :: to specify the global scope
-void zeroGlobal(){ ::globalValue=0;}
