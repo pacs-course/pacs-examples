6aa6fa652ae0b00cdec43e4007d45d1d6c9fc9fc
formaggia <luca.formaggia@gmail.com>
2019-02-06 09:32:19 +0100

    Simplified the example on linkage
    Former-commit-id: 6572c54d0dcb749e4c05d47ac81b0a3642f9bd09

--- Examples/src/Linkage/locals.hpp
+++ Examples/src/Linkage/locals.hpp
@@ -2,4 +2,6 @@
 #define HH_LOCAL__HH
 //! Declaration of a function (external linkage by default)
 double fun(double);
+//! Declaration of a function that zeroes a (maybe) global variable
+void zeroGlobal();
 #endif
