4ae0d1e579cb66152adc08bd4777ca5e36c73979
formaggia <luca.formaggia@gmail.com>
2019-02-09 20:34:29 +0100

    added example on integer overflow/underflow

---
+++ Examples/src/HeatExchange/plot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A script that launches gnuplot on the file result.dat
+#
+# you can also launch gnuplot and type the command at the gnuplot prompt
+#
+
+gnuplot<<EOF
+plot "result.dat" u 1:2 w lp lw 2 title "uh", "result.dat" u 1:3 w l lw 2title "uex"
+pause -1
+EOF
---
+++ Examples/src/IntegerOverflow/mainItOver.cpp
@@ -0,0 +1,19 @@
+#include <limits>
+#include <iostream>
+//Integer overflow produce unwanted results. And they cannot be detected!
+// Normally is not a problem... but beware!
+
+int main()
+{
+  int imax=std::numeric_limits<int>::max();
+  int imin=std::numeric_limits<int>::min();
+  std::cout<<"Max int + 1= "<<imax +1<<std::endl;
+  std::cout<<"Min int - 1= "<<imin -1<<std::endl;
+  // also underflow of unsigned int give unexpected
+  // results, and it is undetected!
+  unsigned int uintmin=0;
+  // Now I subtract 1
+  uintmin -=1;
+  std::cout<<"Unsigned underflow= "<<uintmin<<std::endl;
+  
+}
