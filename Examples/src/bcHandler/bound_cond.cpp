d1876394d0dc83d70acae9bc6fbc85a89237ca3e
formaggia <luca.formaggia@gmail.com>
2019-02-10 09:05:23 +0100

    Fixed some examples
    Former-commit-id: 58d96760c1e5b25d16fda05a0f5f33e8636474db

--- Examples/src/bcHandler/bound_cond.cpp
+++ Examples/src/bcHandler/bound_cond.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include "bound_cond.hpp"
+#include <map>
 namespace FEM{
   
 
@@ -9,10 +10,21 @@ namespace FEM{
   //! Defined through a lambda (C++11 only)
   BCFun onefun([](double const, double const * ){return 1.0;});
   
-  BCName intToBCName(int i){
-    std::ostringstream tmp;
-    tmp<<i;
-    return tmp.str();
+  BCType stringToBCType(BCName const & s){
+    // an use of static function variables
+    // Maybe would be better to make it a method of bcBase and store the map as a
+    // member of the class
+    static std::map<BCName,BCType> dict {
+      {"Dirichlet",Dirichlet},
+      {"Neumann", Neumann},
+      {"Robin", Robin},
+      {"Other",Other},
+    };
+    auto res = dict.find(s);
+    if  (res!=dict.end())
+      return res->second;
+    else
+      return None;
   }
   
   double BCBase::apply(double const t, double const * coord) const {
