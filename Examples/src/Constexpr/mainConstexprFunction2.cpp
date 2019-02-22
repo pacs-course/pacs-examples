3f5e97f0dc2fbfdf6d423930e81f1c36355bc03a
formaggia <luca.formaggia@gmail.com>
2019-02-22 08:18:49 +0100

    bettered example on conxtexpr
    Former-commit-id: 984b208da5f57423d161b0dd3c7adf4f32fbef21

--- Examples/src/Constexpr/mainConstexprFunction2.cpp
+++ Examples/src/Constexpr/mainConstexprFunction2.cpp
@@ -4,8 +4,9 @@ int main()
 {
   double a;
   std::cin >>a;
-  // Here the compilar cannot guess the value of a
-  // function will be inlined but not resolved compile time
+  // Here the compiler cannot guess the value of a function will be
+  // inlined (since it is very simple) but not resolved at compile
+  // time
   double x=cubeconst(a);
 }
 
