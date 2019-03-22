da2f993211616d7e5ae5e4121153e6dc733006f5
Vagrant Default User <vagrant@stretch.localdomain>
2019-03-22 14:02:15 +0000

    LU factorization example

--- Exercises/2019/03-cachealignment/matrix-0.4/matrix.cpp
+++ Exercises/2019/03-cachealignment/matrix-0.4/matrix.cpp
@@ -3,6 +3,16 @@
 #include <algorithm>
 #include <ctime>
 
+matrix
+matrix::transpose () const
+{
+  matrix retval (get_cols (), get_rows ());
+  unsigned int i, j;
+  for (j = 0; j < retval.get_cols (); ++j)
+    for (i = 0; i < retval.get_rows (); ++i)
+      retval (i, j) = const_index (j, i);
+  return (retval);
+}
 
 #if defined (USE_DGEMM)
 #define dgemm dgemm_
@@ -19,16 +29,6 @@ extern "C"
 }
 #endif
 
-matrix
-matrix::transpose () const
-{
-  matrix retval (get_cols (), get_rows ());
-  unsigned int i, j;
-  for (j = 0; j < retval.get_cols (); ++j)
-    for (i = 0; i < retval.get_rows (); ++i)
-      retval (i, j) = const_index (j, i);
-  return (retval);
-}
 
 #if defined (MAKE_TMP_TRANSP)
 matrix
@@ -63,7 +63,7 @@ operator* (const matrix& A, const matrix& B)
     eigen_retval (retval.get_data (), A.get_rows (), B.get_cols ());
 
   eigen_retval = eigen_A * eigen_B;
-    
+
   return (retval);
 }
 #elif defined (USE_DGEMM)
@@ -75,13 +75,13 @@ operator* (const matrix& A, const matrix& B)
   int N = B.get_cols ();
   int K = A.get_cols ();
   assert (K == B.get_rows ());
-  
+
   char ntr = 'n';
   double one = 1.0;
   double zero = 0.0;
 
   matrix retval (M, N);
-  
+
   dgemm (&ntr, &ntr, &M, &N, &K,
          &one, A.get_data (), &M,
          B.get_data (), &K, &zero,
@@ -109,14 +109,14 @@ matrix::solve (matrix &rhs)
 {
   unsigned int ii, jj, kk;
   double f;
-  
+
   // Factorize
   if (! factorized)
     { factorize (); std::cout << "factorize !" << std::endl; }
 
   double *b = rhs.get_data ();
   // Do Forward Substitution
-  std::cout << "fwdsub !" << std::endl; 
+  std::cout << "fwdsub !" << std::endl;
   for (ii = 0; ii < get_rows (); ++ii)
     {
       f = b[p[ii]];
@@ -126,16 +126,29 @@ matrix::solve (matrix &rhs)
     }
 
   // Do Backward Substitution
-  std::cout << "bwdsub !" << std::endl; 
+  std::cout << "bwdsub !" << std::endl;
   for (jj = 1; jj <= get_rows (); ++jj)
     {
       ii = get_rows () - jj;
       f = b[p[ii]];
       for (kk = ii+1; kk < get_cols (); ++kk)
           f -= index (p[ii], kk) * b[p[kk]];
-      b[p[ii]] = f / index (p[ii], ii); 
-    }    
+      b[p[ii]] = f / index (p[ii], ii);
+    }
 };
+
+#ifdef USE_LEVEL1
+#define DAXPY  daxpy_
+#define daxpy  daxpy_
+extern "C"
+{
+
+  void
+  daxpy (const int *N, const double *DA, const double *DX,
+         const int *INCX, double *DY, const int *INCY);
+
+}
+#endif
 
 void
 matrix::factorize ()
@@ -143,20 +156,20 @@ matrix::factorize ()
   p.resize (rows, 0);
   for (int ii = 0; ii < rows; ++ii)
     p[ii] = ii;
-    
+
   int m = this->get_rows ();
   int n = this->get_cols ();
   int ii, jj, kk;
   double pivot = 0.,
     maxpivot = 0.;
   int imaxpivot = 0;
-  
+
   assert (m == n);
   for (ii = 0; ii < m-1; ++ii)
     {
       maxpivot = index (p[ii], ii);
       imaxpivot = ii;
-      for (kk = ii+1; kk < m; ++kk)      
+      for (kk = ii+1; kk < m; ++kk)
         if (index (p[kk], ii) > maxpivot)
           {
             maxpivot = index (p[kk], ii);
@@ -164,17 +177,31 @@ matrix::factorize ()
           }
 
       if (imaxpivot != ii)
-        std::swap (p[ii], p[imaxpivot]);      
-      
-      pivot = index (p[ii], ii);      
+        std::swap (p[ii], p[imaxpivot]);
+
+      pivot = index (p[ii], ii);
       for (jj = ii+1; jj < m; ++jj)
         {
           index (p[jj],ii) = index (p[jj],ii) / pivot;
+
+#ifdef USE_LEVEL1
           
+          const int N = n - (ii + 1);
+          const double DA = - index (p[jj],ii);
+          const double *DX = get_data () + p[ii] + (ii+1) * m;
+          double *DY = get_data () + p[jj] + (ii+1) * m;
+          const int INCX = m;
+          const int INCY = m;
+          daxpy (&N, &DA, DX, &INCX, DY, &INCY);
+
+#else
+
           for (kk = ii+1; kk < n; ++kk)
             index (p[jj],kk) +=
               -index (p[ii],kk) * index (p[jj],ii);
-          
+
+#endif
+
         }
     }
   factorized = true;
---
+++ Exercises/2019/03-cachealignment/matrix-0.4/test_solve.h
@@ -0,0 +1,9 @@
+#ifndef HAVE_TEST_SOLVE_H
+#define HAVE_TEST_SOLVE_H
+
+#include "matrix.h"
+#include <iostream>
+
+constexpr int n = 10;
+
+#endif
