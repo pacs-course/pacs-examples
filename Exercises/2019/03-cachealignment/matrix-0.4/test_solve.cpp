d59de0f38ce316f1e68038f685bf268bd6a27d6f
Vagrant Default User <vagrant@stretch.localdomain>
2019-03-22 14:02:15 +0000

    LU factorization example
    Former-commit-id: da2f993211616d7e5ae5e4121153e6dc733006f5

---
+++ Exercises/2019/03-cachealignment/matrix-0.4/test_solve.cpp
@@ -0,0 +1,41 @@
+#include "test_solve.h"
+#include "test_matrix_mult.h"
+#include <iostream>
+
+int main ()
+{
+
+  matrix A (n);
+  matrix rhs (n, 1);
+
+  for (unsigned int i = 0; i < n; ++i)
+    {
+      rhs (i, 0) = 1.;
+      for (unsigned int j = 0; j < n; ++j)
+        {
+          if (j+1 == i)
+            A(i, j) = -1.;
+          else if (j == i+1)
+            A(i, j) = -1.;
+          else if (i == j)
+            A(i, j) = 2.;
+        }
+    }
+
+  matrix uh = rhs;
+  matrix LU = A;
+  
+  LU.solve (uh);
+  
+  matrix f = A * uh;
+  
+  for (unsigned int i = 0; i < n; ++i)
+    {
+      std::cout << "x (" << i+1 << ") = " << uh(i, 0) << ";"        
+                << "    y (" << i+1 << ") = " << f(i, 0) << ";"
+                << "    b (" << i+1 << ") = " << rhs(i, 0) << ";"
+                << std::endl;
+    }
+  return 0;
+}
+
