566d18027e36b6e626e07b5402f5a53f0bd1332c
formaggia <luca.formaggia@gmail.com>
2019-04-11 12:48:36 +0200

    cleaned up some examples
    Former-commit-id: 53e69d9978b82b65fc54d3f272c093a03c95a59c

--- Examples/src/CompositionWithVariadicTemplates/main.cpp
+++ Examples/src/CompositionWithVariadicTemplates/main.cpp
@@ -4,7 +4,10 @@
 #include <iostream>
 int main()
 {
+  // to save typing the full qualified names
   using namespace Identifier;
+  using namespace Geometry;
+  using namespace BoundaryConditions;
   
   using Point2dSimple = Point<double,2>;
   using Point3dSimple = Point<double,3>;
@@ -23,13 +26,13 @@ int main()
   auto res=dot(a,b);
   /// auto res2=dot(a,e); ERROR dimension differs!!!
   Id ident(5);
-  
+  // I am passing the constructor for the Id!
   Poin2dWithId pwi(ident);
   std::cout<<pwi.getId()<<std::endl;
   Poin2dWithId z=pwi;
   std::cout<<z.getId()<<std::endl;
   Poin2dWithId K(3);
   std::cout<<K.getId()<<std::endl;
-  
+
 }
 
