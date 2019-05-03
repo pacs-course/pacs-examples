b589c8156bab8a68b876d7080d02f04eadf5cdbe
formaggia <luca.formaggia@gmail.com>
2019-05-04 00:50:32 +0200

    corrected comments
    Former-commit-id: 2ddc8b917c900cec2b194cc312aa35539422f200

--- Examples/src/ReferenceWrapper/main_wrap.cpp
+++ Examples/src/ReferenceWrapper/main_wrap.cpp
@@ -8,15 +8,15 @@
 int main()
 {
   // This example is pratically useless. It is here only to show ref()
-  // In more practical situations, if you ave a function whose
-  // argument are passed by value, you may want to use ref() of cref()
+  // In more practical situations, if you have a function whose
+  // arguments are passed by value, you may want to use ref() of cref()
   // to save a copy. But beware you may change the value of the
-  // argument (unless you use cref(), but then if the function bchanges
-  // the value of the parameter you have a compiler error.
+  // argument unless you use cref(), then if the function changes
+  // the value of the argument you have a compiler error.
   
   double a=10;
   apply(timesThree,a);
-  // a has not chenged since it is passed by value!
+  // a has not changed since it is passed by value!
   std::cout<<"a="<<a<<std::endl;
   apply(timesThree,std::ref(a));
   // Now a has changed
