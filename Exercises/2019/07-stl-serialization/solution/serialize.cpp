4ab98c51bcf78690e5fe33a5e7c296f72dbe6145
Carlo de Falco <carlo.defalco@polimi.it>
2019-05-03 15:02:16 +0200

    add mpi example

--- Exercises/2019/07-stl-serialization/solution/serialize.cpp
+++ Exercises/2019/07-stl-serialization/solution/serialize.cpp
@@ -72,5 +72,34 @@ main (int argc, char **argv)
         std::cout << jj->first << " " << jj->second << std::endl;
     }
 
+  MPI_Init (&argc, &argv);
+  int rank, size;
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+
+  if (rank == 0)
+    {
+      std::vector<double> aa(10, 79.1234);  
+      std::vector<unsigned char> saa = serialize::write (a);
+      int n = sa.size ();
+      MPI_Send (&n, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+      MPI_Send (&(sa[0]), n, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+    }
+  else if (rank == 1)
+    {
+      std::vector<double> bb;
+      std::vector<unsigned char> sbb;
+      int n = 0;
+      MPI_Recv (&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      sbb.resize (n);
+      MPI_Recv (&(sbb[0]), n, MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      serialize::read (sbb, bb);
+
+      std::cout << "bb = " << std::endl;
+      for (int ii = 0; ii < bb.size (); ++ii)
+        std::cout << bb[ii] <<  std::endl;
+    }
+  MPI_Barrier (MPI_COMM_WORLD);
+  MPI_Finalize ();
   return 0;
 }
