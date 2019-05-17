be489a5f81632ffbdba52293b147face7163b630
Carlo de Falco <carlo.defalco@polimi.it>
2019-05-17 13:18:46 +0200

    refactor solution

---
+++ Exercises/2019/09-dld/07-dld-example/step3/concrete2.h
@@ -0,0 +1,17 @@
+
+#ifndef HAVE_CONCRETE2_H
+#define HAVE_CONCRETE2_H
+
+#include "abstract.h"
+
+class
+concrete2 : public abstract
+{
+
+public :
+  
+  double eval (double x);
+  ~concrete2 ();
+};
+
+#endif
