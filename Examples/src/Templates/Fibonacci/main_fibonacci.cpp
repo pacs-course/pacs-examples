75653f625d689b4065e4fbeace71abe1577639bd
formaggia <luca.formaggia@gmail.com>
2019-05-20 10:45:10 +0200

    Bettered some examples on template.
    Beware that the constexpr version of fibonacci does not work with clang
    8.0.0 because it does not expand the function at compile time. It works
    with g++ 7.4.0 (you need optimization)
    
    
    Former-commit-id: 98d26cb3c8049ab492bc21391436bfbf5804f888

--- Examples/src/Templates/Fibonacci/main_fibonacci.cpp
+++ Examples/src/Templates/Fibonacci/main_fibonacci.cpp
@@ -1,9 +1,11 @@
 // template metaprogram for Fibonacci numbers
 #include "fibonacci.hpp"
 #include <iostream>
+#include "chrono.hpp"
 
 
 int main() {
+  Timings::Chrono clock;
   const int f3 = fib<3>::value;
   const int f9 = fib<9>::value;
   const int f10 =fib<10>::value;
@@ -14,15 +16,20 @@ int main() {
   std::cout<<" Now with the constexpr function"<<std::endl;
   std::cout << " f3 = " << Fibonacci(3) << '\n';
   std::cout << " f9 = " << Fibonacci(9) << '\n';
-  std::cout << " f20= " << Fibonacci(20) << '\n';
+  clock.start();
+  std::cout << " f40= " << Fibonacci(40) << '\n';
+  clock.stop();
+  std::cout<<"Time to evaluate F(40) with constexpr argument:"<<clock<<std::endl;
   unsigned long l=999ul;
   while (true)
     {
       std::cout << "Give me the index of the Fibonacci number you want (0=exit)"<<std::endl;
       std::cin>>l;
       if(l==0ul)break;
+      clock.start();
       std::cout << " f"<<l<<"= " << Fibonacci(l) << '\n';
+      clock.stop();
+      std::cout<<"Time to evaluate F("<<l<<") with non constexpr argument:"<<clock<<std::endl;
     }
-  
   return 0;
 }
