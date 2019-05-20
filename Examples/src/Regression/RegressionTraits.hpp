da1034bcf9e722f58ac77ef13ba5b7bfb9e18e3a
formaggia <luca.formaggia@gmail.com>
2019-05-20 10:21:58 +0200

    Bettered examples on linear regression
    -Made traits real traits
    -Extended documentation
    
    
    Former-commit-id: d157cce52bb44fe06cf0c09a9583efc4fd93fc6b

--- Examples/src/Regression/RegressionTraits.hpp
+++ Examples/src/Regression/RegressionTraits.hpp
@@ -14,12 +14,19 @@
 
 namespace LinearAlgebra
 {
-  //! The basic type definition. The primary template uses Eigen classes
+  //! An enum that allow to distinguish different traits
   /*!
-   * /tparam T Used to select the appropriate trait
+   *  At the moment only EIGEN is implemented
    */
-  template<class T=void>
-  struct RegressionTraits
+  enum LinearAlgebraLibrary {EIGEN, ARMADILLO};
+  // ! The primary template.
+  template <LinearAlgebraLibrary>
+  struct
+  RegressionTraits{}; //!< Empty. I use only specializations!
+
+  //!The specialization for Eigen classes
+  template<>
+  struct RegressionTraits<EIGEN>
   {
     using Function = std::function<double (double const &)>;// @suppress("Symbol is not resolved") @suppress("Type cannot be resolved")
     using BasisFunctions = std::vector<Function>; // @suppress("Invalid template argument")
@@ -31,7 +38,7 @@ namespace LinearAlgebra
     using CostFunction=Function;
   };
   //! The default trait
-  using RegressionTraitsDefault=RegressionTraits<>;
+  using RegressionTraitsDefault=RegressionTraits<EIGEN>;
 }
 
 
