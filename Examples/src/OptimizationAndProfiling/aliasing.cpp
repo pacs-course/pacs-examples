7fd3c46f5909b90d268220f652fa22635a276af2
formaggia <luca.formaggia@gmail.com>
2019-05-30 10:32:57 +0200

    Corrected a nasty bug!
    Former-commit-id: 9ad46843747f98b4ed99af4c31e1c508a4ac566b

--- Examples/src/OptimizationAndProfiling/aliasing.cpp
+++ Examples/src/OptimizationAndProfiling/aliasing.cpp
@@ -1,14 +1,22 @@
 #include <vector>
 #include<iostream>
 typedef std::vector<double> Vet;
-// Does not work if you alias the last two arguments
+//! Sums to R the convolution of A and B
+//! Does not work if you alias the last two arguments
 void covol(Vet const & A, Vet const & B , Vet & R)
 {
   for (unsigned int i=0; i<A.size();++i)
     for(unsigned int k=B.size()-1; k!=0;--k)
       R[i]+=A[i]*B[k];
 }
-// This works correctly even if you alias the last two args
+//! Sums to R the convolution of A and B
+//! This works correctly even if you alias the fist and last args
+/*!
+  @note. This is just an example of the effect of argument aliasing
+  Indeed, this function still is not giving the correct results if
+  you alias the second and third argument. If you really want a alias
+  proof function you have to pass by value!
+ */
 void covol2(Vet const & A, Vet const & B , Vet & R)
 {
   for (unsigned int i=0; i<A.size();++i){
@@ -17,14 +25,37 @@ void covol2(Vet const & A, Vet const & B , Vet & R)
       R[i]+=tmp*B[k];
   }
 }
+//! Sums to R the convolution of A and B
+//! This version is argument alias proof! But uses more memory
+/*!
+  @note I could have not returned the new R with the return statement.
+  I just wanted to maintain the same signature as the previous functions.
+ */
+void covol3(Vet const  A, Vet const  B , Vet & R)
+{
+  Vet Rt=R; // copy content of R
+  for (unsigned int i=0; i<A.size();++i){
+    double tmp=A[i];
+    for(unsigned int k=B.size()-1; k!=0;-- k)
+      Rt[i]+=tmp*B[k];
+  }
+  R=Rt; // return the result
+}
+
+
+
 int main()
 {
-  Vet A(100,1.0);
-  Vet B(100,10.0);
-  Vet R(100,10.0);
-  covol(A,R,R);
+  Vet B(100,1.0);
+  Vet A(100,10.0);
+  Vet R=A;
+  covol(R,B,R);
   std::cout<<R[50]<<std::endl;
-  R.resize(100,0.0);
-  covol2(A,R,R);
+  R=A;
+  covol2(R,B,R);
   std::cout<<R[50]<<std::endl;
+  R=A;
+  covol3(R,B,R);
+  std::cout<<R[50]<<std::endl;
+
 }
