67c2a5e46ffc667229990aaf1ef095f4fac63593
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-02-18 13:42:53 +0000

    Update for mk2020.0

--- load_modules.sh
+++ load_modules.sh
@@ -1,8 +1,8 @@
 # set up for modules. This file should be included using
 # source load_modules.sh
 # if you want to use the gcc toolchain + main utilities used in the examples
-source /u/sw/etc/bash.bashrc
-module load gcc-glibc/7
+source /u/sw/etc/profile
+module load gcc-glibc/9
 module load superlu
 module load eigen
 module load openblas
