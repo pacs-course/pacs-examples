c25855d7c032bcd4b8f96a28204e0eb1b27f2d00
formaggia <luca.formaggia@gmail.com>
2020-03-06 23:56:52 +0100

    some fixes

--- Examples/src/STL/Optional/main_optional.cpp
+++ Examples/src/STL/Optional/main_optional.cpp
@@ -2,12 +2,21 @@
 #include<vector>
 #include<iostream>
 #include<algorithm>
-template<class C>
-void countValid(C const & v)
+
+//! function templates that counts valid optionals in a vector an prints the valid values 
+//!
+//! \tparam V a std::vector or std::array of optionals
+//! \param v the passed container
+//!
+template<class V>
+void countValid(V const & v)
 {
   int count{0};
   // A possible use of for_each
-  std::for_each(v.begin(),v.end(),[&count](typename C::value_type const & x){if (x) ++count;});
+  // I exploit the fact that std::vector and std::array contain a type
+  // set to the type of the contained objects and that an optional<T>
+  // contextually converts to true if set, to false if unset
+  std::for_each(v.begin(),v.end(),[&count](typename V::value_type const & x){if (x) ++count;});
   std::cout<<"The container has "<< count<<" valid elements:\n";
   for (auto & x: v)
     if(x) // valid
@@ -20,14 +29,14 @@ void countValid(C const & v)
 int main()
 {
   // A vector of 10 optionals
-  // The optional are default contructed and thus are empty
+  // The optional are default contructed and thus are empty (i.e. invalid)
   std::vector<std::optional<double>> v(10);
   
   v[0]=10.0;
   v[3]=30.0;
   countValid(v);
-  // another way of thesting if a value is set
-  // and to get the value: value() will throw an exception
+  // another way of thesting if a value is set, the method has_value,
+  // and to get the value: value() The latter throws an exception
   // if the value is not set, so it is safer than *
   if(v[2].has_value())
     std::cout<<"v[2] stores "<<v[2].value()<<std::endl;
