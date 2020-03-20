5864bb689462259942b3c3d33af9d80340c56d24
Pasquale Africa <pasquale.africa@libero.it>
2020-03-20 14:59:20 +0000

    Add solution
    Former-commit-id: 3367acec3cadf57c5306b34b9b438b89afc7b9fe

---
+++ Exercises/2020/02-compile/src-solution/compile_simple.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x
+
+g++ -c -I../include -I$mkEigenInc -fPIC Utilities.cpp
+g++ -shared -Wl,-soname,liblinearalgebra.so Utilities.o -o liblinearalgebra.so
+g++ -c -I../include -I$mkEigenInc -I$mkSuitesparseInc test.cpp
+g++ -L$mkSuitesparseLib -lumfpack -L$PWD -llinearalgebra -Wl,-rpath=$PWD test.o -o test
+
+set +x
