4b975afc4e3647782863d9634683203eca99e8c4
formaggia <luca.formaggia@gmail.com>
2020-03-24 08:44:07 +0100

    fixed some examples

--- Examples/src/Derivatives/Derivatives.hpp
+++ Examples/src/Derivatives/Derivatives.hpp
@@ -18,14 +18,30 @@ namespace apsc
   }
 
   //! Computes the Nth derivative of a function by finite difference
+  /*!
+    \tparam N the order of the derivative
+    \tparam F the callable object of signature T (T const &)
+    \tparam T The argument and return type of the callable object
+    \tparam DT The type of differencing: either DifferenceType::FORWARD or =DifferenceType::BACKWARD
+   */
   template<unsigned N, typename F, typename T=double,
            typename DT=DifferenceType::FORWARD>
   class NthDerivative
   {
   public:
+    //! the previous derivative, the type is switched.
     using PreviousDerivative=NthDerivative<N-1,F,T,typename DT::otherType>;
     //! Constructor
+    /*!
+      \param f The function (callable object)
+      \param h The spacing to be used in the formula
+     */
     NthDerivative(const F& f, const T & h):pDerivative{f,h}, h{h}{}
+    //! The call operator that computes the derivative
+    /*!
+      \param x The point where the derivative is computed
+      \return The derivative value
+     */
     T operator()(const T& x) const
     {
       // from C++17 constexpr if statement
@@ -39,7 +55,7 @@ namespace apsc
     T h;
   };
   
-
+  //! Specialization for first derivative
  template<typename F, typename T, typename DT>
  class NthDerivative<1u,F,T,DT>
   {
@@ -69,9 +85,10 @@ namespace apsc
   public:
     //! Constructor
     NthDerivative(const F& f, const T & h):pDerivative{f}, h{h}{}
+    //! The call operator returns the value of the function
     T operator()(const T& x) const
     {
-      return pDerivative(x));
+      return pDerivative(x);
     }
   private:
     F pDerivative;
@@ -85,6 +102,7 @@ namespace apsc
    *  auto f =[](const double & x){return x*std::sin(x);};
    *  double h =0.1;
    *  auto d = apsc::makeForwardDerivative<3>(f,h);
+   *  auto value = d(3.0);// 3rd derivative at 3.0
    *  /endcode
    *
    * /param f a callable function with the rigth signature
