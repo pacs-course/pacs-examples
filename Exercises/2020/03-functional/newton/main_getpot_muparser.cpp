a3c8b9f8e3c23b0108f8bba80b6c74297ae822c0
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-03-27 12:10:04 +0000

    Add Newton prototype
    Former-commit-id: 442c081babcc4cdfdf8f2eb657bcc42d2be5de5c

--- Exercises/2020/03-functional/newton/main_getpot_muparser.cpp
+++ Exercises/2020/03-functional/newton/main_getpot_muparser.cpp
@@ -3,12 +3,5 @@
 int
 main(int argc, char **argv)
 {
-  //newton_solver solver(fun, prime);
-  solver.solve(0.0);
-
-  std::cout << "x    = " << solver.get_result() << std::endl;
-  std::cout << "r    = " << solver.get_residual() << std::endl;
-  std::cout << "iter = " << solver.get_iter() << std::endl;
-
   return 0;
 }
