cf41b2e486f617b369d7e8218efb24beadb785dd
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-03-27 12:10:04 +0000

    Add Newton prototype

---
+++ Exercises/2020/03-functional/newton/main_getpot_muparser.cpp
@@ -0,0 +1,14 @@
+#include "newton.h"
+
+int
+main(int argc, char **argv)
+{
+  //newton_solver solver(fun, prime);
+  solver.solve(0.0);
+
+  std::cout << "x    = " << solver.get_result() << std::endl;
+  std::cout << "r    = " << solver.get_residual() << std::endl;
+  std::cout << "iter = " << solver.get_iter() << std::endl;
+
+  return 0;
+}
