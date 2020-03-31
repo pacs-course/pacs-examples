3aa0d0040fa74b53a8efbe6723b45142c2bb824f
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-03-31 17:43:09 +0000

    Improve lab 04 solution

--- Exercises/2020/04-covid-19/src-solution/main.cpp
+++ Exercises/2020/04-covid-19/src-solution/main.cpp
@@ -1,9 +1,14 @@
 #include "Contagion.hpp"
 
+#include <memory>
+
 int
 main(int argc, char **argv)
 {
-  Contagion contagion;
+  std::shared_ptr<const ContagionParameters> params_contagion =
+    std::make_shared<const ContagionParameters>();
+
+  Contagion contagion(params_contagion);
   contagion.simulate();
 
   return 0;
