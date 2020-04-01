28bf1c724ab8340f0dc73405508dbb1f68bfed8a
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-04-01 10:29:18 +0000

    Add peak computation

--- Exercises/2020/04-contagion/src-solution/main.cpp
+++ Exercises/2020/04-contagion/src-solution/main.cpp
@@ -1,15 +1,10 @@
 #include "Contagion.hpp"
 
-#include <memory>
-
 int
 main(int argc, char **argv)
 {
-  std::shared_ptr<const ContagionParameters> params_contagion =
-    std::make_shared<const ContagionParameters>("params.pot");
-
-  Contagion contagion(params_contagion);
-  contagion.simulate();
+  Contagion contagion("params.pot");
+  contagion.run();
 
   return 0;
 }
