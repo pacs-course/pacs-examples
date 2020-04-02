503462f91c5ff5b68e968cff3e175093d007ca98
formaggia <luca.formaggia@gmail.com>
2020-04-02 11:47:37 +0200

    Added examples on variadic and bettered Rational
    Former-commit-id: 7f0f4cc59a03a5744bdb6f9e79a285494cc40d06

---
+++ Examples/src/Templates/VariadicTemplates/folding.hpp
@@ -0,0 +1,37 @@
+/*
+ * folding.hpp
+ *
+ *  Created on: Mar 30, 2020
+ *      Author: forma
+ */
+
+#ifndef EXAMPLES_SRC_TEMPLATES_VARIADICTEMPLATES_FOLDING_HPP_
+#define EXAMPLES_SRC_TEMPLATES_VARIADICTEMPLATES_FOLDING_HPP_
+#include<iostream>
+//! @file  Examples of fold expressions
+//!
+namespace apsc
+{
+  //! Checks that all arguments are true
+  template<typename... Args>
+  bool allTrue(Args const &... args)
+  {
+    return (... && args) ;
+  }
+
+  //! Make a sum of arguments (if it makes sense
+  template<typename... Args>
+   auto allSum(Args const &... args)
+  {
+    return (args +...);
+  }
+
+  //! Make product of arguments (if it makes sense
+    template<typename... Args>
+     auto allProd(Args const &... args)
+    {
+      return (args *...);
+    }
+}// end namespace apsc
+
+#endif /* EXAMPLES_SRC_TEMPLATES_VARIADICTEMPLATES_FOLDING_HPP_ */
