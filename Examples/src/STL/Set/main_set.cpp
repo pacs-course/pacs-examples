069b9ad8de0ec41e1a415efae7bb2bbbf39f5e4b
formaggia <luca.formaggia@gmail.com>
2020-04-06 17:19:48 +0200

    fixed example on set
    Former-commit-id: 3c9eb53d2bae58d2abd339f8833997ae8fc89ca3

--- Examples/src/STL/Set/main_set.cpp
+++ Examples/src/STL/Set/main_set.cpp
@@ -20,7 +20,8 @@
  */
 struct comparison
 {
-  bool operator()(int const & a, int const & b)
+  //! Must be a non-static const member! 
+  bool operator()(int const & a, int const & b) const
   {return a>b;}
 };
 
@@ -29,18 +30,24 @@ int main()
   using namespace std;
   // I create my comparison operator using a lambda
   auto comp=[](int const & a,int const & b){return a>b;};
-  // I have to pass it explicitly since I need to give the type of
-  // the comparison operator. So I have to use decltype
+  // I need to give the type of the comparison operator, so I have to use decltype
+  // The comparison operator has to be passed also argument to the constructor
+  // since it is a lambda and a lambda has no default constructor
   set<int,decltype(comp)> s{comp};
   
   s.insert(10);
   s.insert(11);
   for (auto i: s) cout<<i<<" ";
   cout<<endl;
-  // Another possibility is to wrap the lambda in a function
+  // Another possibility is to wrap the lambda in a function wrapper
   std::function<bool (int const&, int const &)> compfun=comp;
   //Initialization via initializer list
-  set<int,decltype(compfun)> t({1,2,3,4,5,6,7,},compfun);
+  // I have give the comparison object explicitly. The rule is if the comparison object has a state
+  // you have to pass it through the constructor, since we need to pass its state. Otherwise,
+  // the set builds one with the default constructor.
+  set<int,decltype(compfun)> t{{1,2,3,4,5,6,7},compfun};
+  // or use this alternative syntax.
+  // set<int,decltype(compfun)> t={{1,2,3,4,5,6,7},compfun};
   for (auto i: t) cout<<i<<" ";
   cout<<endl;
   // using the functor (no need to give the functor explicitely as argument)
