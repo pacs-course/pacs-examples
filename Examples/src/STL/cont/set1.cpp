a7f0fabc437c960216640e773c5d288b0b60dbf3
formaggia <luca.formaggia@gmail.com>
2020-04-10 18:14:48 +0200

    bettered examples on containers
    Former-commit-id: 2bb7a8bbc993199e20f544771cfeba3f90e5f4fa

--- Examples/src/STL/cont/set1.cpp
+++ Examples/src/STL/cont/set1.cpp
@@ -10,6 +10,7 @@
  */
 #include <iostream>
 #include <set>
+#include <vector>
 #include <algorithm>
 #include <iterator>
 using namespace std;
@@ -23,7 +24,7 @@ int main()
     set<int,greater<int>> coll1;
 
     // insert elements in random order using different member functions
-    coll1.insert({4,8,5,1,6,2});
+    coll1.insert({4,8,5,1,6,2,10,13,18});
     coll1.insert(5);
 
     // print all elements
@@ -50,9 +51,9 @@ int main()
           ostream_iterator<int>(cout," "));
     cout << endl;
 
-    // remove all elements up to element with value 3
-    coll2.erase (coll2.begin(), coll2.find(3));
-    cout<<" Removed all elements up to 3"<<endl;
+    // remove all elements up to element with value 4
+    coll2.erase (coll2.begin(), coll2.find(4));
+    cout<<" Removed all elements up to 4"<<endl;
 
     // print all elements
     copy (coll2.cbegin(), coll2.cend(),
@@ -68,4 +69,54 @@ int main()
     copy (coll2.cbegin(), coll2.cend(),
           ostream_iterator<int>(cout," "));
     cout << endl;
+    // sintersection/union etc.
+    // This algorithm works for any ordered renge, not just sets
+    set<int> coll3{2,4,7,29,32,1,13,18};
+    // union, we show that detination may be container of other type
+    vector<int> allToghether;
+    std::set_union(coll2.begin(), coll2.end(),
+                       coll3.begin(), coll3.end(),                  
+                       std::back_inserter(allToghether));
+    std::cout<<"Union:\n";
+    std::copy (allToghether.cbegin(), allToghether.cend(),
+          ostream_iterator<int>(cout," "));
+    std::cout << std::endl;
+    // Intersection. Now I use a set to collect the result
+    std::set<int> intersection;
+    std::set_intersection(coll2.begin(), coll2.end(),
+                          coll3.begin(), coll3.end(),
+                          std::inserter(intersection,intersection.begin()));
+    std::cout<<"Intersection:\n";
+    std::copy (intersection.cbegin(), intersection.cend(),
+          ostream_iterator<int>(cout," "));
+    std::cout << std::endl;
+    // You also have set_difference and merge among algorithms of the same sort
+    // The new extract facility
+    int value=13;
+    auto node = intersection.extract(value);
+    if(node.empty())
+      {
+        std::cout<<"value "<<value<<" not present"<<std::endl;
+      }
+    else
+      {
+        std::cout<<"value "<<value<<" present"<<std::endl;
+        node.value()=900; // change value
+        auto info = intersection.insert(std::move(node));
+        if(info.inserted==false)
+          {
+            std::cout<<" New value  900 not inserted"<<std::endl;
+          }
+        else
+          {
+            std::cout<<" New value 900 inserted"<<std::endl;
+          }
+      }
+    std::cout<<"Now we have:\n";
+    std::copy (intersection.cbegin(), intersection.cend(),
+               ostream_iterator<int>(cout," "));
+    std::cout << std::endl;
+    
+    
+
 }
