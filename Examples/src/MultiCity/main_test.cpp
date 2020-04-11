997934a42cbe75f9eaf31895922b4d0e1c0db322
formaggia <luca.formaggia@gmail.com>
2020-04-11 20:42:33 +0200

    multicity example
    Former-commit-id: 4e739046580067856b4432397050725432401f48

---
+++ Examples/src/MultiCity/main_test.cpp
@@ -0,0 +1,33 @@
+/*
+ * main_test.cpp
+ *
+ *  Created on: Apr 11, 2020
+ *      Author: forma
+ */
+
+#include "MultiCity.hpp"
+#include <iostream>
+int main()
+{
+  using namespace apsc::multicity;
+  constexpr int N=2;
+  MultiCityEpidemicVariables<2> mcity;
+  MultiCityEpidemicVariables<2>::BlockType S0;
+  MultiCityEpidemicVariables<2>::BlockType I0;
+  for (int i=0; i<N; ++i)
+    for (int j=0; j<N; ++j)
+    {
+      S0(i,j)=i*j;
+      I0(i,j)=0.2;
+    }
+  mcity.S()=S0;
+  mcity.I()=I0;
+  std::cout<<"S="<<mcity.S()<<std::endl;
+  std::cout<<"I="<<mcity.I()<<std::endl;
+
+  MultiCityDataFixed<2> data;
+  data.initializeFromArticle();
+  std::cout<<data;
+}
+
+
