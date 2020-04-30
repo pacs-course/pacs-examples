5d858764b84795c4d80dc5f3a21f141ae4e27949
formaggia <luca.formaggia@gmail.com>
2020-04-30 12:17:55 +0200

    created a better example on object factories and plugins

---
+++ Examples/src/FactoryPlugin/Polygon.cpp
@@ -0,0 +1,40 @@
+#include "Polygon.hpp"
+#include <iostream>
+
+namespace Geometry{
+  
+  // ********************* BASE CLASS **********************
+  Polygon::Polygon(bool convex):isconvex(convex),vertexes(){}
+  Vertices const & Polygon::theVertices()const{return vertexes;};
+  Polygon::Polygon(Vertices v, bool convex):isconvex(convex),vertexes(v){}
+  void Polygon::setVertices(Vertices const & v){
+    vertexes=v;
+  }
+  Polygon::~Polygon(){}
+  bool Polygon::isConvex() const {return isconvex;}
+  void Polygon::showMe(ostream & out)const
+  {out<<"Just a bare Polygon"<<std::endl;}
+
+
+  double Polygon::inscibedCircle() const{
+    // to be implemented
+    return 0;
+  }  
+  double Polygon::diameter() const{
+    // to be implemented
+    return 0;
+  }
+  void Polygon::translate(double const & x, double const & y){
+    // to be implemented
+  }
+  void Polygon::rotate(double const & angle){
+    // to be implemented
+  }
+  void Polygon::scale(double const & xfactor, double const & yfactor){
+    // to be implemented
+  }
+
+}
+
+  
+
---
+++ Examples/src/FactoryPlugin/TriangleAndSquare.hpp
@@ -0,0 +1,55 @@
+/*
+ * TriangleAndSquare.hpp
+ *
+ *  Created on: Apr 29, 2020
+ *      Author: forma
+ */
+
+#ifndef EXAMPLES_SRC_FACTORYPLUGIN_TRIANGLEANDSQUARE_HPP_
+#define EXAMPLES_SRC_FACTORYPLUGIN_TRIANGLEANDSQUARE_HPP_
+#include "FactoryTraits.hpp"
+namespace Geometry
+{
+  /*!
+   * Derived class for triangles
+   */
+  class Triangle: public Polygon
+  {
+  public:
+    Triangle()=default;
+    explicit Triangle(Vertices const & v);
+    //virtual double diameter() const;
+    //virtual double inscibedCircle() const;
+    void showMe(ostream & out=cout) const override;
+    // qui vanno le altre funzionalita`..
+  };
+
+
+/*!
+ * Derived calss for squared
+ */
+    class Square: public Polygon
+    {
+    public:
+      Square()=default;
+      explicit Square(Vertices const & v);
+      //virtual double diameter() const;
+      //virtual double inscibedCircle() const;
+      void showMe(ostream & out=cout) const override;
+      // Metodi propri del quadrato
+      double diagLen()const;
+    };
+
+    //! Internals, not for gerneral use
+    namespace internals
+    {
+      //! Loads the factory when lib is loaded
+      static void __attribute__ ((constructor)) LoadF();
+    }
+
+}
+
+
+
+
+#endif /* EXAMPLES_SRC_FACTORYPLUGIN_TRIANGLEANDSQUARE_HPP_ */
