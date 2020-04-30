ae3ad9efaf4a2a494d5cae27c8c19c1ef85e1984
formaggia <luca.formaggia@gmail.com>
2020-04-30 12:17:55 +0200

    created a better example on object factories and plugins
    Former-commit-id: 667457b7640b70f821f158954021fa2a861e9cd9

---
+++ Examples/src/FactoryPlugin/TriangleAndSquare.cpp
@@ -0,0 +1,45 @@
+/*
+ * TraiangleAndSquare.cpp
+ *
+ *  Created on: Apr 29, 2020
+ *      Author: forma
+ */
+#include "TriangleAndSquare.hpp"
+namespace Geometry
+{
+  // ********************* TRIANGLE **********************
+
+   Triangle::Triangle(Vertices const & v):Polygon(v,true){}
+
+   void Triangle::showMe(ostream & out) const
+   {out<<"The eternal TRIANGLE"<<std::endl;}
+
+
+   // ********************* SQUARE **********************
+
+   Square::Square(Vertices const & v):Polygon(v,true){}
+
+
+   void Square::showMe(ostream & out) const
+   {out<<"I am rather SQUARE, what about you?"<<std::endl;}
+
+   double Square::diagLen()const{
+       //not implemented yet
+       return 0;};
+
+   namespace internals
+   {
+     // in principle I do not have to repeat the attribute here (it is sufficient in the declaration in the hpp file)
+     // but it does not hurt.
+     static void __attribute__ ((constructor)) LoadF()
+    {
+       // add triangle and square to the factory
+      polyFactory["Triangle"]=[](){return std::make_unique<Triangle>();};
+      polyFactory["Square"]=[](){return std::make_unique<Square>();};
+    }
+   }
+ }
+
+
+
+
