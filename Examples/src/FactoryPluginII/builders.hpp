1a5a7a97af4bfec4c1294b37f75831c2f0b1fd50
formaggia <luca.formaggia@gmail.com>
2020-05-01 21:12:10 +0200

    New example that doeas not use compiler attributes
    Former-commit-id: f5811a4fc1258abc3e607f29d5ee77dd72cd22cf

---
+++ Examples/src/FactoryPluginII/builders.hpp
@@ -0,0 +1,51 @@
+#ifndef HH_BUILDERS__
+#define HH_BUILDERS__
+#include<type_traits>
+#include <memory>
+
+
+
+//! An example of abstract builder base class
+/*!
+  It forms the base for builders to be used with 
+  object factories.
+
+  It uses the default constructor. It would be nice to have
+  a generic builder able to pass also arguments to the constructor.
+
+  However it is veri difficult to build a generic one of that sort. Of
+  course for a specific hyerarchy of classes you may construct a
+  specific builder that hase several create() methods each taking
+  different arguments, to be paseed to the constructor.
+
+  Unfortunately, template methods cannot be virtual, otherwise the solution
+  would be simple indeed!
+ */
+template<class AbstractProduct>
+class
+BaseBuilder
+{
+ public:
+
+  virtual std::unique_ptr<AbstractProduct>  create()=0;
+  virtual ~BaseBuilder(){};
+};
+//! Abstract builder
+/*!
+  It relies on the default constructor
+  \pre ConcreteProduct must be default constructible
+  \pre ConcreteProduct must derive from AbstractProduct
+ */
+template<class ConcreteProduct, class AbstractProduct>
+class
+Builder : public BaseBuilder<AbstractProduct>
+{
+public:
+ static_assert(
+		    std::is_base_of<AbstractProduct, ConcreteProduct>::value,
+		    "Builder requires that AbstractProduct be a base of ConcreteProduct");
+  std::unique_ptr<AbstractProduct> create(){return
+      std::make_unique<ConcreteProduct>();
+  } 
+};
+#endif
