f703a02e0bdd5f7d2861f0bdc47faaa2e2bf8cb6
formaggia <luca.formaggia@gmail.com>
2020-05-01 19:27:23 +0200

    corrected a nasty bug
    Former-commit-id: 53a287f219230e03f7a5e3f628c2d952b1bd05f6

--- Examples/src/FactoryPlugin/Quadrilateral.cpp
+++ Examples/src/FactoryPlugin/Quadrilateral.cpp
@@ -20,11 +20,11 @@ namespace Geometry
    void Quadrilateral::showMe(ostream & out) const
    {out<<"I have four sides, like any QUADRILATERAL should"<<std::endl;}
 
-  namespace internals
+  namespace
    {
     // in principle I do not have to repeat the attribute here (it is sufficient in the declaration in the hpp file)
     // but it does not hurt.
-     static void __attribute__ ((constructor)) LoadF()
+     void __attribute__ ((constructor)) LoadF()
     {
        // add quadrilateral to the factory
       polyFactory["Quadrilateral"]=[](){return std::make_unique<Quadrilateral>();};
