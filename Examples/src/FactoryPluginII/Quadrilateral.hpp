bbaf8d1b214a2ef4cbb44af326c87e55787e783d
formaggia <luca.formaggia@gmail.com>
2020-05-01 21:12:10 +0200

    New example that doeas not use compiler attributes
    Former-commit-id: f5811a4fc1258abc3e607f29d5ee77dd72cd22cf

---
+++ Examples/src/FactoryPluginII/Quadrilateral.hpp
@@ -0,0 +1,30 @@
+/*
+ * Quadrilateral.hpp
+ *
+ *  Created on: Apr 29, 2020
+ *      Author: forma
+ */
+
+#ifndef EXAMPLES_SRC_FACTORYPLUGIN_QUADRILATERAL_HPP_
+#define EXAMPLES_SRC_FACTORYPLUGIN_QUADRILATERAL_HPP_
+#include "Polygon.hpp"
+namespace Geometry
+{
+  /*!
+   * Derived class for quadrilaterals
+   */
+  class Quadrilateral: public Polygon
+  {
+  public:
+    Quadrilateral();
+    explicit Quadrilateral(Vertices const & v);
+    explicit Quadrilateral(Vertices const & v,bool convex);
+    virtual ~Quadrilateral();
+    virtual void showMe(ostream & out=cout) const override;
+    // other stuff in the future...
+  };
+
+
+}
+
+#endif /* EXAMPLES_SRC_FACTORYPLUGIN_QUADRILATERAL_HPP_ */
---
+++ Examples/src/FactoryPluginII/TriangleAndSquare.cpp
@@ -0,0 +1,46 @@
+/*
+ * TraiangleAndSquare.cpp
+ *
+ *  Created on: Apr 29, 2020
+ *      Author: forma
+ */
+#include "TriangleAndSquare.hpp"
+#include "FactoryTraits.hpp"
+namespace Geometry
+{
+  // ********************* TRIANGLE **********************
+  
+  Triangle::Triangle(Vertices const & v):Polygon(v,true){}
+  
+  void Triangle::showMe(ostream & out) const
+  {out<<"The eternal TRIANGLE"<<std::endl;}
+  
+  
+  // ********************* SQUARE **********************
+  
+  Square::Square(Vertices const & v):Polygon(v,true){}
+  
+  
+  void Square::showMe(ostream & out) const
+   {out<<"I am rather SQUARE, what about you?"<<std::endl;}
+  
+  double Square::diagLen()const{
+    //not implemented yet
+    return 0;};
+  
+  namespace
+  {
+    struct LoadF
+    {
+      LoadF(){
+        polyFactory["Triangle"]=[](){return std::make_unique<Triangle>();};
+        polyFactory["Square"]=[](){return std::make_unique<Square>();};
+      }
+    };
+    const LoadF loadf; // local variable
+  }
+}
+
+
+
+
