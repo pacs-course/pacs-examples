65a9de3e19a0b567b78f9bd3a04d60ae0e638e1a
formaggia <luca.formaggia@gmail.com>
2020-05-02 18:55:50 +0200

    bettered Generic Factory

--- Examples/src/GenericFactory/main_testAbstractFactory.cpp
+++ Examples/src/GenericFactory/main_testAbstractFactory.cpp
@@ -1,5 +1,6 @@
 //! A simple example of use of the generic object Factory template class
 #include "Factory.hpp"
+#include "Proxy.hpp"
 #include <iostream>
 // an abstract class
 class Abstract
@@ -52,16 +53,19 @@ Builder build3=[]{return std::make_unique<Derived3>();};
   
 //! Normally this is done elsewhere, but this is only a test
 /*! 
-  Filling up the factory with the builders 
+  Filling the first 2 factory with the builders 
  */
 void loadFactory()
 {
   auto & factory=MyObjectFactory::Instance();
   factory.add(1,build1);
   factory.add(2,build2);
-  factory.add(3,build3);
 }
-
+// the third with the proxy
+namespace
+{
+  GenericFactory::Proxy<MyObjectFactory,Derived3> P3{3,build3};
+}
   
 int main()
 {
