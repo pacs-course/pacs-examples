ce2bae2e0bfaabd09ad76cea0c10f596a008b319
formaggia <luca.formaggia@gmail.com>
2020-05-03 18:30:56 +0200

    Bettered examples for numerical quadrature and OneDMesh
    Former-commit-id: 16f28d4098d8e2b81d4977f448168c79cf0dcf77

--- Examples/src/OneDMesh/mesh.hpp
+++ Examples/src/OneDMesh/mesh.hpp
@@ -10,7 +10,8 @@ namespace Geometry{
     //! Default constructor is defaulted.
     Mesh1D()=default;
     //! Constructor for an equaly spaced mesh
-    /*! \param d  A domain
+    /*! 
+      \param d  A domain
       \param n  Number of intervals (not nodes!)
     */
     Mesh1D(Domain1D const & d, unsigned int const &n);
@@ -20,32 +21,6 @@ namespace Geometry{
     */
     Mesh1D(Geometry::OneDMeshGenerator const & gf):
       myDomain(gf.getDomain()),myNodes(gf()){};
-    //! I use the default copy-constructor (C++11)
-    /*!
-      I explicitely report the default even if not strictly necessary, to 
-      recall the reader that C++ provides a synthetic copy constructor.
-    */
-    Mesh1D(Mesh1D const&)=default;
-    //! Move constructor
-    /*
-      Since I store the data in a vector<T> and std vectors implement
-      move semantic I can just use the synthetic move constructor!
-    */
-    Mesh1D(Mesh1D&&)=default; //C++11
-    //! I use default copy-assignement.
-    /*!
-      I explicitely report the default even if not strictly necessary, to 
-      recall the reader that C++ provides a synthetic copy assignment.
-    */
-    Mesh1D & operator=(Mesh1D const &)=default;
-    //! I use default move-assignement.
-    /*!
-      I explicitely report the default even if not strictly necessary, to 
-      recall the reader that C++ provides a synthetic move assignment.
-      Here I use the default because the data is stored in a vector<T> and
-      std vector already support move semantic!
-    */
-    Mesh1D & operator=(Mesh1D&&)=default;
     //! Generate mesh (it will destroy old mesh)
     /*!
       @param mg a mesh generator
@@ -58,10 +33,10 @@ namespace Geometry{
     double operator [](int i)const {return myNodes[i];}
     //! To use the mesh in range based for loop I need begin()
     std::vector<double>::iterator begin(){return myNodes.begin();}
-    std::vector<double>::const_iterator begin()const {return myNodes.cbegin();}
+    std::vector<double>::const_iterator cbegin()const {return myNodes.cbegin();}
     //! To use the mesh in range based for loop I need end()
     std::vector<double>::iterator end(){return myNodes.end();}
-    std::vector<double>::const_iterator end() const{return myNodes.cend();}
+    std::vector<double>::const_iterator cend() const{return myNodes.cend();}
     //! I return a copy of the DOmain1D.
     /*!
       In case it is needed.
