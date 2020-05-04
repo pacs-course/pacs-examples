58cae07db9e316232325303919cdb89f92b7ad12
formaggia <luca.formaggia@gmail.com>
2020-05-04 19:50:22 +0200

    bettered documentation
    Former-commit-id: 808177420869c0dbdc8324388fd7f24e1534d4b3

--- Examples/src/QuadratureRule/AllDynamic/udfHandler.cpp
+++ Examples/src/QuadratureRule/AllDynamic/udfHandler.cpp
@@ -3,22 +3,20 @@
 
 namespace NumericalIntegration{
 
-  UdfHandler::UdfHandler():M_lib_handle(nullptr){}
-  
   void UdfHandler::openUdfLibrary(const std::string & s)
   {
-    if (this->M_lib_handle)dlclose(this->M_lib_handle);
+    if (this->M_lib_handle!=nullptr)dlclose(this->M_lib_handle);
     this->M_lib_handle=dlopen(s.c_str(),RTLD_LAZY);
-    if (!this->M_lib_handle)
+    if (this->M_lib_handle==nullptr)
       throw std::invalid_argument("Udf library " + s + " not found");
   }
   
   FunPoint UdfHandler::getFunction(const std::string & s) const
   {
-    if (!this->M_lib_handle)
+    if (this->M_lib_handle==nullptr)
       throw std::invalid_argument("Trying to access function on unopened library");
     void * funp=dlsym(this->M_lib_handle,s.c_str());
-    if (!funp)
+    if (funp==nullptr)
       throw std::invalid_argument("Integrand named " +s+ " not found in library");
     else
       return FunPoint(reinterpret_cast<double (*)(double const &)>(funp));
