e4b4a349c11108faea92bda571b717d1923b42e3
formaggia <luca.formaggia@gmail.com>
2020-05-04 14:49:17 +0200

    Extended factory to functions

--- Examples/src/GenericFactory/main_testAbstractFactory.cpp
+++ Examples/src/GenericFactory/main_testAbstractFactory.cpp
@@ -2,6 +2,7 @@
 #include "Factory.hpp"
 #include "Proxy.hpp"
 #include <iostream>
+#include <cmath>
 // an abstract class
 class Abstract
 {
@@ -10,7 +11,7 @@ public:
   virtual ~Abstract()=default;
 };
 
-//! Some conclrete classes
+//! Some concrete classes
 /*!@{ */
 class Derived1: public Abstract
 {
@@ -67,6 +68,23 @@ namespace
   GenericFactory::Proxy<MyObjectFactory,Derived3> P3{3,build3};
 }
   
+//! now an example of using the Factory with function (an alternative to the C-linkage)
+
+using FunType= std::function<double (const double &, const double &)>;
+using FunFactory=GenericFactory::FunctionFactory<Identifier,FunType>;
+//! loads functions (normally in a separate file)
+void loadFunctions()
+{
+  auto & Ffactory=FunFactory::Instance();
+
+  Ffactory.add(1,[](const double & a, const double & b){return a*b;});
+  // incomplete elliptic integral of the first kind, from the STL cmath lib (since c++17);
+  // note I need to specify template argument explicitely to get the correct overload
+  Ffactory.add(2,&std::ellint_1<double,double>);
+  //atan2 form std: note I need to specify template argument explicitely to get the correct overload
+  Ffactory.add(3,&std::atan2<double,double>);
+}
+
 int main()
 {
   // fill factory
@@ -84,4 +102,15 @@ int main()
   std::cin>>i;
   auto object = factory.create(i);
   object->whoAmI();
+  // Now the functions
+  loadFunctions();
+  auto & ffactory=FunFactory::Instance();
+  std::cout<<" \nType identifier of desired function (1,2,3)"<<std::endl;
+  std::cin>>i;
+  // get the function from the factory
+  FunType f =ffactory.get(i);
+  std::cout<<" Type x and y "<<std::endl;
+  double x, y;
+  std::cin>>x>>y;
+  std::cout<<"f_"<<i<<"("<<x<<", "<<y<<")="<<f(x,y)<<std::endl;
 }
