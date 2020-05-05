7e0b9c9ad5d42af5fb11f2a698f1f24d86c2c313
formaggia <luca.formaggia@gmail.com>
2020-05-05 12:46:48 +0200

    got rid of the hideous -Wl-E option!
    Former-commit-id: 523c9b4fec02886339639a9a612b93e78247d010

---
+++ Examples/src/QuadratureRule/AllDynamic/ruleFactory.cpp
@@ -0,0 +1,14 @@
+/*
+ * ruleFactory.cpp
+ *
+ *  Created on: May 5, 2020
+ *      Author: forma
+ */
+#include "ruleFactory.hpp"
+namespace QuadratureRuleFactory
+{
+  RulesFactory & MyFactory = RulesFactory::Factory::Instance();
+}
+
+
+
