df36e88f492b1b316fc0bb227742e41b2d5a4459
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-05-08 13:00:48 +0000

    Use ReturnType
    Former-commit-id: 14cecdb92f691802cae67250b872170710a8aa07

--- Exercises/2020/07-eigen-newton/src-solution/main.cpp
+++ Exercises/2020/07-eigen-newton/src-solution/main.cpp
@@ -7,11 +7,12 @@
 int
 main(int argc, char **argv)
 {
-  using ArgumentType = NewtonTraits::ArgumentType;
+  using ArgumentType       = NewtonTraits::ArgumentType;
+  using ReturnType         = NewtonTraits::ReturnType;
   using JacobianMatrixType = NewtonTraits::JacobianMatrixType;
 
-  auto system = [](const ArgumentType &x) -> ArgumentType {
-    ArgumentType y(2);
+  auto system = [](const ArgumentType &x) -> ReturnType {
+    ReturnType y(2);
 
     y(0) = (1 - x(0)) * (1 - x(0)) + 0.1 * (5 - x(1)) * (5 - x(1));
     y(1) = 1.5 - x(0) - 0.1 * x(1);
