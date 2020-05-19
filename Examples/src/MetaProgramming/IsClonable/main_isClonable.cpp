a54cf225a65168ba99399041554eb173c4b3249d
formaggia <luca.formaggia@gmail.com>
2020-05-19 12:17:14 +0200

    Bettered some examples
    Former-commit-id: 6b61fa0cb7226a69c72a7a310bbbe4e094d20f9f

--- Examples/src/MetaProgramming/IsClonable/main_isClonable.cpp
+++ Examples/src/MetaProgramming/IsClonable/main_isClonable.cpp
@@ -1,6 +1,7 @@
 #include "IsClonable.hpp"
 #include <iostream>
 #include <iomanip>
+#include <type_traits>
 struct C
 {
   C * clone(){return new C(*this);}
@@ -12,11 +13,38 @@ struct NC
   int i;
 };
 
+template <class T>
+class NiftyContainer{
+public:
+  void DoSomething(T* pObj)
+  {
+    using namespace TypeTraits;
+    if constexpr (isClonable<T>())
+    {
+      T* pNewObj = pObj->clone();
+      pNewObj->i=10; // avoid warning for unused variable
+      std::cout<<"Using polymorphic algorithm"<<std::endl;
+    }
+    else
+      {
+	T* pNewObj = new T(*pObj);
+	std::cout<<"Using non polymorphic algorithm"<<std::endl;
+	delete pNewObj;
+      }
+  }
+};
+
 
 int main()
 {
   using namespace TypeTraits;
   std::cout<<"C is clonable:  "<<std::boolalpha<< isClonable<C>()<<std::endl;
+  NiftyContainer<C> ncc;
+  C pc;
+  ncc.DoSomething(&pc);
   std::cout<<"NC is clonable: "<<std::boolalpha<< isClonable<NC>()<<std::endl;
+  NiftyContainer<NC> ncnc;
+  NC pnc;
+  ncnc.DoSomething(&pnc);
 }
 
