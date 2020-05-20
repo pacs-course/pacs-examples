3f66870b1a1eeff5c8b2853776143d425c951d32
formaggia <luca.formaggia@gmail.com>
2020-05-20 18:46:29 +0200

    added a files and cleaned up style
    Former-commit-id: 9075c27b20ecf4564450840a5d8875694208960d

--- Examples/src/FactoryPlugin/TriangleAndSquare.hpp
+++ Examples/src/FactoryPlugin/TriangleAndSquare.hpp
@@ -10,35 +10,35 @@
 #include "Polygon.hpp"
 namespace Geometry
 {
-  /*!
-   * Derived class for triangles
-   */
-  class Triangle: public Polygon
-  {
-  public:
-    Triangle()=default;
-    explicit Triangle(Vertices const & v);
+/*!
+ * Derived class for triangles
+ */
+class Triangle: public Polygon
+{
+public:
+    Triangle() = default;
+    explicit Triangle (Vertices const& v);
     //virtual double diameter() const;
     //virtual double inscibedCircle() const;
-    void showMe(ostream & out=cout) const override;
+    void showMe (ostream& out = cout) const override;
     // qui vanno le altre funzionalita`..
-  };
+};
 
 
 /*!
  * Derived calss for squared
  */
-    class Square: public Polygon
-    {
-    public:
-      Square()=default;
-      explicit Square(Vertices const & v);
-      //virtual double diameter() const;
-      //virtual double inscibedCircle() const;
-      void showMe(ostream & out=cout) const override;
-      // Metodi propri del quadrato
-      double diagLen()const;
-    };
+class Square: public Polygon
+{
+public:
+    Square() = default;
+    explicit Square (Vertices const& v);
+    //virtual double diameter() const;
+    //virtual double inscibedCircle() const;
+    void showMe (ostream& out = cout) const override;
+    // Metodi propri del quadrato
+    double diagLen() const;
+};
 
 }
 
