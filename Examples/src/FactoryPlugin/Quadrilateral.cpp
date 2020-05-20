a818a6573fe17d800cff9aceeadbc9be23b3c94a
formaggia <luca.formaggia@gmail.com>
2020-05-20 18:46:29 +0200

    added a files and cleaned up style
    Former-commit-id: 072e070db6b54b28fee0c60d064523f8274e445d

--- Examples/src/FactoryPlugin/Quadrilateral.cpp
+++ Examples/src/FactoryPlugin/Quadrilateral.cpp
@@ -8,29 +8,36 @@
 #include "FactoryTraits.hpp"
 namespace Geometry
 {
-  // ********************* QUADRILATERAL **********************
+// ********************* QUADRILATERAL **********************
 
-   Quadrilateral::Quadrilateral():Polygon(){};
-   Quadrilateral::Quadrilateral(Vertices const & v):Polygon(v){
-     // codice per deteminare la convessita` a n. vertici
-     ;}
-   Quadrilateral::Quadrilateral(Vertices const & v,bool c):Polygon(v,c){}
+Quadrilateral::Quadrilateral() : Polygon() {};
+Quadrilateral::Quadrilateral (Vertices const& v) : Polygon (v)
+{
+    // codice per deteminare la convessita` a n. vertici
+    ;
+}
+Quadrilateral::Quadrilateral (Vertices const& v, bool c) : Polygon (v, c) {}
 
-   Quadrilateral::~Quadrilateral(){}
+Quadrilateral::~Quadrilateral() {}
 
-   void Quadrilateral::showMe(ostream & out) const
-   {out<<"I have four sides, like any QUADRILATERAL should"<<std::endl;}
+void Quadrilateral::showMe (ostream& out) const
+{
+    out << "I have four sides, like any QUADRILATERAL should" << std::endl;
+}
 
-  namespace
-   {
-    // in principle I do not have to repeat the attribute here (it is sufficient in the declaration in the hpp file)
-    // but it does not hurt.
-     void __attribute__ ((constructor)) LoadF()
+namespace
+{
+// in principle I do not have to repeat the attribute here (it is sufficient in the declaration in the hpp file)
+// but it does not hurt.
+void __attribute__ ( (constructor) ) LoadF()
+{
+    // add quadrilateral to the factory
+    polyFactory["Quadrilateral"] = []()
     {
-       // add quadrilateral to the factory
-      polyFactory["Quadrilateral"]=[](){return std::make_unique<Quadrilateral>();};
-    }
-   }
+        return std::make_unique<Quadrilateral>();
+    };
+}
+}
 }
 
 
