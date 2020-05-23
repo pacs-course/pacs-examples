4763cdac142cee07f8d7f47ccfa6551bd000f48a
formaggia <luca.formaggia@gmail.com>
2020-05-23 16:20:02 +0200

    cleaned up code

--- Examples/src/CompositionWithVariadicTemplates/Id.hpp
+++ Examples/src/CompositionWithVariadicTemplates/Id.hpp
@@ -1,28 +1,32 @@
 #ifndef __ID_HPP__
 #define __ID_HPP__
-#include<limits>
+#include <limits>
 namespace Identifier
 {
-  //! Since c++17 I could have used std::optional 
-  constexpr unsigned int notAnId=std::numeric_limits<unsigned int>::max();
-  class Id
+//! Since c++17 I could have used std::optional
+constexpr unsigned int notAnId = std::numeric_limits<unsigned int>::max ();
+class Id
+{
+protected:
+  unsigned int M_id;
+
+public:
+  Id (unsigned int i = notAnId) : M_id (i){};
+  void
+  setId (unsigned int const &i)
   {
-  protected:
-    unsigned int M_id;
-  public:
-    Id(unsigned int i=notAnId):M_id(i){};
-    void setId(unsigned int const & i)
-    {
-      M_id=i;
-    }
-    unsigned int getId() const
-    {
-      return M_id;
-    }
-    bool isUnassigned() const
-    {
-      return M_id==notAnId;
-    }
-  };
+    M_id = i;
+  }
+  unsigned int
+  getId () const
+  {
+    return M_id;
+  }
+  bool
+  isUnassigned () const
+  {
+    return M_id == notAnId;
+  }
+};
 }
 #endif
