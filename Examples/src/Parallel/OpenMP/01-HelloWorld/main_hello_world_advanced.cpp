843f9400508a5425d52c4d324a470dd3c7a05d36
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2020-05-26 09:21:46 +0000

    Add MPI and OpenMP folders
    Former-commit-id: dc0d44492a02acda9c0f4bad724d007da85ff145

---
+++ Examples/src/Parallel/OpenMP/01-HelloWorld/main_hello_world_advanced.cpp
@@ -0,0 +1,52 @@
+/**
+ * @author Pasquale Claudio Africa <pasqualeclaudio.africa@polimi.it>
+ * @date 2020
+ */
+
+#include <omp.h>
+
+#include <iostream>
+
+int
+main(int argc, char **argv)
+{
+  int n_threads;
+  int thread_id;
+
+  /**
+   * Fork a team of threads with each thread
+   * having a private thread_id variable.
+   */
+#pragma omp parallel private(thread_id)
+  {
+    thread_id = omp_get_thread_num();
+
+/**
+ * The following block is "critical", i.e. we only let one thread
+ * execute this block at a time.
+ *
+ * Other types of blocks include single (only one thread executes
+ * this block), master (only the master thread executes this
+ * block), atomic (protect a variable by changing it in one step).
+ */
+#pragma omp critical
+    std::cout << "Hello World from thread " << thread_id << "!"
+              << std::endl;
+
+
+// Let's wait until all threads reach the following line.
+#pragma omp barrier
+
+// Only master thread prints this.
+#pragma omp master
+    // The previous line is equivalent to: if (thread_id == 0).
+    {
+      n_threads = omp_get_num_threads();
+      std::cout << "Number of threads = " << n_threads << std::endl;
+    }
+  }
+
+  // All threads join master thread and terminate.
+
+  return 0;
+}
---
+++ Examples/src/Parallel/OpenMP/03-PI/main_pi.cpp
@@ -0,0 +1,68 @@
+/**
+ * @author Pasquale Claudio Africa <pasqualeclaudio.africa@polimi.it>
+ * @date 2020
+ */
+
+#include <omp.h>
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+/**
+ * This exercise presents a simple program to determine the value of
+ * pi. The algorithm suggested here is chosen for its simplicity. The
+ * method evaluates the integral of 4 / (1 + x^2) between 0 and 1
+ * using the composite midpoint rule. We sum over the integral range
+ * in a for loop, parallelized using OpenMP.
+ */
+int
+main(int argc, char **argv)
+{
+  const unsigned int n = 1e8;
+  const double       h = 1.0 / n;
+
+  double x;
+  double sum = 0.0;
+  double pi  = 0.0;
+
+/**
+ * We declare x as private, i.e. each thread keeps its own copy.
+ * Private variables are NOT initialized, we can declare them
+ * outside the parallel block's scope, but they must be initialized
+ * in each thread.
+ *
+ * We declare sum as firstprivate, i.e. it's private but initialized
+ * with the value defined in the master thread; 0.0 in this case.
+ *
+ * We declare pi as shared, any thread can access it. Shared is the
+ * default, however being explicit is not a bad thing. You should be
+ * careful that multiple threads are not writing to shared variables
+ * at the same time (race conditions).
+ */
+#pragma omp parallel private(x), firstprivate(sum), shared(pi)
+  {
+/**
+ * See http://jakascorner.com/blog/2016/06/omp-for-scheduling.html
+ * for an explanation about scheduling types.
+ */
+#pragma omp for schedule(static)
+    for (unsigned int i = 1; i <= n; ++i)
+      {
+        x = h * (i - 0.5);
+        sum += 4.0 / (1.0 + x * x);
+      }
+
+// Critical block, to prevent data race!
+#pragma omp critical
+    {
+      pi += h * sum;
+    }
+  }
+
+  std::cout << std::setprecision(16) << "pi = " << pi << ", error = "
+            << std::fabs(pi - 3.141592653589793238462643)
+            << std::endl;
+
+  return 0;
+}
