8f248f1642d2bf8d03afc9e7d7345b59dad6669f
formaggia <luca.formaggia@gmail.com>
2020-05-27 13:07:34 +0200

    bettered two examples
    Former-commit-id: f182aeecb63bd4f989dc818b56ec64521c76eef1

--- Examples/src/MetaProgramming/Enable_if/Norms.hpp
+++ Examples/src/MetaProgramming/Enable_if/Norms.hpp
@@ -1,3 +1,4 @@
+#include "is_complex.hpp"
 #include <type_traits>
 #include <vector>
 #include <complex>
@@ -6,22 +7,20 @@
 #define HH_NORMS_HH
 //! Computes the 2-norm of a std:;vector<double> or of a std::vector<std::complex<double>>
 /*! 
-  Is  an example of the use of enable_if to select a specific function member depending on the type
+  An example of the use of enable_if to select a specific function member depending on the type
   of the template argument.
 
   In this case the SFINAE paradigm is activated on the return type of the call operator.
 
   <b>Take it only as an example</b>. The function std::norm works also for floating point types, so you do not
-  need to have two separate call operators, you might just use the second version (taking away the enable_if suff of course)
+  need to have two separate call operators, you can just use the second version (taking away the enable_if stuff of course)
   
-  \note The class template Norm2 works only if the template argument is
-  a vector<double> or vector<complex<double>>. Making it more general is possible, but more involved.
  */
 struct Norm2
 {
   //! For vector<double>
   template<class C>
-  std::enable_if_t<std::is_same_v<C,std::vector<double>>, double>
+  std::enable_if_t<std::is_floating_point_v<typename C::value_type>, double>
     operator()(C const & c)
   {
     double tmp{0.0};
@@ -31,7 +30,7 @@ struct Norm2
 
   //! For vector<complex<double>>
   template<class C>
-  std::enable_if_t<std::is_same_v<C,std::vector<std::complex<double>> >, double>
+  std::enable_if_t<TypeTraits::is_complex<typename C::value_type>::value, double>
     operator()(C const & c)
   {
     double tmp{0.0};
