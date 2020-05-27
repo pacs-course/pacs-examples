9da8786e6dde057c509613ce39a322430483c4e1
formaggia <luca.formaggia@gmail.com>
2020-05-27 18:02:05 +0200

    bettered example
    Former-commit-id: e41f35df6ceec85b390625bc14fa5fe0c4e7fab2

--- Examples/src/MetaProgramming/MetaDot/metadot.hpp
+++ Examples/src/MetaProgramming/MetaDot/metadot.hpp
@@ -2,7 +2,7 @@
 #define HH_METADOT_HH
 #include <array>
 #include <cstddef>
-
+#include <type_traits>
 /*! An example of template metaprogramming.
 
   It implements the dot product of two std::arrays.
@@ -21,7 +21,7 @@ template<std::size_t M>
 struct metaDot
 {
   template<std::size_t N,typename T>
-  static T apply(std::array<T,N>const & a,std::array<T,N> const & b)
+  static constexpr T apply(std::array<T,N>const & a,std::array<T,N> const & b)
   {
     return a[M-1]*b[M-1] + metaDot<M-1>::apply(a,b);
   }
@@ -31,11 +31,12 @@ struct metaDot
 template<> struct metaDot<1>
 {
   template<std::size_t N,typename T>
-  static T apply(std::array<T,N>const & a,std::array<T,N> const & b)
+  static constexpr T apply(std::array<T,N>const & a,std::array<T,N> const & b)
   {
     return a[0]*b[0];
   }
 };
+  
 }
 //! Operator overloading: this is what will be actually used.
 template<std::size_t N,typename T>
@@ -43,6 +44,20 @@ T operator * (std::array<T,N>const & a,std::array<T,N> const & b)
 {
   return internals::metaDot<N>::apply(a,b);
 }
+
+  //! C++14 onward version. You may use this if you prefer
+  //! which is also more flexibe. You can convert it it operator*,
+  //! since also operators can be constexpr
+template<std::size_t N,typename T1, typename T2>
+  constexpr auto metadot17(std::array<T1,N>const & a,std::array<T2,N> const & b)
+  {
+    using result_type=std::common_type_t<T1,T2>;
+    // casting 0 not really needed
+    result_type result=static_cast<result_type>(0);
+    for (std::size_t i = 0; i<N; ++i) result+=a[i]*b[i];
+    return result;                     
+  }
+
 
 //
 #endif
