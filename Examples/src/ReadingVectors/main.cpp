b72c491161e8c8d39b93c7218485f2f3ec13479c
formaggia <luca.formaggia@gmail.com>
2020-06-10 09:45:26 +0200

    added example on reading vector as a stream

---
+++ Examples/src/ReadingVectors/main.cpp
@@ -0,0 +1,20 @@
+#include "ReadingVector.hpp"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  { // block for laziness
+    std::vector<int> a;
+    a <<1,2,3,4; //eigen style
+    for (auto v:a)std::cout<<v<<", ";
+    std::cout<<std::endl;
+  }
+  {
+    // but you can do also simply
+    std::vector<int> a;
+    a <<1<<2<<3<<4; // Using  vector as a stream
+    for (auto v:a)std::cout<<v<<", ";
+    std::cout<<std::endl;
+  }
+}
