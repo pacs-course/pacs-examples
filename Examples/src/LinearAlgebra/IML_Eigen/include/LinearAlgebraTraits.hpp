e6758343cad82a7f100babfc9613781ffa8c5584
formaggia <luca.formaggia@gmail.com>
2020-06-14 22:03:49 +0200

    bettered adaptors
    Former-commit-id: ae2ad588a5ad44c46521cd294c85941b016617aa

--- Examples/src/LinearAlgebra/IML_Eigen/include/LinearAlgebraTraits.hpp
+++ Examples/src/LinearAlgebra/IML_Eigen/include/LinearAlgebraTraits.hpp
@@ -3,36 +3,57 @@
 #include <Eigen/SparseCore>
 #include <Eigen/SparseLU>
 #include <memory>
+/*!
+ * Adaptors to interface with Eugen
+ */
 namespace LinearAlgebra
 {
-template <class Derived>
-inline double
-dot (const Eigen::SparseMatrixBase<Derived> &a,
-     const Eigen::SparseMatrixBase<Derived> &b)
-{
-  return a.dot (b);
-}
-
-template <class Derived>
-inline double
-dot (const Eigen::MatrixBase<Derived> &a, const Eigen::MatrixBase<Derived> &b)
-{
-  return a.dot (b);
-}
-
-template <class Derived>
-inline double
-norm (const Eigen::SparseMatrixBase<Derived> &a)
-{
-  return a.norm ();
-}
-
-template <class Derived>
-inline double
-norm (const Eigen::MatrixBase<Derived> &a)
-{
-  return a.norm ();
-}
+  /*!
+   * @tparam Derived The type of the actual Eigen sparse vector
+   * @param a a sparse Eigen vector
+   * @param b a sparse Eigen vector
+   * @return the dot product
+   */
+  template <class Derived>
+  decltype(auto)
+  dot (const Eigen::SparseMatrixBase<Derived> &a, const Eigen::SparseMatrixBase<Derived> &b)
+  {
+    return a.dot (b);
+  }
+  /*!
+   * @tparam Derived The type of the actual Eigen dense vector
+   * @param a a dense Eigen vector
+   * @param b a dense Eigen vector
+   * @return the dot product
+   */
+  template <class Derived>
+  decltype(auto)
+  dot (const Eigen::MatrixBase<Derived> &a, const Eigen::MatrixBase<Derived> &b)
+  {
+    return a.dot (b);
+  }
+  /*!
+   * @tparam Derived The type of the actual Eigen sparse matrix (or vector)
+   * @param a a sparse Eigen matrix
+   * @return the 2-norm
+   */
+  template <class Derived>
+  decltype(auto)
+  norm (const Eigen::SparseMatrixBase<Derived> &a)
+  {
+    return a.norm ();
+  }
+  /*!
+   * @tparam Derived The type of the actual Eigen dense matrix (or vector)
+   * @param a a denst Eigen matrix
+   * @return the 2-norm
+   */
+  template <class Derived>
+  decltype(auto)
+  norm (const Eigen::MatrixBase<Derived> &a)
+  {
+    return a.norm ();
+  }
 
 } // end namespace
 #endif
