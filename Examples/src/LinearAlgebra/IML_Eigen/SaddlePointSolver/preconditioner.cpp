5254aca69191004bded4c5048120a522e7323ad6
formaggia <luca.formaggia@gmail.com>
2020-07-09 16:15:33 +0200

    cleanup
    Former-commit-id: ffc7bc9915b83cf3ce5bb683e5af54a00ae4b1aa

--- Examples/src/LinearAlgebra/IML_Eigen/SaddlePointSolver/preconditioner.cpp
+++ Examples/src/LinearAlgebra/IML_Eigen/SaddlePointSolver/preconditioner.cpp
@@ -15,7 +15,6 @@
 #include <iostream>
 namespace FVCode3D
 {
-// LF si potrebbe usare decltype(auto)?
 DiagMat
 ComputeApproximateInverseInnerProd (const SaddlePointMat &SP, bool lumping)
 {
@@ -29,7 +28,7 @@ ComputeApproximateInverseInnerProd (const SaddlePointMat &SP, bool lumping)
         {
           for (SpMat::InnerIterator it (M, j); it; ++it)
             {
-              ML[it.row ()] += it.value ();
+              ML[it.row ()] += std::abs(it.value ());
             }
         }
       return ML.asDiagonal ().inverse ();
@@ -50,52 +49,6 @@ ComputeApproximateSchur (const SaddlePointMat &SP, const DiagMat &D)
   return res;
 }
 
-SpMat
-SaddlePointMat::fullMatrix () const
-{
-  long int const nrows = M.rows () + B.rows ();
-  long int const Msize = M.rows ();
-  SpMat          result (nrows, nrows);
-  long int const nnz = static_cast<long int> (this->nonZeros ());
-  result.reserve (nnz);
-  std::vector<Eigen::Triplet<double, long int> > tripletList;
-  tripletList.reserve (nnz);
-  double s = isSymUndef ? 1.0 : -1.0;
-  for (int k = 0; k < M.outerSize (); ++k)
-    for (SpMat::InnerIterator it (M, k); it; ++it)
-      {
-        tripletList.emplace_back (
-          Eigen::Triplet<double, long int>{it.row (), it.col (), it.value ()});
-      }
-  for (int k = 0; k < B.outerSize (); ++k)
-    for (SpMat::InnerIterator it (B, k); it; ++it)
-      {
-        tripletList.emplace_back (Eigen::Triplet<double, long int>{
-          it.row () + Msize, it.col (), s * it.value ()});
-        tripletList.emplace_back (Eigen::Triplet<double, long int>{
-          it.col (), it.row () + Msize, it.value ()});
-      }
-
-  for (int k = 0; k < T.outerSize (); ++k)
-    for (SpMat::InnerIterator it (T, k); it; ++it)
-      {
-        tripletList.emplace_back (Eigen::Triplet<double, long int>{
-          it.row () + Msize, it.col () + Msize, s * it.value ()});
-      }
-  result.setFromTriplets (tripletList.begin (), tripletList.end ());
-  return result;
-}
-
-void
-SaddlePointMat::clear ()
-{
-  M.resize (0, 0);
-  B.resize (0, 0);
-  T.resize (0, 0);
-  M.data ().squeeze ();
-  B.data ().squeeze ();
-  T.data ().squeeze ();
-}
 
 Vector
 diagonal_preconditioner::solve (const Vector &r) const
