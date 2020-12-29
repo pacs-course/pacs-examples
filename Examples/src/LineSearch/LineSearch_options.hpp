5bedc6c5c000ded83986061aad19687574468483
formaggia <luca.formaggia@gmail.com>
2020-12-29 11:07:13 +0100

    Tools for line search optimization
    Former-commit-id: 0b1cfa10c99387879975b02304fa89dccfcfea7b

---
+++ Examples/src/LineSearch/LineSearch_options.hpp
@@ -0,0 +1,30 @@
+/*
+ * LineSearch_options.hpp
+ *
+ *  Created on: Dec 27, 2020
+ *      Author: forma
+ */
+
+#ifndef EXAMPLES_SRC_LINESEARCH_LINESEARCH_OPTIONS_HPP_
+#define EXAMPLES_SRC_LINESEARCH_LINESEARCH_OPTIONS_HPP_
+#include "LineSearch_traits.hpp"
+namespace apsc
+{
+
+
+
+  struct LineSearchOptions
+  {
+    using Scalar=apsc::LineSearch_traits::Scalar;
+    Scalar sufficientDecreaseCoefficient=1.e-2; //!<The coefficient for sufficient decrease
+    Scalar stepSizeDecrementFactor=0.5;//!< How much to decrement alpha
+    Scalar secondWolfConditionFactor=0.9; //!< Second Wolfe conditin factor (not used)
+    Scalar initialStep=1.0; //!< the initial apha value.
+    unsigned int maxIter=40;//!< max number of iterations in the backtracking algorithm
+  };
+
+}// end namespace
+
+
+
+#endif /* EXAMPLES_SRC_LINESEARCH_LINESEARCH_OPTIONS_HPP_ */
