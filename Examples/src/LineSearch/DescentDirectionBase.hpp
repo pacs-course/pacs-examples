c3c5a285c10613eba716878699418b945f8ed459
formaggia <luca.formaggia@gmail.com>
2020-12-29 11:07:13 +0100

    Tools for line search optimization
    Former-commit-id: 58f733159e6bd6637bfe115c5aba27a31abedd75

---
+++ Examples/src/LineSearch/DescentDirectionBase.hpp
@@ -0,0 +1,35 @@
+/*
+ * LineaSearchSolvers.hpp
+ *
+ *  Created on: Dec 27, 2020
+ *      Author: forma
+ */
+#include "LineSearch.hpp"
+#ifndef EXAMPLES_SRC_LINESEARCH_DESCENTDIRECTIONBASE_HPP_
+#define EXAMPLES_SRC_LINESEARCH_DESCENTDIRECTIONBASE_HPP_
+namespace apsc{
+/*!
+ * The base class for the callable objects that compute the descent direction
+ */
+ class DescentDirectionBase
+ {
+public:
+   /*!
+    * Given the current values (point, gradient etc) as defined in apsc::OptimizationCurrentValues
+    * returns a descent direction.
+    * @param values The current values.
+    * @return The descent direction
+    * @note It is not declared const since in some derived class it may modify the state of the object.
+    */
+  virtual apsc::LineSearch_traits::Vector operator()(apsc::OptimizationCurrentValues const & values)=0;
+  /*!
+   * This function by default it does nothing, but it may be used to reset data if needed
+   */virtual void reset(){};
+  virtual ~DescentDirectionBase()=default;
+};
+
+
+}// end namespace
+
+
+#endif /* EXAMPLES_SRC_LINESEARCH_LINEASEARCHSOLVERS_HPP_ */
---
+++ Examples/src/LineSearch/DescentDirections.hpp
@@ -0,0 +1,88 @@
+/*
+ * DescentDirections.hpp
+ *
+ *  Created on: Dec 28, 2020
+ *      Author: forma
+ */
+
+#ifndef EXAMPLES_SRC_LINESEARCH_DESCENTDIRECTIONS_HPP_
+#define EXAMPLES_SRC_LINESEARCH_DESCENTDIRECTIONS_HPP_
+#include "DescentDirectionBase.hpp"
+#include <limits>
+namespace apsc
+{
+  /*!
+   * Implements the gradient search
+   */
+  class GradientDirection : public DescentDirectionBase
+  {
+  public:
+    /*!
+     *  Returns - gradient as descent direction
+     * @param values The current values.
+     * @return The descent direction.
+     */
+    apsc::LineSearch_traits::Vector operator()
+    (apsc::OptimizationCurrentValues const & values) override
+    {
+      return -values.currentGradient;
+    }
+  };
+/*!
+ *  Implements the classic BFGS quasi-Newton algorithm.
+ */
+  class BFGSDirection: public DescentDirectionBase
+  {
+  public:
+    apsc::LineSearch_traits::Vector operator()
+        (apsc::OptimizationCurrentValues const & values) override;
+    /*!
+     * You need to reset if you run another problem or you start from a different initial point.
+     * @note This is done inside LineSearchSolver
+     */
+    void reset() override;
+  private:
+    apsc::OptimizationCurrentValues previousValues;
+    Eigen::MatrixXd H;
+    bool firstTime{true};
+    double const smallNumber=std::sqrt(std::numeric_limits<double>::epsilon());
+  };
+
+  /*!
+   * Implements BFGS with the direct computation of the approximate inverse of
+   * the Hessian.
+   */
+  class BFGSIDirection: public DescentDirectionBase
+    {
+    public:
+      apsc::LineSearch_traits::Vector operator()
+          (apsc::OptimizationCurrentValues const & values) override;
+      void reset() override;
+    private:
+      apsc::OptimizationCurrentValues previousValues;
+      Eigen::MatrixXd H;
+      bool firstTime{true};
+      double const smallNumber=std::sqrt(std::numeric_limits<double>::epsilon());
+    };
+  /*!
+   * Bazrzilain-Borwein
+   */
+  class BBDirection: public DescentDirectionBase
+      {
+      public:
+        apsc::LineSearch_traits::Vector operator()
+            (apsc::OptimizationCurrentValues const & values) override;
+        void reset() override
+        {firstTime=true;};
+      private:
+        apsc::OptimizationCurrentValues previousValues;
+        bool firstTime{true};
+        double const smallNumber=std::sqrt(std::numeric_limits<double>::epsilon());
+      };
+
+}// end namespace
+
+
+
+
+#endif /* EXAMPLES_SRC_LINESEARCH_DESCENTDIRECTIONS_HPP_ */
