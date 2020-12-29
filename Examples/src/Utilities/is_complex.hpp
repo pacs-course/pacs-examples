0f7ce236867bdb16acd9382c1775c22627fa5a40
formaggia <luca.formaggia@gmail.com>
2020-12-29 11:03:23 +0100

    some fixes and documentation

--- Examples/src/Utilities/is_complex.hpp
+++ Examples/src/Utilities/is_complex.hpp
@@ -1,18 +1,37 @@
 #ifndef HH_IS_COMPLEX_HHH
 #define HH_IS_COMPLEX_HHH
-#include <type_traits>
 #include <complex>
+#include <type_traits>
+/*!
+ * @file is_complex.hpp
+ * Apparently the Standard Libeary has no trait to test if a type si a complex
+ * number
+ */
+namespace apsc
+{
 namespace TypeTraits
 {
-  //! Primary template: false
-  template<class T>
-  struct is_complex : public std::false_type
-  {};
+//! Primary template: false
+template <class T> struct is_complex : public std::false_type
+{
+  /*!
+   * The type of the complex. Here set to void
+   */
+  using complex_type = void;
+};
 
-  //! Specialization: true if complex
-  template<class T>
-  struct is_complex<std::complex<T>> : public std::true_type
-  {};
-  
-}
+//! Specialization: true if complex
+template <class T> struct is_complex<std::complex<T>> : public std::true_type
+{
+  /*!
+   * The type of the complex
+   */
+  using complex_type = std::complex<T>;
+};
+
+//! to be consistent with C++17 style
+template <class T> inline bool constexpr is_complex_v = is_complex<T>::value;
+
+} // end namespace TypeTraits
+} // end namespace apsc
 #endif
