6f036b4c0106e2ee0d0890c34e17eaaf4266a249
formaggia <luca.formaggia@gmail.com>
2020-12-29 11:05:41 +0100

    Some new utilities for testing if a type is an eigen matrix

---
+++ Examples/src/Utilities/test_is_complex.cpp
@@ -0,0 +1,24 @@
+/*
+ * test_is_complex.cpp
+ *
+ *  Created on: Dec 27, 2020
+ *      Author: forma
+ */
+#include <iostream>
+#include "is_complex.hpp"
+int main()
+{
+  using namespace apsc::TypeTraits;
+  std::complex<double> a{1.,1.};
+  double c=9.0;
+  std::cout<<c<<" is complex="<<std::boolalpha<<is_complex<decltype(c)>()<<std::endl;
+  std::cout<<a<<" is complex="<<std::boolalpha<<is_complex<decltype(a)>()<<std::endl;
+  // Now using the traits value directly
+  std::cout<<c<<" is complex="<<std::boolalpha<<is_complex_v<decltype(c)><<std::endl;
+  std::cout<<a<<" is complex="<<std::boolalpha<<is_complex_v<decltype(a)><<std::endl;
+
+}
+
+
+
+
