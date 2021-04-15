6aaaa722365fb2d10ec579da531477b3d64ad7c3
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2021-04-15 13:35:23 +0000

    Add Lab07
    Former-commit-id: a4a011a1f831a9b619e843b70185003c8d0973c1

---
+++ Exercises/2021/07-eigen-newton/src/main.cpp
@@ -0,0 +1,31 @@
+#include "NewtonTraits.hpp"
+
+int
+main(int argc, char **argv)
+{
+  using ArgumentType       = NewtonTraits::ArgumentType;
+  using ReturnType         = NewtonTraits::ReturnType;
+  using JacobianMatrixType = NewtonTraits::JacobianMatrixType;
+
+  auto system = [](const ArgumentType &x) -> ReturnType {
+    ReturnType y(2);
+
+    y(0) = (1 - x(0)) * (1 - x(0)) + 0.1 * (5 - x(1)) * (5 - x(1));
+    y(1) = 1.5 - x(0) - 0.1 * x(1);
+
+    return y;
+  };
+
+  auto jacobianFun = [](const ArgumentType &x) -> JacobianMatrixType {
+    JacobianMatrixType J(2, 2);
+
+    J(0, 0) = -2 * (1 - x(0));
+    J(0, 1) = -0.2 * (5 - x(1));
+    J(1, 0) = -1;
+    J(1, 1) = -0.1;
+
+    return J;
+  };
+
+  return 0;
+}
