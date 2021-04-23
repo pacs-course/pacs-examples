d9823d66d841515959b0bd7c56cfcac1a1921029
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2021-04-23 13:16:28 +0000

    Upload lab 08 solutions
    Former-commit-id: 6b0ef4f8f06b7f8e860130456df70bbe3276c2ca

---
+++ Exercises/2021/08-traits-rkf/src-solution/RKFTraits.hpp
@@ -0,0 +1,51 @@
+#ifndef RKFTRAITS_HPP
+#define RKFTRAITS_HPP
+
+#include "Eigen/Dense"
+
+#include <cmath>
+#include <functional>
+
+/// The three kinds of RK solver for scalar and vector differential problems.
+class RKFType
+{
+public:
+  using Scalar = double;
+  using Vector = Eigen::VectorXd;
+};
+
+/// Primary class template.
+/// This class is in principle valid also for matrix problems.
+template <class ProblemType>
+class RKFTraits
+{
+public:
+  using VariableType = ProblemType;
+
+  using ForcingTermType =
+    std::function<VariableType(const double &, const VariableType &)>;
+
+  static double
+  norm(const VariableType &x)
+  {
+    return x.norm();
+  }
+};
+
+/// Specialization for scalar problems.
+template <>
+struct RKFTraits<double>
+{
+  using VariableType = double;
+
+  using ForcingTermType =
+    std::function<VariableType(const double &, const VariableType &)>;
+
+  static double
+  norm(const VariableType &x)
+  {
+    return std::abs(x);
+  }
+};
+
+#endif /* RKFTRAITS_HPP */
--- Exercises/2021/08-traits-rkf/src/RKF.hpp
+++ Exercises/2021/08-traits-rkf/src/RKF.hpp
@@ -2,5 +2,57 @@
 #define RKF_HPP
 
 
+/// Output stream for gnuplot.
+/// Possible extension: export also an exact solution, if provided.
+template <class ProblemType>
+std::ostream &
+operator<<(std::ostream &out, const RKFResult<ProblemType> &res)
+{
+  out << "# Number ot time steps: " << res.time.size() << std::endl
+      << "# Number of reductions: " << res.reductions << std::endl
+      << "# Number of expansions: " << res.expansions << std::endl
+      << "# Error estimate: " << res.error_estimate << std::endl;
+
+  double h_min = res.time[1] - res.time[0];
+  double h_max = h_min;
+
+  for (unsigned int i = 0; i < res.time.size() - 1; ++i)
+    {
+      const auto dt = res.time[i + 1] - res.time[i];
+      h_max         = std::max(h_max, dt);
+      h_min         = std::min(h_min, dt);
+    }
+
+  out << "# h_min: " << h_min << ", h_max: " << h_max << std::endl;
+
+  out << "# t ";
+  if constexpr (std::is_same_v<ProblemType, RKFType::Scalar>)
+    out << "y";
+  else // if constexpr (std::is_same_v<ProblemType, RKFType::Vector>)
+    out << "y[0] ... y[" << res.y[0].size() - 1 << "]";
+
+  out << std::endl;
+
+  size_t i = 0;
+  for (const auto &t : res.time)
+    {
+      out << "  " << t << "\t";
+      const auto &yy = res.y[i];
+
+      if constexpr (std::is_same_v<ProblemType, RKFType::Scalar>)
+        {
+          out << res.y[i];
+        }
+      else // if constexpr (std::is_same_v<ProblemType, RKFType::Vector>)
+        {
+          for (int k = 0; k < yy.size(); ++k)
+            out << yy[k] << "\t";
+        }
+
+      out << "\n";
+      ++i;
+    }
+  return out;
+}
 
 #endif /* RKF_HPP */
