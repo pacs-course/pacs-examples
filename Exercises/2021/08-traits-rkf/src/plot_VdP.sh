0d4334061331c5bf385c7e89da814882cc1e93f6
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2021-04-28 19:41:25 +0000

    Update Lab 08 solution

--- Exercises/2021/08-traits-rkf/src/plot_VdP.sh
+++ Exercises/2021/08-traits-rkf/src/plot_VdP.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gnuplot -p -e 'plot "results_VdP.out" using 1:2 with linespoints, "results_VdP.out" using 1:3 with linespoints'
+gnuplot -p -e 'plot "results_VdP.out" using 1:2 with linespoints,
+                    "results_VdP.out" using 1:3 with linespoints'
