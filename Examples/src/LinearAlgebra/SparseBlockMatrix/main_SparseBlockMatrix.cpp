dc883c5f33300ad1f49394de599a004c9527650e
formaggia <luca.formaggia@gmail.com>
2021-05-12 16:23:27 +0200

    Betttered and corrected bugs

--- Examples/src/LinearAlgebra/SparseBlockMatrix/main_SparseBlockMatrix.cpp
+++ Examples/src/LinearAlgebra/SparseBlockMatrix/main_SparseBlockMatrix.cpp
@@ -12,6 +12,7 @@ int main()
 {
   using namespace apsc;
   using namespace Eigen;
+  {
   SparseBlockMatrix<double,2,2> A({2,4},{2,4});
   // Block 0 0
   SparseBlockMatrix<double,2,2>::SpMat A00(A.rows({0,0}),A.cols({0,0}));
@@ -46,9 +47,47 @@ int main()
   y=SparseBlockMatrix<double,2,2>::RowVector::Ones(A.rows());
   std::cout<<"res==\n"<<y*A<<std::endl;
   std::cout<<"with full\n"<<y*F<<std::endl;
-
-
-
+  std::cout<<"Norm ="<< A.norm()<<std::endl;
+  }
+  {
+  //SparseBlockMatrix<double,2,2> A({2,4},{2,4});
+  SparseBlockMatrix<double,2,2> A;
+  A.resize({2,4},{2,4});
+// Block 0 0
+  SparseBlockMatrix<double,2,2>::SpMat A00(A.rows({0,0}),A.cols({0,0}));
+  using Tri=Eigen::Triplet<double>;
+  std::vector<Tri> coeff;
+  coeff.emplace_back(Tri{0,0,3.0});
+  coeff.emplace_back(Tri{0,1,9.0});
+  A00.setFromTriplets(coeff.begin(),coeff.end());
+  A.setBlock({0,0},A00);
+  // Block 0,1
+  coeff.clear();
+  coeff.emplace_back(Tri{0,0,1.0});
+  coeff.emplace_back(Tri{0,1,2.0});
+  coeff.emplace_back(Tri{1,0,3.0});
+  A.getBlock({0,1}).setFromTriplets(coeff.begin(),coeff.end());
+  // Block 1,0
+  A.addTranspose({1,0},{0,1});
+  // output
+  std::cout<<"A00=\n"<<A.getBlock({0,0})<<std::endl;
+  std::cout<<"A01=\n"<<A.getBlock({0,1})<<std::endl;
+  std::cout<<"A10=\n"<<A.getBlock({1,0}).transpose()<<std::endl;
+  std::cout<<"A11=\n"<<A.getBlock({1,1})<<std::endl;
+  auto F=A.fullMatrix();
+  std::cout<<"Full Matrix=\n"<<F<<std::endl;
+  SparseBlockMatrix<double,2,2>::ColVector x;
+  x.resize(A.cols());
+  x=SparseBlockMatrix<double,2,2>::ColVector::Ones(A.cols());
+  auto res = A*x;
+  std::cout<<"res==\n"<<res<<std::endl;
+  std::cout<<"with full\n"<<F*x<<std::endl;
+  SparseBlockMatrix<double,2,2>::RowVector y;
+  y=SparseBlockMatrix<double,2,2>::RowVector::Ones(A.rows());
+  std::cout<<"res==\n"<<y*A<<std::endl;
+  std::cout<<"with full\n"<<y*F<<std::endl;
+  std::cout<<"Norm ="<< A.norm()<<std::endl;
+  }
 
 
 }
