685a259229e2f602a76f353a1411a45960a7deee
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2021-05-14 12:53:46 +0000

    Update parallel examples according to lab 11

--- Examples/src/Parallel/OpenMP/03-PI/main_pi.cpp
+++ Examples/src/Parallel/OpenMP/03-PI/main_pi.cpp
@@ -1,8 +1,3 @@
-/**
- * @author Pasquale Claudio Africa <pasqualeclaudio.africa@polimi.it>
- * @date 2020
- */
-
 #include <omp.h>
 
 #include <cmath>
@@ -12,14 +7,14 @@
 /**
  * This exercise presents a simple program to determine the value of
  * pi. The algorithm suggested here is chosen for its simplicity. The
- * method evaluates the integral of 4 / (1 + x^2) between 0 and 1
+ * method evaluates the integral of 4 / (1 + x^2) over the interval (0, 1)
  * using the composite midpoint rule. We sum over the integral range
  * in a for loop, parallelized using OpenMP.
  */
 int
 main(int argc, char **argv)
 {
-  const unsigned int n = 1e8;
+  const unsigned int n = 1e9;
   const double       h = 1.0 / n;
 
   double x;
@@ -38,31 +33,33 @@ main(int argc, char **argv)
  * We declare pi as shared, any thread can access it. Shared is the
  * default, however being explicit is not a bad thing. You should be
  * careful that multiple threads are not writing to shared variables
- * at the same time (race conditions).
+ * at the same time (race conditions, as in exercise 03).
+ *
+ * Specifying default(none) is also a good practice, so the compiler
+ * forces us to specify whether each of the variables used herein
+ * are private or shared.
  */
 #pragma omp parallel private(x), firstprivate(sum), shared(pi)
   {
 /**
  * See http://jakascorner.com/blog/2016/06/omp-for-scheduling.html
- * for an explanation about scheduling types.
+ * for a description of all possible scheduling types.
  */
 #pragma omp for schedule(static)
-    for (unsigned int i = 1; i <= n; ++i)
+    for (unsigned int i = 0; i < n; ++i)
       {
-        x = h * (i - 0.5);
+        x = h * (i + 0.5);
         sum += 4.0 / (1.0 + x * x);
       }
 
 // Critical block, to prevent data race!
-#pragma omp critical
-    {
-      pi += h * sum;
-    }
+// Atomic is just a shorthand for a critical variable read/write operation.
+#pragma omp atomic update
+    pi += h * sum;
   }
 
-  std::cout << std::setprecision(16) << "pi = " << pi << ", error = "
-            << std::fabs(pi - 3.141592653589793238462643)
-            << std::endl;
+  std::cout << std::setprecision(16) << "pi = " << pi
+            << ", error = " << std::abs(pi - M_PI) << std::endl;
 
   return 0;
 }
