624190326cab14388c0b562ad7023b416072b677
Pasquale Africa <pasqualeclaudio.africa@polimi.it>
2021-05-26 10:27:48 +0000

    Add Lab 13

---
+++ Exercises/2021/13-pybind11/pybind11_examples/08_cpp-overload-eigen/example.cpp
@@ -0,0 +1,40 @@
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
+#include <iostream>
+
+#include <Eigen/LU>
+
+// ----------------
+// regular C++ code
+// ----------------
+
+Eigen::MatrixXd
+mul(const Eigen::MatrixXd &xs, double fac)
+{
+  std::cout << "Double" << std::endl;
+  return fac * xs;
+}
+
+Eigen::MatrixXi
+mul(const Eigen::MatrixXi &xs, int fac)
+{
+  std::cout << "Int" << std::endl;
+  return fac * xs;
+}
+
+// ----------------
+// Python interface
+// ----------------
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(example, m)
+{
+  m.doc() = "pybind11 example plugin";
+
+  // N.B. the order here is crucial, in the reversed order every "int" is
+  // converted to a "double"
+  m.def("mul", py::overload_cast<const Eigen::MatrixXi &, int>(&mul));
+  m.def("mul", py::overload_cast<const Eigen::MatrixXd &, double>(&mul));
+}
