728468a6c62b6cbd89453c02587c4ae4d379cab6
formaggia <luca.formaggia@gmail.com>
2021-10-11 19:55:27 +0200

    add another submodule in the list
    Former-commit-id: 2e1157d2b8092c5455a273a3132db2109f2bedb6

--- add_submodule_upstream.sh
+++ add_submodule_upstream.sh
@@ -13,4 +13,8 @@ git remote add upstream git@github.com:beltoforion/muparser.git
 popd
 pushd Extras/muparserx
 git remote add upstream git@github.com:beltoforion/muparserx.git
+pushd Examples/src/LinearAlgebra/spectra
+git remote add upstream https://github.com/yixuan/spectra.git
+popd
+
 
