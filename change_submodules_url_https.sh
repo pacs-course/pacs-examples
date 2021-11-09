17dba984779cd79096b0f4b0a0b5af3fd0bbc80e
formaggia <luca.formaggia@gmail.com>
2021-11-09 18:59:24 +0100

    fixed new submodule data
    Former-commit-id: 95e11ba67a84cb2cd2ea7b5b4baebc19fe91236f

--- change_submodules_url_https.sh
+++ change_submodules_url_https.sh
@@ -3,6 +3,7 @@ git config --file=.gitmodules submodule.Examples/src/LinearAlgebra/spectra.url h
 git config --file=.gitmodules submodule.Examples/src/LinearAlgebra/redsvd-h.url https://github.com/lformaggia/redsvd-h.git
 git config --file=.gitmodules submodule.Extras/muparser.url https://github.com/lformaggia/muparser.git
 git config --file=.gitmodules submodule.Extras/muparserx.url https://github.com/lformaggia/muparserx.git
+git config --file=.gitmodules submodule.Extras/json.url https://github.com/lformaggia/json.git
 git submodule sync
 git submodule update --recursive --remote
 
