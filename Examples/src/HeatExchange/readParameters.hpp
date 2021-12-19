28564a40be56a2d1b596d0c93591354b1e257b04
formaggia <luca.formaggia@gmail.com>
2021-12-19 20:22:54 +0100

    Added json reader to two examples
    Former-commit-id: 5d3abca4bc9b79e8ac21540d48825cb4c39018eb

--- Examples/src/HeatExchange/readParameters.hpp
+++ Examples/src/HeatExchange/readParameters.hpp
@@ -1,11 +1,18 @@
 #ifndef HH_READPARAMETERS_HH
 #define HH_READPARAMETERS_HH
-#include "parameters.hpp"
 #include <string>
+#include "json.hpp"
+#include "parameters.hpp"
 //! Reads problem parameters from GetPot file
 /*!
-  @param filename The getopot file with the new parameter values
+  @param filename The getopot file with the new values
   @param verbose Prints some information on the parameters
  */
 parameters readParameters(std::string const &filename, bool verbose = false);
+//! Reads problem parameters from json file
+/*!
+  @param filename The json file with the parameter values
+  @param verbose Prints some information on the parameters
+ */
+parameters readParameters_json(std::string const &filename, bool verbose = false);
 #endif
---
+++ Examples/src/QuadratureRule/AllDynamic/QuadParameters.cpp
@@ -0,0 +1,162 @@
+/*
+ * QuadParameters.cpp
+ *
+ *  Created on: Dec 20, 2021
+ *      Author: forma
+ */
+
+#include <stdexcept>
+#include "GetPot"
+#include "json.hpp"
+#include <iostream>
+#include <fstream>
+#include "QuadParameters.hpp"
+
+namespace apsc::NumericalIntegration
+{
+  QuadParameters readQuadParameters_GP(std::string const & filename)
+  {
+    using namespace std;
+    QuadParameters param;
+    GetPot cl(filename.c_str()); // open getpot file
+
+    // Read quadratur rule libraries
+    auto nlibs = cl.vector_variable_size("library");
+    if(nlibs == 0)
+      {
+        throw std::runtime_error("You need to specify at least one plugin library");
+      }
+    else
+      {
+        cout << "Reading " << nlibs << " plugin libraries\n";
+      }
+
+    std::vector<string> libs(nlibs);
+    for(unsigned int i = 0; i < nlibs; ++i)
+      {
+        string quadlib = cl("library", i, "NONE");
+        if(quadlib == string("NONE"))
+          {
+            throw std::runtime_error("Getpot file wrongly parsed,. Cannot read library");
+          }
+        else
+          {
+            cout << "Reading plugin library " << quadlib << std::endl;
+          }
+        libs[i]=quadlib;
+      }
+    param.library=libs;
+    libs.clear();
+
+    // read integrand libraries
+    nlibs = cl.vector_variable_size("udflib");
+    libs.resize(nlibs);
+    if(nlibs == 0)
+      {
+        throw std::runtime_error("You need to specify at least one integrand library");
+      }
+    else
+      {
+        cout << "Reading " << nlibs << " integrand libraries\n";
+      }
+
+    for(unsigned int i = 0; i < nlibs; ++i)
+      {
+        string intlib = cl("udflib", i, "NONE");
+        if(intlib == string("NONE"))
+          {
+            throw("Getpot file wrongly parsed. Cannot read library");
+          }
+        else
+          {
+            cout << "Integrands library " << intlib << std::endl;
+          }
+        libs[i]=intlib;
+      }
+    param.udflib=std::move(libs);
+
+    std::string fun_name = cl("integrand", "NULL");
+    if(fun_name == "NULL")
+      {
+        throw std::runtime_error("Getpot file must contain integrand=integrandName\n or integrand=?");
+      }
+    param.integrand=std::move(fun_name);
+
+    param.a=cl("a",param.a);
+    param.b=cl("b",param.b);
+    param.nint=cl("nint",param.nint);
+    param.maxIter=cl("maxIter",param.maxIter);
+    param.targetError=cl("targetError",param.targetError);
+    param.rule=cl("rule","?");
+    return param;
+  }
+
+  QuadParameters readQuadParameters_json(std::string const & filename)
+  {
+    using namespace std;
+    QuadParameters param;
+    std::ifstream file(filename);
+    nlohmann::json cl;
+    file >>cl;
+    file.close();
+
+
+    // Read quadratur rule libraries
+    // The second argument is there just to deduce the type
+    auto libs = cl.extract("library",param.library);
+    auto nlibs = libs.size();
+    if(nlibs == 0)
+      {
+        throw std::runtime_error("You need to specify at least one plugin library");
+      }
+    else
+      {
+        cout << "Reading " << nlibs << " plugin libraries\n";
+      }
+
+    for(unsigned int i = 0; i < nlibs; ++i)
+      {
+        cout << "Reading plugin library " << libs[i] << std::endl;
+      }
+    param.library=libs;
+    libs.clear();
+
+    // read integrand libraries
+    libs = cl.extract("udflib", param.udflib);
+    nlibs = libs.size();
+    if(nlibs == 0)
+      {
+        throw std::runtime_error("You need to specify at least one integrand library");
+      }
+    else
+      {
+        cout << "Reading " << nlibs << " integrand libraries\n";
+      }
+
+    for(unsigned int i = 0; i < nlibs; ++i)
+      {
+        cout << "Integrands library " << libs[i] << std::endl;
+      }
+    param.udflib=std::move(libs);
+
+    // @note: I must tell that the II argument is a std::string, since it is used to derive
+    //        the return type of exec();
+    std::string fun_name = cl.extract("integrand", std::string("NULL"));
+
+    if(fun_name == "NULL")
+      {
+        throw std::runtime_error("Json file must contain \"integrand\": integrandName\n or integrand=?");
+      }
+    param.integrand=std::move(fun_name);
+    // The last parameters
+    param.a=cl.extract("a",param.a);
+    param.b=cl.extract("b",param.b);
+    param.nint=cl.extract("nint",param.nint);
+    param.maxIter=cl.extract("maxIter",param.maxIter);
+    param.targetError=cl.extract("targetError",param.targetError);
+    param.rule=cl.extract("rule",std::string("?"));
+    return param;
+  }
+
+}
+
