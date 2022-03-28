74c62d6cec97805ce5615be034ea2162d8ee5296
formaggia <luca.formaggia@gmail.com>
2022-03-28 17:42:54 +0200

    cleaned up a bit example on libraries

--- Examples/src/SharedLibrary/exec.sh
+++ Examples/src/SharedLibrary/exec.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 echo "Silently recompiling the libraries"
+echo "main is linked against libsmall.so"
 make clean >/dev/null
 make >/dev/null
-
+rm -f libsmall.so libsmall.so.1
+echo "Creating links for version 1.0 of the library"
+set -x
+ln -s libsmall.so.1.0 libsmall.so.1
+ln -s libsmall.so.1 libsmall.so
+set +x
 export LD_LIBRARY_PATH=`pwd`
 echo "Executing main"
 ./main
 echo "Linking to the new version of the library"
 set -x
-rm -f libsmall.so libsmall.so.1
-ln -s libsmall.so.1.1 libsmall.so
+rm -f libsmall.so.1
 ln -s libsmall.so.1.1 libsmall.so.1
 set +x
 echo "Re-executing main with the new library"
