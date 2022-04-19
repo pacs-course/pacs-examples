77b71c026cfd7afa537853ff03a05ad9b223b8db
formaggia <luca.formaggia@gmail.com>
2022-04-19 19:55:29 +0200

    added new exampes of ranges and views
    Former-commit-id: 769317433404e8f8087d8eae952f1242fdc354b8

--- Examples/src/C++20/RangesAndViews/main.cpp
+++ Examples/src/C++20/RangesAndViews/main.cpp
@@ -65,6 +65,7 @@ int main()
     for (auto const & p:friends)
       std::cout<<p;
     }
+
   {
     // sort by age using the function member and greater than comparison operator
     std::ranges::sort(friends,std::greater<unsigned>(),&Person::getAge);
@@ -72,11 +73,60 @@ int main()
     for (auto const & p:friends)
       std::cout<<p;
     }
+  {
+    //Partition a vector into odd end even
+    std::ostream_iterator<int> out {std::cout, " "};
+    std::vector<int> v {-3,7,9,0,2,1,4,3,7,6,5,8,9};
+    std::cout << "Original vector:  \t";
+    std::ranges::copy(v, out);
+    // first the elements for wich the predicate returns true
+    auto tail = std::ranges::partition(v, [](int i){return i % 2 == 0;});
+
+    std::cout << "\nPartitioned vector: \t";
+    std::ranges::copy(std::ranges::begin(v), std::ranges::begin(tail), out);
+    std::cout << "│ ";
+    std::ranges::copy(tail, out);
+    std::cout<<std::endl;
+
+  }
 
   {
-    // Anather more complex example of projections
+    //Sort a vector (the simplest case!)
+    std::ostream_iterator<int> out {std::cout, " "};
+    std::vector<int> v {-3,7,9,0,2,1,4,3,7,6,5,8,9};
+    std::cout << "Original vector:  \t";
+    std::ranges::copy(v, out);
+
+    std::ranges::sort(v);
+
+    std::cout << "\nSorted vector: \t";
+    std::ranges::copy(v, out);
+    std::cout<<std::endl;
+  }
+
+  {
+      //Sort a vector (with a projector)
+      std::ostream_iterator<int> out {std::cout, " "};
+      std::vector<int> v {-3,7,9,0,2,1,4,3,7,6,5,8,9};
+      std::cout << "Original vector:  \t";
+      std::ranges::copy(v, out);
+
+      // default comparison, squared
+      std::ranges::sort(v,{},[](int x){return x*x;});
+
+      std::cout << "\nSorted vector^2: \t";
+      std::ranges::copy(v, out);
+      std::cout<<std::endl;
+    }
+
+
+  {
+    // Another more complex example of projections
     // and a use of a view. I compute v^2_i - v^2_{n-i}
+    std::ostream_iterator<int> out {std::cout, " "};
     std::vector<int> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::cout << "Original vector:  \t";
+    std::ranges::copy(v, out);
     std::vector<int> result;
     std::ranges::transform(v,                       // first range
                            v | std::views::reverse, // second range
@@ -84,6 +134,7 @@ int main()
                            std::minus<int>(), // operation on the elements of the two projected values
                            [](int v) { return v*v; }, // projection on the first range
                            [](int v) { return v*v; }); // projection on the II range
+    std::cout << "v^2_i - v^2_{n-i}\t";
     std::ranges::copy(result,
                       std::ostream_iterator<int>(std::cout, ","));
     std::cout<<std::endl;
@@ -91,18 +142,25 @@ int main()
 
   {
     // also a list is a range...
+    std::ostream_iterator<int> out {std::cout, " "};
     std::list<double> l{3.,4.,-9.5,-6.};
+    std::cout << "Original list:  \t";
+    std::ranges::copy(l, out);
     // extract the sum of all positive numbers (using a loop)
     auto gt0=[](double const & x){return x>0;};
     double sum{0};
     for (auto i: l|std::views::filter(gt0))
       sum+=i;
-    std::cout<<"Sum is "<<sum<<std::endl;
+    std::cout<<"Sum of positive numbers is "<<sum<<std::endl;
     //
   }
   {
     // Now we sum the square of the values greater than 0
+    std::ostream_iterator<int> out {std::cout, " "};
     std::list<double> l{3.,4.,-9.5,-6.};
+    std::cout << "Original list:  \t";
+    std::ranges::copy(l, out);
+
     // extract the sum of all positive numbers (using a loop)
     auto gt0=[](double const & x){return x>0;};
     double sum{0};
@@ -111,16 +169,17 @@ int main()
         std::views::transform([](double x){return x*x;})
         )
       sum+=i;
-    std::cout<<"Sum of sqares is "<<sum<<std::endl;
+    std::cout<<"Sum of squares of positive numbers is "<<sum<<std::endl;
     //
   }
 
   {
     // Joining strings
-   std::vector<std::string> course{"Advanced ","Programming ", "for ", "Scientific ", "Computing"};
-   auto joined=std::ranges::join_view(course);
-   for (auto const & c:joined)
-     std::cout<<c;
+    using namespace std::literals;
+   std::vector<std::string> course{"Advanced "s,"Programming "s, "for "s, "Scientific "s, "Computing"s};
+   auto joined=course | std::views::join;
+   std::string joinedString{joined.begin(),joined.end()};
+   std::cout<<joinedString;
    std::cout<<std::endl;
   }
 
