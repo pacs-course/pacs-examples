939056bfee00baf5b218d4c977bd3547722fbb8f
formaggia <luca.formaggia@gmail.com>
2023-01-25 12:19:45 +0100

    added few things

--- Examples/src/Utilities/absdiff.hpp
+++ Examples/src/Utilities/absdiff.hpp
@@ -3,6 +3,24 @@
 #include <type_traits>
 namespace apsc
 {
+/*!
+ * Safely computes the absolute difference of two unsigned.
+ *
+ * The computation of \f$|a-b|\f$ is troublesome if a and b are of signed type.
+ * Indeed if \f$a<b\f$ the result of std::abs(a-b) would be incorrect since
+ * the value (a-b) is not representable as an unsigned.
+ *
+ * This code works only if the arguments are both signed or both unsigned types. Otherwise
+ * the situation would be ambiguous since we would perform a comparison between quantities
+ * of different type.
+ *
+ * @tparam T1 the type of the first parameter
+ * @tparam T2 Te type of the second parameter
+ * @param a The first value
+ * @param b The second value
+ * @return The absolute difference between a and b. The type is equivalent to decltype(a-b);
+ * @pre parameters must be both of signed or unsigned type. Parameters must be subtractable.
+ */
   template<class T1, class T2>
   constexpr auto absdiff(T1 const & a, T2 const & b)
   {
