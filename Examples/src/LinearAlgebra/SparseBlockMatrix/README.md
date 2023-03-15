a23b19066055aca7b45710dd625838ee29575970
formaggia <luca.formaggia@gmail.com>
2023-03-15 13:15:08 +0100

    fixed a few things
    Former-commit-id: 633ff08089c05c462e8d00df2f90cfc8a9e2252a

--- Examples/src/LinearAlgebra/SparseBlockMatrix/README.md
+++ Examples/src/LinearAlgebra/SparseBlockMatrix/README.md
@@ -3,4 +3,30 @@
 With this tool you can cleare block matrices of any size, whose block is
 an Eigen sparse matrix.
 
-Enjoy!
+## How does it work ? ##
+You can construct a block matrix M using the constituting blocs M_ij.
+
+You have first to construct an empty block matrix, specifying the block sizes.  In particular,
+you have to give the number of rows and column of each block.
+
+Then, you can add (maybe by moving) the actual sparse mmatrix. You may indicate the a matrix block is actually the transpose of another block, without the need to create the transpose!
+
+Eventually you have methods for
+
+- Accessing a given block;
+- Matrix vector multiplication M*v
+- Vector matrix multiplication v*M (v row vector)
+- Computation of Frobenius norm 
+- Extracting the complete sparse matrix as a single matrix
+
+The blocks are stored using *shared pointers to Eigen matrices*, this is the trick needed to avoid storing 
+the transpose of a given block. The transposed block is a pointer to the block to transpose and I just keep track that that block refer to a transposition. 
+
+I rely on the fact that Eigen matrices do (finally!) support move semantic,
+so use `std::move` if you vant to move a block inside the block matrix and not just copy it.
+ 
+Some checks are made to verify that the dimensions of a stored block corresponds to the block size allocated during construction.
+
+## What do I learn here? #  
+- Some tools of th Eigen library
+- A use of `shared_ptr`
--- Examples/src/Utilities/test_is_eigen.cpp
+++ Examples/src/Utilities/test_is_eigen.cpp
@@ -6,13 +6,27 @@
  */
 #include "is_eigen.hpp"
 #include <iostream>
-// using concepts to test if tempalte argument is an eigen matrix
+// using concepts to test if template argument is an eigen matrix
 template <apsc::TypeTraits::EigenMatrixType M>
 void
 fun(M const &)
 {
   std::cout<<"fun() called with an Eigen matrix"<<std::endl;
 }
+// example of everloading with concepts
+template <apsc::TypeTraits::EigenSparseMatrixType M>
+void
+fun2(M const &)
+{
+  std::cout<<"fun2() called with a Sparse Eigen matrix"<<std::endl;
+}
+template <apsc::TypeTraits::EigenDenseMatrixType M>
+void
+fun2(M const &)
+{
+  std::cout<<"fun2() called with a Dense Eigen matrix"<<std::endl;
+}
+
 
 int
 main()
@@ -26,20 +40,33 @@ main()
   Eigen::Matrix<double, 2, 2> A;
   A << 1, 2, 3, 4;
   double c = 9.0;
-  std::cout << c << " is eigen= (should be false)" << std::boolalpha << is_eigen<decltype(c)>()
-            << std::endl;
-  std::cout << A << " is eigen= (should be true)" << std::boolalpha << is_eigen<decltype(A)>()
-            << std::endl;
-  // Now using the traits value directly
-  std::cout << c << " is eigen= (should be false)" << std::boolalpha
+  // Using the traits value directly
+  std::cout << " is eigen= (should be false)" << std::boolalpha
             << is_eigen_v<decltype(c)> << std::endl;
-  std::cout << A << " is eigen= (should be true)" << std::boolalpha
+  std::cout << " is eigen= (should be true)" << std::boolalpha
             << is_eigen_v<decltype(A)> << std::endl;
-  std::cout << smat << " is eigen= (should be true)" << std::boolalpha
+  std::cout << " is eigen= (should be true)" << std::boolalpha
             << is_eigen_v<decltype(smat)> << std::endl;
-  std::cout << svec << " is eigen= (should be true)" << std::boolalpha
+  std::cout << " is eigen= (should be true)" << std::boolalpha
             << is_eigen_v<decltype(svec)> << std::endl;
+  std::cout << " is dense eigen= (should be false)" << std::boolalpha
+             << is_dense_eigen_v<decltype(smat)> << std::endl;
+  std::cout << " is sparse eigen= (should be true)" << std::boolalpha
+              << is_sparse_eigen_v<decltype(smat)> << std::endl;
+    // Using concepts as traits
+  std::cout << " is eigen= (should be false)" << std::boolalpha
+            << EigenMatrixType<decltype(c)> << std::endl;
+  std::cout << " is eigen= (should be true)" << std::boolalpha
+            << EigenMatrixType<decltype(A)> << std::endl;
+  std::cout << " is eigen= (should be true)" << std::boolalpha
+            << EigenMatrixType<decltype(smat)> << std::endl;
+  std::cout << " is eigen= (should be true)" << std::boolalpha
+            << EigenMatrixType<decltype(svec)> << std::endl;
+
   fun(svec);
   fun(A);
   //fun(c); // it fails compilation
+  fun2(smat);
+  fun2(A);
+
 }
