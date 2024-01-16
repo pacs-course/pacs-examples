c9d33509a674783a02d51617a6c6648faf500046
formaggia <luca.formaggia@gmail.com>
2024-01-16 18:34:38 +0100

    corrected golden search and added some routines adapted from Brent library

--- Examples/src/LinearAlgebraUtil/test/testBasicMinFun.cpp
+++ Examples/src/LinearAlgebraUtil/test/testBasicMinFun.cpp
@@ -19,8 +19,20 @@ main()
     auto [x1, x2, s] = apsc::bracketIntervalMinumum(fun, a);
     std::cout << "Bracket found:"
               << " a=" << x1 << " b=" << x2 << " with status=" << s << "\n";
+    {
     auto [res, status] = apsc::golden_search(fun, x1, x2);
     std::cout << "Golden Search result " << res << " Status= " << std::boolalpha
               << status << std::endl;
+    }
+    {
+    auto [res, fval] = apsc::Brent_glomin(x1, x2,x1,1,1e-6,1e-6,fun);
+    std::cout << "Brent_glomin result " << res << " functionvalue= "
+              << fval << std::endl;
+    }
+    {
+    auto [res, fval] = apsc::Brent_local_min(x1, x2,1e-6,fun);
+    std::cout << "Brent_local_min result " << res << " functionvalue= "
+              << fval << std::endl;
+    }
   }
 }
