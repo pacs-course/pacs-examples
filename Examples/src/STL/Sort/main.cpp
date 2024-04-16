51f0eb2d38a5da4fab1edae8c09de04cf0e14b60
formaggia <luca.formaggia@gmail.com>
2024-04-16 09:20:17 +0200

    bettered example

--- Examples/src/STL/Sort/main.cpp
+++ Examples/src/STL/Sort/main.cpp
@@ -1,7 +1,9 @@
 #include "compare.hpp"
-#include <iostream>
-#include <vector>
+#include <algorithm>
 #include <cmath>
+#include <iostream>
+#include <iterator>
+#include <vector>
 
 int
 main()
@@ -9,34 +11,33 @@ main()
   using namespace std;
 
   // ************** compare
-  vector<std::string> s={"Apple", "applejuice", "milk", "milKshake", "APPLE", "Milk"};
-
+  vector<std::string> s = {"Apple",     "applejuice", "milk",
+                           "milKshake", "APPLE",      "Milk"};
 
   // useless : it is the default!!
   cout << " Normal sort (case sensitive)" << endl;
   std::sort(s.begin(), s.end(), CaseCompare());
-  for(auto const & i :s)
+  for(auto const &i : s)
     cout << i << " ";
   cout << endl;
 
   cout << " Sorted ignoring case" << endl;
   //  std::sort(s.begin(),s.end(),NocaseCompare());
   std::sort(s.begin(), s.end(), Nocase_locale());
-  for(auto const & i :s)
+  for(auto const &i : s)
     cout << i << " ";
   cout << endl;
 
   cout << " Now using lambdas" << endl;
-  s={"Apple", "applejuice", "milk", "milKshake", "APPLE", "Milk"};
- 
+  s = {"Apple", "applejuice", "milk", "milKshake", "APPLE", "Milk"};
 
   // useless : it is the default!!
   std::sort(s.begin(), s.end(),
             [](string const &a, string const &b) { return a < b; });
   cout << " Normal sort (case sensitive)" << endl;
-  for(auto const & i :s)
+  for(auto const &i : s)
     cout << i << " ";
- 
+
   cout << endl;
 
   auto nocomp = [](std::string const &a, std::string const &b) {
@@ -50,25 +51,34 @@ main()
   };
   std::sort(s.begin(), s.end(), nocomp);
   cout << " Sorted ignoring case" << endl;
-  for(auto const & i :s)
+  for(auto const &i : s)
     cout << i << " ";
   cout << endl;
 
 // Here the new constrained algorithm
 #if __cplusplus >= 202002L
   cout << " Now using the new constrained algorithm, ignoring cases" << endl;
-  s={"Apple", "applejuice", "milk", "milKshake", "APPLE", "Milk"};
+  s = {"Apple", "applejuice", "milk", "milKshake", "APPLE", "Milk"};
   std::ranges::sort(s, nocomp); // much simpler!
-  for(auto const & i :s)
+  for(auto const &i : s)
     cout << i << " ";
   cout << endl;
   // a different possibility, using projectors
   // I sort the string by the first letter only
-  cout << " Now using the new constrained algorithm, sorting by first letter and not ignoring case" << endl;
+  cout << " Now using the new constrained algorithm, sorting by first letter "
+          "and not ignoring case"
+       << endl;
   std::ranges::sort(s, {}, [](std::string const &a) { return a[0]; });
   // The {} indicate to use the default comparison, which is here less<char
+  // I show the  std::ranges::copy to print the vector
+  std::ranges::copy(s, std::ostream_iterator<std::string>(std::cout, " "));
+  /* the same with a for loop
   for(auto const & i :s)
     cout << i << " ";
+*/
   cout << endl;
+  std::cout << "The result may look strange... but remember that upper case "
+               "letters are \"smaller\" than lower case ones\n";
+
 #endif
 }
