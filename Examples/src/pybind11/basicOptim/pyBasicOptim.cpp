9e7fc2ceecc5fc7533c6995b272b0b8f8ca36179
formaggia <luca.formaggia@gmail.com>
2024-08-13 17:16:50 +0200

    Bettered some pybind11 examples

--- Examples/src/pybind11/basicOptim/pyBasicOptim.cpp
+++ Examples/src/pybind11/basicOptim/pyBasicOptim.cpp
@@ -1,23 +1,29 @@
-#include "pybind11/pybind11.h"
-#include "pybind11/functional.h"
-#include "pybind11/stl.h"
 #include "basicOptimization.hpp" // Include the header file where the functions are defined
-
+#include "pybind11/functional.h"
+#include "pybind11/pybind11.h"
+#include "pybind11/stl.h"
 
 namespace py = pybind11;
 using namespace apsc;
 /*
-I need to explicitely instantiate the templates for the functions I want to export to python.
+I need to explicitely instantiate the templates for the functions I want to
+export to python.
 */
-using funct=const std::function<double (double)> &;
-template std::tuple<double, bool> apsc::golden_search(funct f, double a, double b, double tol,unsigned maxIter);
-template std::array<double,2> apsc::Brent_glomin(double a, double b, double x0, double m, double e, double t,funct f);
-template std::array<double,2> apsc::Brent_local_min(double a, double b, double tol,funct f);
-template std::tuple<double, double, int> apsc::bracketIntervalMinimum(funct f, double x1, double h,unsigned maxIter);
+using funct = const std::function<double(double)> &;
+template std::tuple<double, bool>
+apsc::golden_search(funct f, double a, double b, double tol, unsigned maxIter);
+template std::array<double, 2> apsc::Brent_glomin(double a, double b, double x0,
+                                                  double m, double e, double t,
+                                                  funct f);
+template std::array<double, 2> apsc::Brent_local_min(double a, double b,
+                                                     double tol, funct f);
+template std::tuple<double, double, int>
+apsc::bracketIntervalMinimum(funct f, double x1, double h, unsigned maxIter);
 
-
-PYBIND11_MODULE(basicOptim, m) {
-    m.def("golden_search", &golden_search<funct>,py::arg("f"),py::arg("a"),py::arg("b"),py::arg("tol"),py::arg("maxIter"), 
+PYBIND11_MODULE(basicOptim, m)
+{
+    m.def("golden_search", &golden_search<funct>),
+    py::arg("f"),py::arg("a"),py::arg("b"),py::arg("tol"),py::arg("maxIter"), 
     R"pbdoc(
         Golden search function.
 
@@ -34,9 +40,10 @@ PYBIND11_MODULE(basicOptim, m) {
             tuple: A tuple containing the minimum value and a boolean indicating if the optimization converged.
 
         )pbdoc");
-    
-    m.def("Brent_glomin", &Brent_glomin<funct>, py::arg("a"),py::arg("b"),py::arg("x0"),py::arg("m"),py::arg("e"),py::arg("t"),py::arg("f"),
-    R"pbdoc(
+
+    m.def("Brent_glomin", &Brent_glomin<funct>, py::arg("a"), py::arg("b"),
+          py::arg("x0"), py::arg("m"), py::arg("e"), py::arg("t"), py::arg("f"),
+          R"pbdoc(
         Brent glomin function.
 
         This function performs Brent's method for global optimization to find the minimum of a given function.
@@ -54,9 +61,10 @@ PYBIND11_MODULE(basicOptim, m) {
             array: An array containing the minimum value and the number of iterations performed.
 
         )pbdoc");
-    
-    m.def("Brent_local_min", &Brent_local_min<funct>, py::arg("a"),py::arg("b"),py::arg("tol"),py::arg("f"),
-    R"pbdoc(
+
+    m.def("Brent_local_min", &Brent_local_min<funct>, py::arg("a"),
+          py::arg("b"), py::arg("tol"), py::arg("f"),
+          R"pbdoc(
         Brent local min function.
 
         This function performs Brent's method for local optimization to find the minimum of a given function.
@@ -71,9 +79,10 @@ PYBIND11_MODULE(basicOptim, m) {
             array: An array containing the minimum value and the number of iterations performed.
 
         )pbdoc");
-    
-    m.def("bracketIntervalMinimum", &bracketIntervalMinimum<funct>, py::arg("f"),py::arg("x1"),py::arg("h"),py::arg("maxIter"),
-    R"pbdoc(
+
+    m.def("bracketIntervalMinimum", &bracketIntervalMinimum<funct>,
+          py::arg("f"), py::arg("x1"), py::arg("h"), py::arg("maxIter"),
+          R"pbdoc(
         Bracket interval minimum function.
 
         This function finds an interval containing the minimum of a given function.
@@ -89,7 +98,3 @@ PYBIND11_MODULE(basicOptim, m) {
 
         )pbdoc");
 }
-
-
-
-
