348622c5a9159fd718281e02b189dd907d74338f
formaggia <luca.formaggia@gmail.com>
2024-09-16 16:18:51 +0200

    bettered example findiff

--- Examples/src/Functors/main_functors.cpp
+++ Examples/src/Functors/main_functors.cpp
@@ -1,10 +1,10 @@
 #include "functors.hpp"
 #include <algorithm>
+#include <cmath>
 #include <iomanip>
 #include <iostream>
-#include <vector>
-#include <cmath>
 #include <limits>
+#include <vector>
 int
 main()
 {
@@ -14,7 +14,8 @@ main()
   // Set maximal precision
   std::cout.precision(std::numeric_limits<double>::max_digits10);
   // comput fifth root of five
-  std::cout << "Exact value of fifth root of 5=" << std::pow(5,1./5.) << std::endl;
+  std::cout << "Exact value of fifth root of 5=" << std::pow(5, 1. / 5.)
+            << std::endl;
   // we have set a larger precision to see more digits. The setting
   // std::cout<<std::defaultfloat;
 
@@ -25,10 +26,14 @@ main()
   sqrt5.tolerance = 1.e-2;
   std::cout << "sqrt5(5)=" << sqrt5(5) << " with tol=1e-2" << std::endl;
   // sqrt4(10) with the generic functor
+  std::cout << "Exact value of the fouth root of 10=" << std::pow(10.0, 1. / 4.)
+            << std::endl;
   rootN<4> sqrt4;
-  std::cout << "sqrt4(10)=" << sqrt4(10) <<  " with tol= "<<sqrt4.tolerance<< std::endl;
-
-
+  std::cout << "sqrt4(10)=" << sqrt4(10) << " with tol= " << sqrt4.tolerance
+            << std::endl;
+  sqrt4.tolerance = 1.e-2;
+  std::cout << "sqrt4(10)=" << sqrt4(10) << " with tol= " << sqrt4.tolerance
+            << std::endl;
 
   Isgreater        ig(5.0);
   std::vector<int> v = {1, 2, 3, 4, 6, 7, 8, 18, 25};
@@ -48,8 +53,10 @@ main()
   else
     std::cout << " value greater than " << ig.my_value << " not found"
               << std::endl;
+
   // Let's order module 10 I create the object on the fly!
   std::sort(v.begin(), v.end(), LessModulo10());
+  std::cout << "Vector ordered in increaing order modulo 10:" << std::endl;
   for(auto x : v)
     std::cout << x << " ";
   std::cout << std::endl;
@@ -59,6 +66,7 @@ main()
   // I can use array directly Vector3D is just an alias
   std::array<double, 3> b{3., 2., 1.};
   auto                  crp = cross(a, b);
+  std::cout << "Cross product of (1,2,3) and (3,2,1) is: ";
   for(auto x : crp)
     std::cout << x << " ";
   std::cout << std::endl;
