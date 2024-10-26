aedb6824504a717ff56ecddc06c0a3be2228c43f
formaggia <luca.formaggia@gmail.com>
2024-10-26 09:26:11 +0200

    minor chenches in Polygon

--- Examples/src/Polygon/PolygonUtilities.hpp
+++ Examples/src/Polygon/PolygonUtilities.hpp
@@ -6,17 +6,19 @@
 namespace Geometry
 {
 /*!
- * @fn std::unique_ptr<AbstractPolygon> createPolygon(const std::string&, Args...)
+ * @fn std::unique_ptr<AbstractPolygon> createPolygon(const std::string&,
+ * Args...)
  * @brief A simple factory of polygons
  *
  * @tparam Args The type of the optional arguments
  * @param name The name of the polygon
  * @param args The optional arguments to be passed to the constructor
- * @return A pointer to the abstract class pointing to the newly created concrete Polygon
+ * @return A pointer to the abstract class pointing to the newly created
+ * concrete Polygon
  */
-  template<typename ...Args>
+template <typename... Args>
 std::unique_ptr<AbstractPolygon>
-createPolygon(std::string const &name, const Args& ...args)
+createPolygon(std::string const &name, const Args &...args)
 {
   if(name == "Polygon")
     return std::make_unique<Polygon>(args...);
@@ -25,8 +27,7 @@ createPolygon(std::string const &name, const Args& ...args)
   if(name == "Triangle")
     return std::make_unique<Triangle>(args...);
   // this way I can easily check if I try to construct a non existent Polygon
-  return std::unique_ptr<AbstractPolygon>(nullptr);
+  return std::unique_ptr<AbstractPolygon>{nullptr};
 }
 } // namespace Geometry
-
 #endif
