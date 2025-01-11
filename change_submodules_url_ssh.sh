b425ed11c66aee68bb0c3f7f8ce2007a80b18e1a
formaggia <luca.formaggia@polimi.it>
2025-01-11 20:52:47 +0100

    updated tools

--- change_submodules_url_ssh.sh
+++ change_submodules_url_ssh.sh
@@ -4,6 +4,9 @@ git config --file=.gitmodules submodule.Examples/src/LinearAlgebra/redsvd-h.url 
 git config --file=.gitmodules submodule.Extras/muparser.url git@github.com:pacs-course/muparser.git
 git config --file=.gitmodules submodule.Extras/muparserx.url git@github.com:pacs-course/muparserx.git
 git config --file=.gitmodules submodule.Extras/json.url git@github.com:pacs-course/json.git
+git config --file=.gitmodules submodule.Extras/pybind11.url git@github.com:pacs-course/pybind11.git
+git config --file=.gitmodules submodule.Examples/src/pybind11/pybind11_examples.url git@github.com:pacs-course/pybind11_examples.git
+
 git submodule sync
 git submodule update --recursive --remote
 
