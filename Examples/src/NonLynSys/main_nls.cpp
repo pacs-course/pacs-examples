2eb2741971c43cbc24b766c62b47a12203200c47
formaggia <luca.formaggia@polimi.it>
2025-03-14 16:28:43 +0100

    bettered nonlinsystem

--- Examples/src/NonLynSys/main_nls.cpp
+++ Examples/src/NonLynSys/main_nls.cpp
@@ -14,59 +14,79 @@ int
 main()
 {
   using namespace apsc::NonLinSysTraits;
-  // The greater common division and largest common multipliers with 2 to 100
-  apsc::NonLinSys<unsigned int, ScalarTraits> nls;
-  // I extract the type from the non linear system (not really necessary, I know
-  // it is an unsigned int)
-  using ArgumentType =
-    apsc::NonLinSys<unsigned int, ScalarTraits>::ArgumentType;
-  for(unsigned int i = 2; i <= 100u; ++i)
-    {
-      nls.addToSystem([i](ArgumentType const &x) { return std::gcd(x, i); });
-    }
-  // gcd/lcm of 333 with 11
-  unsigned int res1 = nls(10, 333);
-  auto         res2 = nls(333);
-  std::cout << "gcd 333  and 11 " << res1 << std::endl;
 
-  std::size_t k = 0;
-  for(std::size_t i = 2; i <= 100u; ++i)
-    {
-      auto z = res2[k++];
-      if(z == 1u)
-        std::cout << "333 and " << i << " are co-prime\n";
-    }
-  std::cout << std::endl;
+  // I create a system of scalar functions
+  apsc::NonLinSys<double, ScalarTraits> system;
+  // I add a function to the system
+  system.addToSystem([](double const &x) { return x * x; });
+  // I add another function
+  system.addToSystem([](double const &x) { return std::sin(x); });
+  // I add another function
+  system.addToSystem([](double const &x) { return std::cos(x); });
+  // I add another function
+  system.addToSystem([](double const &x) { return std::tan(x); });
 
-  // The case of a system of two equation of two variable. I use the default
-  // trait
-  apsc::NonLinSys nlsd;
-  using ArgumentTyped = apsc::NonLinSys<double>::ArgumentType;
-  nlsd.addToSystem(
-    [](ArgumentTyped const &x) { return std::sin(x[0]) + std::cos(x[1]); });
-  nlsd.addToSystem(
-    [](ArgumentTyped const &x) { return std::sinh(x[0]) + std::cosh(x[1]); });
-  auto res = nlsd({3.4, 5.7}); // pass the vector 3.4, 5.7
-  std::cout << "Result of the call to the system of functions:\n";
+  // I can get the function with the index
+  auto fun = system.getFunction(2);
+  std::cout << "Function 2(3.1415)=" << fun(3.1415) << std::endl;
+  // I can change the function
+  system.updateFunction([](double const &x) { return x * x * x; }, 2);
+  fun = system.getFunction(2);
+  std::cout << "Function 2(3.1415)=" << fun(3.1415) << std::endl;
+  // I can call the system
+  auto res = system(3.0);
+  std::cout << "system(3.0)= ";
   for(auto y : res)
+    std::cout << y << ",";
+  std::cout << std::endl;
+  // I can iterate on a system
+  std::cout << "iterating on the system :\n ";
+  for(auto const &f : system)
     {
-      std::cout << y << " ";
+      std::cout << f(3.0) << ",";
     }
+
   std::cout << std::endl;
 
+  // Now I try the case with Eigen Vector as arguments
+  apsc::NonLinSys<double, apsc::NonLinSysTraits::EigenVectorTraits> system2;
+  // I add a function to the system
+  system2.addToSystem(
+    [](Eigen::VectorXd const &x) { return x.array().square().sum(); });
+  // I add another function
+  system2.addToSystem(
+    [](Eigen::VectorXd const &x) { return x.array().sin().sum(); });
+  // I add another function
+  system2.addToSystem([](Eigen::VectorXd const &x) { return x.norm(); });
+  // I add another function
+  system2.addToSystem([](Eigen::VectorXd const &x) { return x.lpNorm<1>(); });
+
+  // I can get the function with the index
+  auto fun2 = system2.getFunction(2);
+  std::cout << "Function 2(3.1415)="
+            << fun2(Eigen::VectorXd::Constant(3, 3.1415)) << std::endl;
+  // I can call the system
+  apsc::NonLinSys<
+    double, apsc::NonLinSysTraits::EigenVectorTraits>::ArgumentType arg(3);
+  arg << 1., 2., -9;
+  auto res3 = system2(arg);
+  std::cout << "system2(1,2,-9)=" << res3[0] << "," << res3[1] << "," << res3[2]
+            << std::endl;
   // Now I try the function factory
   apsc::FunctionFactory<double, ScalarTraits> factory;
 
-  // Add two functions
+  // Add three functions
   factory.addToFactory("Function 1",
                        [](double const &x) { return std::sin(x); });
   factory.addToFactory("Function 2",
                        [](double const &x) { return std::cos(x); });
+  factory.addToFactory("Function 3",
+                       [](double const &x) { return std::tan(x); });
   // if uncommented I have an error you cannot have t2 functions with the same
   // id factory.addToFactory("Function 2",[](double const &x){return
   // std::tan(x);}); Get one function with the identifier
-  auto fun = factory.getFunction("Function 2");
-  std::cout << "Function 2(3.1415)=" << fun(3.1415) << std::endl;
+  auto funct = factory.getFunction("Function 2");
+  std::cout << "Function 2(3.1415)=" << funct(3.1415) << std::endl;
   // If uncommented I have an error
   // auto fun2 = factory.getFunction("Function 3");
   // Also if I uncomment th enext line I get an error. I cannot just add a
